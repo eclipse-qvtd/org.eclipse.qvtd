d65eb7389dd9af95b65e267e0ee067f381426c30
qglineur <qglineur>
2008-05-29 16:36:04 +0000

    Revised version of base

--- plugins/org.eclipse.qvt.declarative.compilation/src/org/eclipse/qvt/declarative/compilation/CompilationProvider.java
+++ plugins/org.eclipse.qvt.declarative.compilation/src/org/eclipse/qvt/declarative/compilation/CompilationProvider.java
@@ -1,12 +1,16 @@
 package org.eclipse.qvt.declarative.compilation;
 
+import java.util.List;
 import java.util.Map;
 
-import org.eclipse.qvt.declarative.common.Provider;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.qvt.declarative.common.framework.service.Provider;
 
 public interface CompilationProvider extends Provider{
 	
 	public String getName ();
-	public Object compile (Object source, Map<String, String> parameters);
+	public List<IFile> compile (IAdaptable structuredSource, Map<String, String> parameters, IFolder sourceFolder, IFolder buildFolder);
 
 }
--- plugins/org.eclipse.qvt.declarative.compilation/src/org/eclipse/qvt/declarative/compilation/CompileOperation.java
+++ plugins/org.eclipse.qvt.declarative.compilation/src/org/eclipse/qvt/declarative/compilation/CompileOperation.java
@@ -1,37 +1,68 @@
 package org.eclipse.qvt.declarative.compilation;
 
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.qvt.declarative.common.Operation;
-import org.eclipse.qvt.declarative.common.Provider;
+import org.eclipse.qvt.declarative.common.framework.service.Operation;
+import org.eclipse.qvt.declarative.common.framework.service.Provider;
 
 public class CompileOperation implements Operation {
 	
-	protected Object source;
+	protected IAdaptable source;
 	protected Map<String, String> parameters;
+	protected IFolder sourceFolder;
+	protected IFolder buildFolder;
 	
-	public CompileOperation(IAdaptable source, Map<String, String> parameters) {
+	public CompileOperation(IAdaptable source, Map<String, String> parameters, IFolder sourceFolder, IFolder buildFolder) {
 		this.source = source;
 		this.parameters = parameters;
+		this.sourceFolder = sourceFolder;
+		this.buildFolder = buildFolder;
 	}
 
 	@Override
-	public Object execute(Provider provider) {
-		Object result = null;
+	public List<IFile> execute(Provider provider) {
+		List<IFile> result = null;
 		if (provider instanceof CompilationProvider) {
 			CompilationProvider compilationProvider = (CompilationProvider) provider;
-			result = compilationProvider.compile(source, parameters);
+			result = compilationProvider.compile(source, parameters, sourceFolder, buildFolder);
 		}
 		return result;
 	}
 
-	public Object getSource() {
+	public IAdaptable getSource() {
 		return source;
+	}
+
+	public void setSource(IAdaptable source) {
+		this.source = source;
 	}
 
 	public Map<String, String> getParameters() {
 		return parameters;
+	}
+
+	public void setParameters(Map<String, String> parameters) {
+		this.parameters = parameters;
+	}
+
+	public IFolder getSourceFolder() {
+		return sourceFolder;
+	}
+
+	public void setSourceFolder(IFolder sourceFolder) {
+		this.sourceFolder = sourceFolder;
+	}
+
+	public IFolder getBuildFolder() {
+		return buildFolder;
+	}
+
+	public void setBuildFolder(IFolder buildFolder) {
+		this.buildFolder = buildFolder;
 	}
 
 }
