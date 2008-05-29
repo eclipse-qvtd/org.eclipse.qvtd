149d902cd3b153a1a1d01a334bafe3d963fa9913
qglineur <qglineur>
2008-05-29 16:36:05 +0000

    Revised version of base

--- plugins/org.eclipse.qvt.declarative.execution/src/org/eclipse/qvt/declarative/execution/ExecuteOperation.java
+++ plugins/org.eclipse.qvt.declarative.execution/src/org/eclipse/qvt/declarative/execution/ExecuteOperation.java
@@ -1,35 +1,56 @@
 package org.eclipse.qvt.declarative.execution;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.qvt.declarative.common.Operation;
-import org.eclipse.qvt.declarative.common.Provider;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.qvt.declarative.common.framework.service.Operation;
+import org.eclipse.qvt.declarative.common.framework.service.Provider;
 
 public class ExecuteOperation implements Operation {
 
 	protected IFile sourceFile;
 	protected ExecutionContext parameters;
+	protected IFolder sourceFolder;
+	protected IFolder buildFolder;
 
-	public ExecuteOperation(IFile sourceFile, ExecutionContext parameters) {
+	public ExecuteOperation(IFile sourceFile, ExecutionContext parameters, IFolder sourceFolder, IFolder buildFolder) {
 		super();
 		this.sourceFile = sourceFile;
 		this.parameters = parameters;
+		this.sourceFolder = sourceFolder;
+		this.buildFolder = buildFolder;
 	}
 
 	@Override
 	public Object execute(Provider provider) {
 		if (provider instanceof ExecutionProvider) {
 			ExecutionProvider executionProvider = (ExecutionProvider) provider;
-			return executionProvider.execute(sourceFile, parameters);
+			return executionProvider.execute(sourceFile, parameters, sourceFolder, buildFolder);
 		}
 		return null;
 	}
 
-	public IFile getExecutable() {
+	public IFolder getSourceFolder() {
+		return sourceFolder;
+	}
+
+	public void setSourceFolder(IFolder sourceFolder) {
+		this.sourceFolder = sourceFolder;
+	}
+
+	public IFolder getBuildFolder() {
+		return buildFolder;
+	}
+
+	public void setBuildFolder(IFolder buildFolder) {
+		this.buildFolder = buildFolder;
+	}
+
+	public IFile getSourceFile() {
 		return sourceFile;
 	}
 
-	public void setExecutable(IFile executable) {
-		this.sourceFile = executable;
+	public void setSourceFile(IFile sourceFile) {
+		this.sourceFile = sourceFile;
 	}
 
 	public ExecutionContext getParameters() {
--- plugins/org.eclipse.qvt.declarative.execution/src/org/eclipse/qvt/declarative/execution/ExecutionProvider.java
+++ plugins/org.eclipse.qvt.declarative.execution/src/org/eclipse/qvt/declarative/execution/ExecutionProvider.java
@@ -1,12 +1,13 @@
 package org.eclipse.qvt.declarative.execution;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.qvt.declarative.common.Provider;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.qvt.declarative.common.framework.service.Provider;
 
 public interface ExecutionProvider extends Provider {
 	
 	public String getName();
 	
-	public Object execute(IFile executable, ExecutionContext parameters);
+	public Object execute(IFile sourceFile, ExecutionContext parameters, IFolder sourceFolder, IFolder buildFolder);
 
 }
