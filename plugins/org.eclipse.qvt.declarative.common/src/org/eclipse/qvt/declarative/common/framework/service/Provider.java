60d2d9a485e130b350ecd3179a6a888ec865dc4d
qglineur <qglineur>
2008-06-27 18:03:42 +0000

    Added comments

--- plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Provider.java
+++ plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Provider.java
@@ -1,7 +1,21 @@
 package org.eclipse.qvt.declarative.common.framework.service;
 
+/**
+ * Base interface for providers. This interface is intended to be specialized to
+ * described a service. A service delegates call to the provider implementation.
+ * 
+ * @author Quentin Glineur
+ * 
+ */
 public interface Provider {
-	
-	public boolean provides (Operation operation);
+
+	/**
+	 * Indicates whether the provider support the given operation .
+	 * 
+	 * @param operation
+	 *            The operation to test for support.
+	 * @return true if the providers support the operation, false otherwise.
+	 */
+	public boolean provides(Operation operation);
 
 }
