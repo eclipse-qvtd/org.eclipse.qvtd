10bfa7d18d4f59770064756d1e9aa3dc2cb6e7e2
qglineur <qglineur>
2008-06-27 18:07:06 +0000

    Corrected handling of labeled models

--- plugins/org.eclipse.qvt.declarative.relations.atlvm/src/org/eclipse/qvt/declarative/relations/atlvm/utils/ASMEMFModelUtils.java
+++ plugins/org.eclipse.qvt.declarative.relations.atlvm/src/org/eclipse/qvt/declarative/relations/atlvm/utils/ASMEMFModelUtils.java
@@ -14,10 +14,16 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.m2m.atl.drivers.emf4atl.ASMEMFModel;
 import org.eclipse.qvt.declarative.execution.LabelledModel;
 
+/**
+ * Utility class with conversion methods between EMF and ASMModel
+ * 
+ * @author Quentin Glineur
+ */
 public class ASMEMFModelUtils {
 
 	/**
@@ -26,7 +32,7 @@ public class ASMEMFModelUtils {
 	 * @param resource
 	 * @return
 	 */
-	public static EPackage getMetamodel(Resource resource) {
+	protected static EPackage getMetamodel(Resource resource) {
 		EPackage result = null;
 		List<EObject> contents = resource.getContents();
 		if (contents.isEmpty())
@@ -36,8 +42,12 @@ public class ASMEMFModelUtils {
 		return result;
 	}
 
-	public static String getManageableURI(EPackage ePackage) {
-		return "uri:" + ePackage.getNsURI(); //$NON-NLS-1$
+	protected static String getManageableURI(String uri) {
+		return "uri:" + uri; //$NON-NLS-1$
+	}
+
+	protected static String getManageableURI(EPackage ePackage) {
+		return getManageableURI(ePackage.getNsURI());
 	}
 
 	/**
@@ -46,11 +56,13 @@ public class ASMEMFModelUtils {
 	 * @param ePackage
 	 * @return the created ASMEMFModel
 	 */
-	public static ASMEMFModel getASMEMFModelFrom(EPackage ePackage) {
+	public static ASMEMFModel getASMEMFModelFrom(EPackage ePackage, String alias) {
+
+		String name = alias != null ? alias : ePackage.getName();
+
 		try {
-			ASMEMFModel result = ASMEMFModel.loadASMEMFModel(
-					ePackage.getName(), ASMEMFModel.createMOF(null),
-					getManageableURI(ePackage), null);
+			ASMEMFModel result = ASMEMFModel.loadASMEMFModel(name, ASMEMFModel
+					.createMOF(null), getManageableURI(ePackage), null);
 			return result;
 		} catch (Exception e) {
 			return null;
@@ -58,29 +70,42 @@ public class ASMEMFModelUtils {
 	}
 
 	/**
-	 * Create an ASMEMFModel from a resource whose metamodel is registered
+	 * Trivial facility creating an ASMEMFModel from a non empty resource. The
+	 * metamodel is guessed from the first element of the resource
 	 * 
 	 * @param resource
+	 *            the resource to containing the model
 	 * @param name
+	 *            the wanted alias
 	 * @return
 	 * @throws Exception
 	 */
-	public static ASMEMFModel getASMEMFModelFrom(Resource resource,
-			String name, URI metamodelURI) throws Exception {
+	public static ASMEMFModel getASMEMFModelFrom(Resource resource, String name)
+			throws Exception {
 		resource.load(Collections.EMPTY_MAP);
 
-		ASMEMFModel metamodel = metamodelURI == null ? getASMEMFModelFrom(getMetamodel(resource))
-				: ASMEMFModel.newASMEMFModel(name, metamodelURI.toString(),
-						ASMEMFModel.createMOF(null), null);
+		ASMEMFModel metamodel = getASMEMFModelFrom(getMetamodel(resource), null);
 		return ASMEMFModel.loadASMEMFModel(name, metamodel, resource.getURI(),
 				null);
 	}
 
-	// private static Resource getResourceFrom (URI uri) {
-	// Resource resource = ASMEMFModel.getResourceSet().getResource(uri, true);
-	// return resource;
-	// }
-	//
+	/**
+	 * Trivial method creating an ASMEMFModel wrapping a given resource.
+	 * 
+	 * @param resource
+	 *            The EMF resource to wrap
+	 * @param name
+	 *            The model alias
+	 * @param metamodel
+	 *            The ASMEMFModel wrapping the metamodel
+	 * @return The corresponding ASMEMFModel
+	 * @throws Exception
+	 */
+	protected static ASMEMFModel getASMEMFModelFrom(Resource resource,
+			String name, ASMEMFModel metamodel) throws Exception {
+		return ASMEMFModel.loadASMEMFModel(name, metamodel, resource.getURI(),
+				null);
+	}
 
 	// FIXME: getResource loadOnDemand
 	protected static Resource getResourceFrom(IFile file, boolean autoCreate)
@@ -92,8 +117,6 @@ public class ASMEMFModelUtils {
 			file.create(new FileInputStream(newFile), true, null);
 			Resource resource = ASMEMFModel.getResourceSet().createResource(
 					fileURI);
-			// Map<String, Boolean> options = new HashMap<String, Boolean>();
-			// options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
 			resource.save(null);
 			return resource;
 		}
@@ -102,12 +125,6 @@ public class ASMEMFModelUtils {
 				true);
 		return resource;
 	}
-
-	// public static ASMEMFModel getASMEMFModelFrom (IFile file, String name)
-	// throws Exception {
-	// Resource resource = getResourceFrom(file);
-	// return getASMEMFModelFrom(resource, name);
-	// }
 
 	protected static IFile getFileFrom(URI uri) throws Exception {
 		String fileString = uri.toFileString();
@@ -119,40 +136,70 @@ public class ASMEMFModelUtils {
 		throw new Exception("Cannot find file from uri: " + uri);
 	}
 
-	// TODO implement
-	public static ASMEMFModel getASMEMFModelFrom(LabelledModel namedModel,
+	protected static ASMEMFModel getASMEMFModelFromDynamicEcore(URI modelURI,
+			String name) {
+		Resource modelResource = ASMEMFModel.getResourceSet().getResource(
+				modelURI, true);
+		EPackage mainPackage = null;
+		for (EObject object : modelResource.getContents()) {
+			if (object instanceof EPackage) {
+				EPackage ePackage = (EPackage) object;
+				if (mainPackage == null) {
+					mainPackage = ePackage;
+				}
+				String nsURI = ePackage.getNsURI();
+				if (nsURI == null)
+					throw new IllegalArgumentException(
+							"Invalid metamodel with no NS URI:" + modelResource);
+				ASMEMFModel.getResourceSet().getPackageRegistry().put(nsURI,
+						ePackage);
+			}
+		}
+		ASMEMFModel result = getASMEMFModelFrom(mainPackage, name);
+		return result;
+	}
+
+	/**
+	 * A utility method to build an ASMEMFModel from a LabelledModel
+	 * 
+	 * @param labelledModel
+	 *            the labeled model from which to build the ASMEMFModel
+	 * @param autoCreate
+	 *            if set to true, the model will be created if it does not exist
+	 * @return the associated ASMEMFModel
+	 * @throws Exception
+	 */
+	public static ASMEMFModel getASMEMFModelFrom(LabelledModel labelledModel,
 			boolean autoCreate) throws Exception {
-		URI modelURI = URI.createURI(namedModel.getAccessor());
-		ASMEMFModel metamodel = null;
-		String modelName = namedModel.getName();
-		URI metamodelURI = URI.createURI(namedModel.getMetamodel()
+		URI modelURI = URI.createURI(labelledModel.getAccessor());
+		URI metamodelURI = URI.createURI(labelledModel.getMetamodel()
 				.getAccessor());
 
+		ASMEMFModel metamodel = null;
+		String modelName = labelledModel.getName();
+		String metamodelName = labelledModel.getMetamodel().getName();
+
 		// load the metamodel if needed
-		if (metamodelURI != null) {
-			Resource metamodelResource = ASMEMFModel.getResourceSet()
-					.getResource(metamodelURI, true);
-			for (EObject object : metamodelResource.getContents()) {
-				if (object instanceof EPackage) {
-					EPackage metamodelPackage = (EPackage) object;
-					String metamodelNsURI = metamodelPackage.getNsURI();
-					if (metamodelNsURI == null)
-						throw new IllegalArgumentException(
-								"Invalid metamodel with no NS URI:"
-										+ metamodelResource);
-					ASMEMFModel.getResourceSet().getPackageRegistry().put(
-							metamodelNsURI, metamodelPackage);
-				}
+		if (metamodelURI.isFile()) {
+			metamodel = getASMEMFModelFromDynamicEcore(metamodelURI,
+					labelledModel.getMetamodel().getName());
+		} else {
+			if (metamodelURI.toString().equals(EcorePackage.eNS_URI)) {
+				metamodel = ASMEMFModel.createMOF(null);
+			} else {
+				metamodel = ASMEMFModel.loadASMEMFModel(metamodelName,
+						ASMEMFModel.createMOF(null),
+						getManageableURI(metamodelURI.toString()), null);
 			}
-			// metamodel =
-			// ASMEMFModel.loadASMEMFModel(namedModel.getName()+"_MM",
-			// ASMEMFModel.createMOF(null), namedModel.getMetamodel(), null);
 		}
 
 		if (modelURI.isFile()) {
 			IFile modelFile = getFileFrom(modelURI);
 			Resource modelResource = getResourceFrom(modelFile, autoCreate);
-			return getASMEMFModelFrom(modelResource, modelName, metamodelURI);
+			ASMEMFModel result = getASMEMFModelFrom(modelResource, modelName,
+					metamodel);
+			result.setIsTarget(true);
+			return result;
 		}
 		throw new Exception("Invalid model URI: " + modelURI);
 	}
