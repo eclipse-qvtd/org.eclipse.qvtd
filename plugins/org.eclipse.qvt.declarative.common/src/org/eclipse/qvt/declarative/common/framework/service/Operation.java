b364389fa995ec5c5c48d93ed21f19e643f1db69
qglineur <qglineur>
2008-07-08 16:05:31 +0000

    Comments

--- plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Operation.java
+++ plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Operation.java
@@ -1,8 +1,23 @@
 package org.eclipse.qvt.declarative.common.framework.service;
 
-
+/**
+ * An operation represent a call to a provider functionality.
+ * 
+ * @author Quentin Glineur
+ * 
+ */
 public interface Operation {
-	
-	public Object execute (Provider provider) throws Exception;
-	
+
+	/**
+	 * Effectively call the operation by delegating to a given provider.
+	 * 
+	 * @param provider
+	 *            The provider that will perform the action represented by the
+	 *            operation.
+	 * @return Whatever the delegated execution should return.
+	 * @throws Exception
+	 *             Any exception the delegated execution should raise.
+	 */
+	public Object execute(Provider provider) throws Exception;
+
 }
--- plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Service.java
+++ plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Service.java
@@ -10,49 +10,82 @@ import java.util.Map;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 
-public class Service implements Provider {
-		
+/**
+ * Base class for services. A service is a group of providers classed by
+ * priority. It is itself a provider proxy as it delegates the operation to one
+ * or more of its providers according to a strategy.
+ * 
+ * @author Quentin Glineur
+ * 
+ */
+public abstract class Service implements Provider {
+
 	protected final Map<ProviderDescriptor.Priority, ArrayList<Provider>> providers;
-	
-	
-	protected final List<? extends Object> execute(ExecutionStrategy strategy, Operation operation) throws Exception {
+
+	protected final List<? extends Object> execute(ExecutionStrategy strategy,
+			Operation operation) throws Exception {
 		assert null != operation : "null operation"; //$NON-NLS-1$
 		return strategy.execute(this, operation);
 	}
-	
+
+	/**
+	 * Construct and initialize the service base.
+	 */
 	public Service() {
 		providers = new HashMap<ProviderDescriptor.Priority, ArrayList<Provider>>();
-		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority.values()) {
+		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority
+				.values()) {
 			providers.put(providerPriority, new ArrayList<Provider>());
 		}
-		
+
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.qvt.declarative.common.framework.service.Provider#provides(org.eclipse.qvt.declarative.common.framework.service.Operation)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.qvt.declarative.common.framework.service.Provider#provides
+	 * (org.eclipse.qvt.declarative.common.framework.service.Operation)
 	 */
 	@Override
 	public boolean provides(Operation operation) {
 		assert null != operation : "null operation passed to provides(IOperation)"; //$NON-NLS-1$
 
-		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority.values()) {
+		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority
+				.values()) {
 			for (Provider provider : providers.get(providerPriority)) {
 				if (provider.provides(operation)) {
 					return true;
 				}
 			}
 		}
-		
-		
+
 		return false;
 	}
-	
-	public final void registerProviders(String namespace, String extensionPointName) {
-		registerProviders(Platform.getExtensionRegistry()
-									.getExtensionPoint(namespace, extensionPointName)
-									.getConfigurationElements());
+
+	/**
+	 * Registers all providers that are declaring an extension of a particular
+	 * extension point.
+	 * 
+	 * @param namespace
+	 *            The extension point name space.
+	 * @param extensionPointName
+	 *            The extension point name.
+	 */
+	public final void registerProviders(String namespace,
+			String extensionPointName) {
+		registerProviders(Platform.getExtensionRegistry().getExtensionPoint(
+				namespace, extensionPointName).getConfigurationElements());
 	}
 
+	/**
+	 * Registers all providers that are declaring an extension of a particular
+	 * extension point.
+	 * 
+	 * @param elements
+	 *            The <code>IConfigurationElement</code> array from an extension
+	 *            point representing the extenders.
+	 */
 	public final void registerProviders(IConfigurationElement[] elements) {
 		assert null != elements : "null elements"; //$NON-NLS-1$
 
@@ -61,28 +94,34 @@ public class Service implements Provider {
 			ProviderDescriptor descriptor = new ProviderDescriptor(element);
 			addProvider(descriptor.getPriority(), descriptor);
 		}
-		
-		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority.values()) {
+
+		for (ProviderDescriptor.Priority providerPriority : ProviderDescriptor.Priority
+				.values()) {
 			providers.get(providerPriority).trimToSize();
 		}
 	}
-	
-	protected final void addProvider(ProviderDescriptor.Priority priority, ProviderDescriptor provider) {
+
+	protected final void addProvider(ProviderDescriptor.Priority priority,
+			ProviderDescriptor provider) {
 		assert null != priority : "null ProviderPriority"; //$NON-NLS-1$
 		assert null != provider : "null ProviderDescriptor"; //$NON-NLS-1$
-		
+
 		providers.get(priority).add(provider);
 	}
-	
+
 	/**
-	 * Return the first <code>provider</code> with the highest priority able to provides the <code>operation</code> given in parameter
-	 * @param the operation the provider should support
+	 * Return the first <code>provider</code> with the highest priority able to
+	 * provides the <code>operation</code> given in parameter
+	 * 
+	 * @param the
+	 *            operation the provider should support
 	 * @return the provider
 	 */
 	protected Provider getFirstProvider(Operation operation) {
-		List<ProviderDescriptor.Priority> providerPriorities = Arrays.asList(ProviderDescriptor.Priority.values());
+		List<ProviderDescriptor.Priority> providerPriorities = Arrays
+				.asList(ProviderDescriptor.Priority.values());
 		Collections.reverse(providerPriorities);
-		
+
 		for (ProviderDescriptor.Priority providerPriority : providerPriorities) {
 			for (Provider provider : providers.get(providerPriority)) {
 				if (provider.provides(operation)) {
@@ -92,13 +131,15 @@ public class Service implements Provider {
 		}
 		return null;
 	}
-	
-	protected List<Provider> getProvidersByPriorityDescending (Operation operation) {
+
+	protected List<Provider> getProvidersByPriorityDescending(
+			Operation operation) {
 		List<Provider> result = new ArrayList<Provider>();
-		
-		List<ProviderDescriptor.Priority> providerPriorities = Arrays.asList(ProviderDescriptor.Priority.values());
+
+		List<ProviderDescriptor.Priority> providerPriorities = Arrays
+				.asList(ProviderDescriptor.Priority.values());
 		Collections.reverse(providerPriorities);
-		
+
 		for (ProviderDescriptor.Priority providerPriority : providerPriorities) {
 			for (Provider provider : providers.get(providerPriority)) {
 				if (provider.provides(operation)) {
@@ -108,6 +149,5 @@ public class Service implements Provider {
 		}
 		return result;
 	}
-
 
 }
