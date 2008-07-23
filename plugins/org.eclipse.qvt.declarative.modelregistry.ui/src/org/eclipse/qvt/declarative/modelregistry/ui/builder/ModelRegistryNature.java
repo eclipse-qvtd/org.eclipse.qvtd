ff033a14ead1cf2c976fd0c5374c65fd3172628d
qglineur <qglineur>
2008-07-23 10:04:26 +0000

    Initial commit of the UMLX project contribution.

---
+++ plugins/org.eclipse.qvt.declarative.modelregistry.ui/src/org/eclipse/qvt/declarative/modelregistry/ui/builder/ModelRegistryNature.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2007,2008 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.qvt.declarative.modelregistry.ui.builder;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class ModelRegistryNature implements IProjectNature
+{
+	/**
+	 * ID of this project nature
+	 */
+	public static final String NATURE_ID = "org.eclipse.qvt.declarative.modelregistry.ui.ModelRegistryNature";
+	/**
+	 * ID of former UMLX project nature
+	 */
+	@Deprecated
+	public static final String UMLX_NATURE_ID = "org.eclipse.gmt.umlx.registry.editor.ModelRegistryNature";
+
+	private IProject project;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#configure()
+	 */
+	public void configure() throws CoreException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#deconfigure()
+	 */
+	public void deconfigure() throws CoreException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#getProject()
+	 */
+	public IProject getProject() {
+		return project;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core.resources.IProject)
+	 */
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+}
