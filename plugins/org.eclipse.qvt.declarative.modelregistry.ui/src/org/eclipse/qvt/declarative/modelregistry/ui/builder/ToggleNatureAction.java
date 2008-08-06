433ade538ac25b7937fdf75096948d753de0a36e
ewillink <ewillink>
2008-08-06 19:35:47 +0000

    Fix nature upgrade from UMLX to QVT namespaces

--- plugins/org.eclipse.qvt.declarative.modelregistry.ui/src/org/eclipse/qvt/declarative/modelregistry/ui/builder/ToggleNatureAction.java
+++ plugins/org.eclipse.qvt.declarative.modelregistry.ui/src/org/eclipse/qvt/declarative/modelregistry/ui/builder/ToggleNatureAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 E.D.Willink and others.
+ * Copyright (c) 2007,2008 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -86,6 +86,7 @@ public class ToggleNatureAction implements IObjectActionDelegate {
 					// Remove the nature
 					String[] newNatures = new String[natures.length - 1];
 					System.arraycopy(natures, 0, newNatures, 0, i);
+					System.arraycopy(natures, i+1, newNatures, i, newNatures.length - i);
 					natures = newNatures;
 				}
 			}
