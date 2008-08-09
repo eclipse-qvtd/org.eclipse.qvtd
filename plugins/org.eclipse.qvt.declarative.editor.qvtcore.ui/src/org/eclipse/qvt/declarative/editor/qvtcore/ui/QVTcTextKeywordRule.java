08e46fdec835209d96e178b027a3f54c317bb3cb
ewillink <ewillink>
2008-08-09 17:56:11 +0000

    $Id$ in header

--- plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/QVTcTextKeywordRule.java
+++ plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/QVTcTextKeywordRule.java
@@ -1,13 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2007 E.D.Willink and others.
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007,2008 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     E.D.Willink - initial API and implementation
- *******************************************************************************/
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: QVTcTextKeywordRule.java,v 1.2 2008/08/09 17:56:12 ewillink Exp $
+ */
 package org.eclipse.qvt.declarative.editor.qvtcore.ui;
 
 import org.eclipse.jface.text.rules.IToken;
