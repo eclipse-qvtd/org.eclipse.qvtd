4391929a4cde1b9633129403050621351b173bb0
ewillink <ewillink>
2008-08-09 17:48:40 +0000

    Support overlaid file images and type and expression text

--- plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/imp/CommonLabelProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/imp/CommonLabelProvider.java
@@ -12,31 +12,45 @@
  * 
  * </copyright>
  *
- * $Id: CommonLabelProvider.java,v 1.1 2008/08/08 16:42:46 ewillink Exp $
+ * $Id: CommonLabelProvider.java,v 1.2 2008/08/09 17:48:40 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ui.imp;
 
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.provider.ComposedImage;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
 import org.eclipse.imp.editor.ModelTreeNode;
 import org.eclipse.imp.services.ILabelProvider;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.imp.utils.MarkerUtils;
 import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.ocl.cst.OCLExpressionCS;
+import org.eclipse.ocl.cst.PrimitiveTypeCS;
+import org.eclipse.ocl.cst.TypeCS;
 import org.eclipse.qvt.declarative.editor.LabelBehavior;
 import org.eclipse.qvt.declarative.editor.LabelElement;
+import org.eclipse.qvt.declarative.editor.ui.QVTEditorPlugin;
 import org.eclipse.qvt.declarative.parser.utils.ASTandCST;
 import org.eclipse.swt.graphics.Image;
+import org.osgi.framework.Bundle;
 
 /**
  * CommonLabelProvider provides the LabelProvider functionality for
@@ -45,6 +59,9 @@ import org.eclipse.swt.graphics.Image;
 public abstract class CommonLabelProvider implements ILabelProvider
 {
 	private Set<ILabelProviderListener> fListeners = new HashSet<ILabelProviderListener>();
+	protected ExtendedImageRegistry imageRegistry = new ExtendedImageRegistry();
+	private Image errorImageOverlay = null;
+	private Image warningImageOverlay = null;
 
 	public void addListener(ILabelProviderListener listener) {
 		fListeners.add(listener);
@@ -59,12 +76,48 @@ public abstract class CommonLabelProvider implements ILabelProvider
 			throw new ClassCastException("Non EObject");
 		EClass objectEClass = ((EObject)object).eClass();
 		EClass pathEClass = path.getEContainingClass();
-		if (!(object instanceof EObject))
-			throw new IllegalArgumentException("Incomaptible " + objectEClass.getName() + " for " + pathEClass.getName());
+		if (!pathEClass.isSuperTypeOf(objectEClass))
+			throw new IllegalArgumentException("Incompatible " + objectEClass.getName() + " for " + pathEClass.getName());
 		return ((EObject)object).eGet(path);
 	}
 
 	public void dispose() {}
+
+	/**
+	 * Provide a formatted exception arising while identifying the object contributing to the index'th part of node's label.
+	 */
+	protected String formatException(Throwable e, Object node, int index) {
+		return "<" + e.getClass().getSimpleName() + ">";
+	}
+
+	protected String formatExpression(OCLExpressionCS expr) {
+		// FIXME Rest of the expressions
+		return "<" + expr.getClass().getSimpleName() + ">";
+	}
+
+	/**
+	 * Provide a formatted string for object contributing to the index'th part of node's label.
+	 */
+	protected String formatObject(Object object, Object node, int index) {
+		String s = object != null ? object.toString() : null;
+		return s != null ? s : "";
+	}
+
+	/**
+	 * Provide a formatted string for object contributing to the index'th part of node's label.
+	 */
+	protected String formatObjects(Object[] object, Object node, int index) {
+		String s = object != null ? object.toString() : null;
+		return s != null ? s : "";
+	}
+
+	protected String formatType(TypeCS type) {
+		if (type instanceof PrimitiveTypeCS) {
+			return ((PrimitiveTypeCS)type).getValue();
+		}
+		// FIXME Rest of the types
+		return "<" + type.getClass().getSimpleName() + ">";
+	}
 
 	protected Object getASTorCSTNode(Object element) {
 		if (element instanceof ModelTreeNode)
@@ -74,29 +127,68 @@ public abstract class CommonLabelProvider implements ILabelProvider
 		else
 			return element;
 	}
+
+	protected Image getErrorImageOverlay() {
+		if (errorImageOverlay == null)
+			errorImageOverlay = getImage(QVTEditorPlugin.getDefault().getBundle(), "icons/error_ovr.gif");
+		return errorImageOverlay;
+	}
 	
 	public Image getImage(Object element) {
 		Object node = getASTorCSTNode(element);
-		ICommonPlugin plugin = getPlugin();
-		CommonEditorDefinition commonEditorDefinition = plugin.getEditorDefinition();
+		CommonEditorDefinition commonEditorDefinition = getPlugin().getEditorDefinition();
 		LabelBehavior behavior = commonEditorDefinition.getBehavior(node, LabelBehavior.class);
-		if (behavior != null) {
-			String imageFile = behavior.getImage();
-			ImageRegistry imageRegistry = plugin.getImageRegistry();
-			Image image = imageRegistry.get(imageFile);
-			if (image == null) {
-				Path path = new Path(imageFile);
-				URL url = FileLocator.find(plugin.getBundle(), path, null);
-				if (url != null) {
-					ImageDescriptor imageDescriptor = ImageDescriptor.createFromURL(url);
-					imageRegistry.put(imageFile, imageDescriptor);
-					image = imageRegistry.get(imageFile);;
-				}
+		if (behavior == null)
+			return null;
+		Image image = getImage(getPlugin().getBundle(), behavior.getImage());
+		if (image == null)
+			return null;
+		Collection<Image> overlays = getOverlayImages(node);
+		if ((overlays == null) || overlays.isEmpty())
+			return image;
+		List<Object> images = new ArrayList<Object>(overlays.size()+1);
+		images.add(image);
+		images.addAll(overlays);
+		return imageRegistry.getImage(new ComposedImage(images));
+	}
+
+	protected Image getImage(Bundle defaultBundle, String imagePath) {
+		Bundle bundle = defaultBundle;
+		String imageFile = imagePath;
+		if (imagePath.startsWith("/")) {
+			int index = imagePath.indexOf('/', 1);
+			if (index > 1) {
+				String bundlePath = imagePath.substring(1, index);
+				bundle = Platform.getBundle(bundlePath);
+				imageFile = imagePath.substring(index+1);
 			}
-			if (image != null)
-				return image;
+		}
+		Path path = new Path(imageFile);
+		URL url = FileLocator.find(bundle, path, null);
+		if (url == null)
+			return null;
+		Image image = imageRegistry.getImage(url);
+		return image;
+	}
+
+	protected Collection<Image> getOverlayImages(Object node) {
+		if (node instanceof IFile) {
+			IFile file = (IFile) node;
+			int sev = MarkerUtils.getMaxProblemMarkerSeverity(file, IResource.DEPTH_ONE);
+			switch (sev) {
+			case IMarker.SEVERITY_ERROR:
+				return Collections.singletonList(getErrorImageOverlay());
+			case IMarker.SEVERITY_WARNING:
+				return Collections.singletonList(getWarningImageOverlay());
+			}
 		}
 		return null;
+	}
+
+	protected Image getWarningImageOverlay() {
+		if (warningImageOverlay == null)
+			warningImageOverlay = getImage(QVTEditorPlugin.getDefault().getBundle(), "icons/warn_ovr.gif");
+		return warningImageOverlay;
 	}
 
 	protected abstract ICommonPlugin getPlugin();
@@ -119,12 +211,12 @@ public abstract class CommonLabelProvider implements ILabelProvider
 				Object object = node;
 				for (EReference path : labelElement.getPath())
 					object = checkedGet(object, path);
-				if (object instanceof EObject)
+				if (object instanceof EObject) 
 					object = checkedGet(object, labelElement.getEnd());
-				objects[i] = object;
+				objects[i] = formatObject(object, node, i);
 			}
 			catch (Throwable e) {
-				objects[i] = "<" + e.getClass().getSimpleName() + ">";
+				objects[i] = formatException(e, node, i);
 			}
 		}
 	    return MessageFormat.format(format, objects);
