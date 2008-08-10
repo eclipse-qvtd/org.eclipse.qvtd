3acb7da80c7edbe533f0ccb4fc9a832332851958
ewillink <ewillink>
2008-08-10 06:44:17 +0000

    $Id$ in header

--- plugins/org.eclipse.qvt.declarative.editor.qvtrelation.ui/src/org/eclipse/qvt/declarative/editor/qvtrelation/ui/QVTrMarkerProblemHandler.java
+++ plugins/org.eclipse.qvt.declarative.editor.qvtrelation.ui/src/org/eclipse/qvt/declarative/editor/qvtrelation/ui/QVTrMarkerProblemHandler.java
@@ -1,13 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2007 E.D.Willink and others.
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007,2008 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     E.D.Willink - initial API and implementation
- *******************************************************************************/
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: QVTrMarkerProblemHandler.java,v 1.2 2008/08/10 06:44:17 ewillink Exp $
+ */
 package org.eclipse.qvt.declarative.editor.qvtrelation.ui;
 
 import org.eclipse.core.resources.IResource;
