5b1e4154416a324f1dafd2d89a042b1a6b99f937
ewillink <ewillink>
2008-08-14 07:58:36 +0000

    Use patched OCL to workaround bug 243976 and 242153

--- plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcLabelProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcLabelProvider.java
@@ -12,49 +12,18 @@
  * 
  * </copyright>
  *
- * $Id: QVTcLabelProvider.java,v 1.2 2008/08/11 08:04:15 ewillink Exp $
+ * $Id: QVTcLabelProvider.java,v 1.3 2008/08/14 07:58:36 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.qvtcore.ui.imp;
 
-import java.util.List;
-
-import org.eclipse.ocl.cst.OCLExpressionCS;
-import org.eclipse.ocl.cst.TypeCS;
-import org.eclipse.qvt.declarative.editor.LabelElement;
+import org.eclipse.qvt.declarative.editor.ocl.ui.imp.OCLLabelProvider;
 import org.eclipse.qvt.declarative.editor.qvtcore.ui.QVTcPlugin;
-import org.eclipse.qvt.declarative.editor.ui.imp.CommonLabelProvider;
 import org.eclipse.qvt.declarative.editor.ui.imp.ICommonPlugin;
 
-public class QVTcLabelProvider extends CommonLabelProvider
+public class QVTcLabelProvider extends OCLLabelProvider
 {
-	@Override
-	protected String formatObject(Object object, Object node, LabelElement labelElement) {
-		if (object instanceof List) {
-			StringBuffer s = new StringBuffer();
-			for (Object o : (List<?>)object) {
-				if (s.length() > 0)
-					s.append(labelElement.getSeparator());
-				s.append(getText(o));
-			}
-			return s.toString();
-		}
-		else if (object instanceof TypeCS) {
-			return formatType((TypeCS)object);
-		}
-		else if (object instanceof OCLExpressionCS) {
-			return formatExpression((OCLExpressionCS)object);
-		}
-		else
-			return super.formatObject(object, node, labelElement);
-	}
-	
 	@Override
 	protected ICommonPlugin getPlugin() {
 		return QVTcPlugin.getInstance();
-	}
-	
-	public String getText(Object element) {
-		String text = super.getText(element);
-		return (text != null) ? text : "<???>";
 	}
 }
