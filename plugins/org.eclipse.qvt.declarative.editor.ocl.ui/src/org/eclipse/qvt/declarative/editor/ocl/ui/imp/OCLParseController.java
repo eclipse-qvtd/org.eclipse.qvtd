be62b47d4c46e2d15da09c9fa4ce5eb088c44b71
ewillink <ewillink>
2008-08-24 18:58:36 +0000

    Migrate functionality to common class

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
@@ -12,36 +12,20 @@
  * 
  * </copyright>
  *
- * $Id: OCLParseController.java,v 1.2 2008/08/18 07:43:06 ewillink Exp $
+ * $Id: OCLParseController.java,v 1.3 2008/08/24 18:58:36 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.ocl.cst.CSTPackage;
 import org.eclipse.ocl.parser.OCLLexer;
 import org.eclipse.qvt.declarative.editor.ocl.ui.OCLCreationFactory;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonParseController;
-import org.eclipse.qvt.declarative.modelregistry.environment.AbstractFileHandle;
-import org.eclipse.qvt.declarative.parser.ocl.environment.OCLTopLevelEnvironment;
 
 public class OCLParseController extends CommonParseController
 {
 	public OCLParseController() {
 		super(OCLCreationFactory.INSTANCE);
 		CSTPackage.eINSTANCE.getClass();
-	}
-
-	@Override
-	protected OCLTopLevelEnvironment createEnvironment(AbstractFileHandle fileHandle) {
-		ResourceSet resourceSet = new ResourceSetImpl();
-		return new OCLTopLevelEnvironment(fileHandle, resourceSet);
-	}
-
-	@Override
-	public ISourcePositionLocator getNodeLocator() {
-		return new OCLNodeLocator(environment);
 	}
 
 	@Override
