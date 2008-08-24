8d45143498c9c11a37f3d64a8cb2966988e48615
ewillink <ewillink>
2008-08-24 19:11:44 +0000

    AST and CST now separate views

--- plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/actions/CommonActionContributor.java
+++ plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/actions/CommonActionContributor.java
@@ -12,27 +12,18 @@
  * 
  * </copyright>
  *
- * $Id: CommonActionContributor.java,v 1.2 2008/08/18 20:36:56 ewillink Exp $
+ * $Id: CommonActionContributor.java,v 1.3 2008/08/24 19:11:44 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ui.actions;
 
 import java.util.ResourceBundle;
 
-import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.services.base.DefaultLanguageActionsContributor;
-import org.eclipse.jface.action.IAction;
 
 public class CommonActionContributor extends DefaultLanguageActionsContributor
 {
 	public CommonActionContributor() {
 		super();
-	}
-
-	@Override
-	public IAction[] getEditorActions(UniversalEditor editor) {
-		return new IAction[] {
-				new SelectASTorCSTAction(editor)
-		};
 	}
 
 	public static ResourceBundle ResBundle = ResourceBundle.getBundle("org.eclipse.qvt.declarative.editor.ui.actions.ActionMessages");
