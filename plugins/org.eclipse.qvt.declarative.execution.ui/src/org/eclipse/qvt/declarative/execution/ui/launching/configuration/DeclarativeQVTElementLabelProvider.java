209b7943fcb32c5441ed7eef09fdce4af1d7e0be
qglineur <qglineur>
2008-10-09 17:20:59 +0000

    added copyright

--- plugins/org.eclipse.qvt.declarative.execution.ui/src/org/eclipse/qvt/declarative/execution/ui/launching/configuration/DeclarativeQVTElementLabelProvider.java
+++ plugins/org.eclipse.qvt.declarative.execution.ui/src/org/eclipse/qvt/declarative/execution/ui/launching/configuration/DeclarativeQVTElementLabelProvider.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * </copyright>
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ *
+ * $Id: DeclarativeQVTElementLabelProvider.java,v 1.2 2008/10/09 17:20:59 qglineur Exp $
+ */
 package org.eclipse.qvt.declarative.execution.ui.launching.configuration;
 
 import org.eclipse.core.resources.IFile;
