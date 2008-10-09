67f2794a92f13d0847a84d2a18e9654bfc4c8842
qglineur <qglineur>
2008-10-09 17:21:05 +0000

    added copyright

--- plugins/org.eclipse.qvt.declarative.relations.atlvm/src/org/eclipse/qvt/declarative/relations/atlvm/utils/ASMUtils.java
+++ plugins/org.eclipse.qvt.declarative.relations.atlvm/src/org/eclipse/qvt/declarative/relations/atlvm/utils/ASMUtils.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * </copyright>
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ *
+ * $Id: ASMUtils.java,v 1.2 2008/10/09 17:21:06 qglineur Exp $
+ */
 package org.eclipse.qvt.declarative.relations.atlvm.utils;
 
 import java.io.BufferedInputStream;
