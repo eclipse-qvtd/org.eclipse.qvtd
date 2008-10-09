dc9220337ef706bf07509f2b5b6007467223dede
qglineur <qglineur>
2008-10-09 17:21:03 +0000

    added copyright

--- plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/Activator.java
+++ plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/Activator.java
@@ -1,3 +1,20 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * </copyright>
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ *
+ * $Id: Activator.java,v 1.4 2008/10/09 17:21:04 qglineur Exp $
+ */
+
 package org.eclipse.qvt.declarative.common;
 
 import org.eclipse.core.runtime.Plugin;
--- plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Service.java
+++ plugins/org.eclipse.qvt.declarative.common/src/org/eclipse/qvt/declarative/common/framework/service/Service.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * </copyright>
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ *
+ * $Id: Service.java,v 1.8 2008/10/09 17:21:03 qglineur Exp $
+ */
 package org.eclipse.qvt.declarative.common.framework.service;
 
 import java.util.ArrayList;
