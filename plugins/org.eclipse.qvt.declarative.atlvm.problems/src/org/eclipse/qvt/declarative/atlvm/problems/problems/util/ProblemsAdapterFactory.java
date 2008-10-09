1d5b31378b22343c110c978d096fe241ea62ff22
qglineur <qglineur>
2008-10-09 17:21:15 +0000

    added copyright

--- plugins/org.eclipse.qvt.declarative.atlvm.problems/src/org/eclipse/qvt/declarative/atlvm/problems/problems/util/ProblemsAdapterFactory.java
+++ plugins/org.eclipse.qvt.declarative.atlvm.problems/src/org/eclipse/qvt/declarative/atlvm/problems/problems/util/ProblemsAdapterFactory.java
@@ -1,8 +1,18 @@
 /**
  * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ * 
  * </copyright>
  *
- * $Id: ProblemsAdapterFactory.java,v 1.1 2008/06/09 08:52:26 qglineur Exp $
+ * $Id: ProblemsAdapterFactory.java,v 1.2 2008/10/09 17:21:15 qglineur Exp $
  */
 package org.eclipse.qvt.declarative.atlvm.problems.problems.util;
 
