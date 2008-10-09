c419bd4afad71c9aeb9ebcfbbd473b4afaf5bbea
qglineur <qglineur>
2008-10-09 17:21:13 +0000

    added copyright

--- plugins/org.eclipse.qvt.declarative.compilation.ide/src/org/eclipse/qvt/declarative/compilation/ide/EclipseCompilationServiceConfigurator.java
+++ plugins/org.eclipse.qvt.declarative.compilation.ide/src/org/eclipse/qvt/declarative/compilation/ide/EclipseCompilationServiceConfigurator.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * </copyright>
+ * 
+ * Contributors:
+ *     Quentin Glineur - initial API and implementation
+ *
+ * $Id: EclipseCompilationServiceConfigurator.java,v 1.2 2008/10/09 17:21:13 qglineur Exp $
+ */
 package org.eclipse.qvt.declarative.compilation.ide;
 
 import java.util.HashMap;
