ea69f4f7e6a61327b93b8f6a6d81d8f4f4db018e
ewillink <ewillink>
2008-10-10 08:13:52 +0000

    Split off to share multiple usages

---
+++ plugins/org.eclipse.qvt.declarative.parser/src/org/eclipse/qvt/declarative/parser/qvt/environment/LPGProgressMonitor.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2008 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.qvt.declarative.parser.qvt.environment;
+
+import lpg.lpgjavaruntime.Monitor;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class LPGProgressMonitor implements Monitor
+{
+	protected final IProgressMonitor monitor;
+
+	public LPGProgressMonitor(IProgressMonitor monitor) {
+		this.monitor = monitor;
+	}
+
+	public boolean isCancelled() {
+		return (monitor != null) && monitor.isCanceled();
+	}
+}
