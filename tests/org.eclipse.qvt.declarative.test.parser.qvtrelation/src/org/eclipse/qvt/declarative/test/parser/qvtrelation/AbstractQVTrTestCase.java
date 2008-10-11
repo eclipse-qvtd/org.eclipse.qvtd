4c023d7dae0341a988446086fee1db5f7dcea5d2
ewillink <ewillink>
2008-10-11 15:45:59 +0000

    Introduce distinct CSTFile/CSTRoot/CSTChild hierarchies Ensure AST-CST mappings are fully initialised

--- tests/org.eclipse.qvt.declarative.test.parser.qvtrelation/src/org/eclipse/qvt/declarative/test/parser/qvtrelation/AbstractQVTrTestCase.java
+++ tests/org.eclipse.qvt.declarative.test.parser.qvtrelation/src/org/eclipse/qvt/declarative/test/parser/qvtrelation/AbstractQVTrTestCase.java
@@ -20,13 +20,14 @@ import org.eclipse.qvt.declarative.ecore.QVTRelation.QVTRelationPackage;
 import org.eclipse.qvt.declarative.editor.qvtrelation.ui.QVTrCreationFactory;
 import org.eclipse.qvt.declarative.editor.ui.ICreationFactory;
 import org.eclipse.qvt.declarative.modelregistry.standalone.FileHandle;
-import org.eclipse.qvt.declarative.parser.environment.IFileEnvironment;
+import org.eclipse.qvt.declarative.parser.environment.ICSTFileEnvironment;
 import org.eclipse.qvt.declarative.parser.qvtrelation.environment.QVTrFileEnvironment;
+import org.eclipse.qvt.declarative.parser.qvtrelation.environment.QVTrTopLevelEnvironment;
 import org.eclipse.qvt.declarative.test.parser.AbstractParseTestCase;
 
 public abstract class AbstractQVTrTestCase extends AbstractParseTestCase
 {
-	@Override protected IFileEnvironment createEnvironment(String fileName, URI astURI) throws IOException, CoreException {
+	@Override protected ICSTFileEnvironment createEnvironment(String fileName, URI astURI) throws IOException, CoreException {
 		FileHandle fileHandle = getFileHandle(fileName);
 		XMIResource astResource = (XMIResource) resourceSet.createResource(astURI, QVTRelationPackage.eCONTENT_TYPE);
 		QVTrFileEnvironment environment = new QVTrFileEnvironment(fileHandle, resourceSet, astResource)
@@ -36,6 +37,12 @@ public abstract class AbstractQVTrTestCase extends AbstractParseTestCase
 				super.initializePackageNs(ePackage);
 				ePackage.setNsURI(ePackage.getNsURI().replace(".unparsed", ""));
 			}			
+
+			@Override
+			protected void postParse(QVTrTopLevelEnvironment rootEnvironment) {
+				super.postParse(rootEnvironment);
+				rootEnvironment.checkMappings();
+			}
 		};
 		return environment;
 	}
