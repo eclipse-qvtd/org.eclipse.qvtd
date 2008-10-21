07c6c6ea2ff082d0d9c2562abd6687d0d8bf911b
ewillink <ewillink>
2008-10-21 20:07:55 +0000

    Improve Constraint documentation

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLDocumentationProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLDocumentationProvider.java
@@ -12,12 +12,21 @@
  * 
  * </copyright>
  *
- * $Id: OCLDocumentationProvider.java,v 1.1 2008/08/14 07:51:10 ewillink Exp $
+ * $Id: OCLDocumentationProvider.java,v 1.2 2008/10/21 20:07:55 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.qvt.declarative.ecore.utils.EcoreUtils;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonDocumentationProvider;
 
 public class OCLDocumentationProvider extends CommonDocumentationProvider
 {
+	@Override
+	protected String getASTDocumentation(Object node) {
+		if (node instanceof Constraint)
+			return "<" + ((Constraint)node).getStereotype() + "> " + EcoreUtils.formatName(((EObject)node).eClass()) + " : " + EcoreUtils.formatQualifiedName(node);
+		return super.getASTDocumentation(node);
+	}
 }
