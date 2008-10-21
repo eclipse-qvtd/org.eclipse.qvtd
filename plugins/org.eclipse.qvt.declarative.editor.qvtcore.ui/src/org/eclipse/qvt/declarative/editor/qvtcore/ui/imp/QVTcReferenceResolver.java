b66c070c7afed1fdff3d7c9a37bc53adf2ce1f2d
ewillink <ewillink>
2008-10-21 20:07:35 +0000

    Inherited behaviour is adequate

--- plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcReferenceResolver.java
+++ plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcReferenceResolver.java
@@ -12,50 +12,12 @@
  * 
  * </copyright>
  *
- * $Id: QVTcReferenceResolver.java,v 1.2 2008/08/18 07:57:40 ewillink Exp $
+ * $Id: QVTcReferenceResolver.java,v 1.3 2008/10/21 20:07:35 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.qvtcore.ui.imp;
 
-import org.eclipse.imp.parser.IParseController;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonReferenceResolver;
-import org.eclipse.qvt.declarative.parser.qvt.cst.IdentifiedCS;
-import org.eclipse.qvt.declarative.parser.qvt.cst.IdentifierCS;
 
 public class QVTcReferenceResolver extends CommonReferenceResolver
 {
-	/**
-	 * Get the target for a given source node in the AST represented by a
-	 * given Parse Controller.
-	 */
-	@Override
-	public Object getLinkTarget(Object node, IParseController controller) {
-		// START_HERE
-		// Replace the given implementation with an implementation
-		// that is suitable to you language and link types
-
-		// NOTE:  The code shown in this method body works with the
-		// example grammar used in the SAFARI language-service templates.
-		// It may be adaptable for use with other languages.  HOWEVER,
-		// this particular code is not essential to reference resolvers
-		// in general, and the user should provide an implementation
-		// that is appropriate to the language and AST structure for
-		// which the service is being defined.
-
-		if (node instanceof IdentifierCS)
-			return ((IdentifierCS) node).getValue();
-		if (node instanceof IdentifiedCS) {
-			IdentifierCS identifier = ((IdentifiedCS) node).getIdentifier();
-			return identifier != null ? identifier.getValue() : null;
-		}
-		
-//		if (node instanceof Iidentifier && controller.getCurrentAst() != null) {
-//			identifier id = (identifier) node;
-//			Km3Parser parser = (Km3Parser) ((SimpleLPGParseController) controller)
-//					.getParser();
-//			Km3Parser.SymbolTable symtab = parser.getEnclosingSymbolTable(id);
-//			return symtab.findDeclaration(id.toString());
-//		}
-
-		return null;
-	}
 }
