71c6edd1df17d2345c70f391c029ce14a61ce5b7
ewillink <ewillink>
2008-10-24 15:23:16 +0000

    Eliminate workarounds for migrated to OCL via Bug 242236 Add FullOCLCST model to provide TopLevelCS

--- plugins/org.eclipse.qvt.declarative.parser.ocl/src/org/eclipse/qvt/declarative/parser/ocl/OCLFileAnalyzer.java
+++ plugins/org.eclipse.qvt.declarative.parser.ocl/src/org/eclipse/qvt/declarative/parser/ocl/OCLFileAnalyzer.java
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: OCLFileAnalyzer.java,v 1.4 2008/10/15 20:01:53 ewillink Exp $
+ * $Id: OCLFileAnalyzer.java,v 1.5 2008/10/24 15:23:16 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.parser.ocl;
 
@@ -29,31 +29,17 @@ import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.ocl.Environment;
-import org.eclipse.ocl.cst.CSTNode;
-import org.eclipse.ocl.cst.ClassifierContextDeclCS;
-import org.eclipse.ocl.cst.ContextDeclCS;
-import org.eclipse.ocl.cst.InvOrDefCS;
-import org.eclipse.ocl.cst.IterateExpCS;
-import org.eclipse.ocl.cst.IteratorExpCS;
-import org.eclipse.ocl.cst.OperationCS;
-import org.eclipse.ocl.cst.OperationCallExpCS;
 import org.eclipse.ocl.cst.PackageDeclarationCS;
-import org.eclipse.ocl.cst.PrePostOrBodyDeclCS;
-import org.eclipse.ocl.cst.PropertyContextCS;
-import org.eclipse.ocl.cst.VariableExpCS;
+import org.eclipse.ocl.cst.SimpleNameCS;
 import org.eclipse.ocl.ecore.CallOperationAction;
 import org.eclipse.ocl.ecore.Constraint;
 import org.eclipse.ocl.ecore.SendSignalAction;
-import org.eclipse.ocl.expressions.IterateExp;
-import org.eclipse.ocl.expressions.IteratorExp;
-import org.eclipse.ocl.expressions.OCLExpression;
-import org.eclipse.ocl.expressions.Variable;
 import org.eclipse.ocl.parser.OCLAnalyzer;
 import org.eclipse.ocl.parser.OCLLexer;
 import org.eclipse.ocl.parser.OCLParser;
 import org.eclipse.qvt.declarative.parser.environment.ICSTFileAnalyzer;
-import org.eclipse.qvt.declarative.parser.ocl.environment.IOCLEnvironment;
 import org.eclipse.qvt.declarative.parser.ocl.environment.OCLFileEnvironment;
 import org.eclipse.qvt.declarative.parser.ocl.environment.OCLTopLevelEnvironment;
 
@@ -63,6 +49,7 @@ public final class OCLFileAnalyzer extends
 {
 	private Monitor monitor;
 	private OCLTopLevelEnvironment topLevelEnvironment = null;
+	private EClass iteratorsClass = null;
 
 	public OCLFileAnalyzer(OCLFileEnvironment environment, Monitor monitor) {
 		super(new OCLParser(new OCLLexer(environment)));
@@ -72,34 +59,18 @@ public final class OCLFileAnalyzer extends
 	public boolean analyze(OCLTopLevelEnvironment environment) {
 		this.topLevelEnvironment = environment;
 		List<Constraint> constraints = new ArrayList<Constraint>();
-		for (PackageDeclarationCS pkgdecl = environment.getCSTNode(); pkgdecl != null; pkgdecl = pkgdecl.getPackageDeclarationCS())
+		for (PackageDeclarationCS pkgdecl : environment.getCSTNode().getPackages())
 			packageDeclarationCS(pkgdecl, constraints);
 		if (!constraints.isEmpty())
 			environment.getASTNode().getContents().addAll(constraints);
 		return true;
 	}
 
-	protected void checkAndSetAst(CSTNode cstNode, Object astNode) {
-		if ((astNode != null) && (cstNode != null)) {
-			if (cstNode.getAst() == null) {
-//				System.out.println("Missing CST mapping for " + EcoreUtils.formatQualifiedName(cstNode) + " corrected");
-				cstNode.setAst(astNode);
-			}
-		}
-	}	
-
-	@Override protected void contextDeclCS(ContextDeclCS contextDeclCS,
+	@Override
+	protected Object createDummyPackage(
 			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			List<Constraint> constraints) {
-		try {
-//			if (((OCLEnvironment)env).getCSTNode() == null)
-//				env = new OCLNodeEnvironment((OCLEnvironment) env, contextDeclCS);	// Ensure non-null CSTNode for error messages
-			super.contextDeclCS(contextDeclCS, env, constraints);
-		} catch (OCLParsingError e) {
-			ERROR(contextDeclCS, "contextDeclCS", e.getMessage());
-		} catch (Exception e) {
-			ERROR(contextDeclCS, "contextDeclCS", e.toString());
-		}
+			PackageDeclarationCS packageDeclarationCS) {
+		return EcoreFactory.eINSTANCE.createEPackage();
 	}
 
 	@Override
@@ -107,112 +78,23 @@ public final class OCLFileAnalyzer extends
 		return topLevelEnvironment != null ? topLevelEnvironment : super.getOCLEnvironment();
 	}
 
+	@Override
+	protected void resolveIteratorOperation(SimpleNameCS simpleNameCS,
+			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
+		if (iteratorsClass == null) {
+			EPackage iteratorsPackage = EcoreFactory.eINSTANCE.createEPackage();
+			iteratorsPackage.setName("iterators");
+			iteratorsClass = EcoreFactory.eINSTANCE.createEClass();
+			iteratorsClass.setName("Iterators");
+			iteratorsPackage.getEClassifiers().add(iteratorsClass);
+		}
+		EOperation oper = EcoreFactory.eINSTANCE.createEOperation();
+		oper.setName(simpleNameCS.getValue());
+		iteratorsClass.getEOperations().add(oper);
+		simpleNameCS.setAst(oper);
+	}
+
 	protected boolean isCancelled() {
 		return monitor != null ? monitor.isCancelled() : false;
-	}
-
-	@Override
-	protected EClassifier lookupClassifier(CSTNode cstNode,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			List<String> className) {
-		EClassifier eClassifier = super.lookupClassifier(cstNode, env, className);
-		if (eClassifier != null) {
-			if (((IOCLEnvironment)env).getASTNodeToCSTNodeMap().get(eClassifier) == null)
-				((IOCLEnvironment)env).initASTMapping(eClassifier, cstNode);
-				else
-					cstNode.setAst(eClassifier);
-		}
-		return eClassifier;
-	}
-
-	@Override
-	protected Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> classifierContextDeclCS(
-			ClassifierContextDeclCS classifierContextDeclCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			List<Constraint> constraints) {
-		Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> classifierContext = super.classifierContextDeclCS(classifierContextDeclCS, env, constraints);
-		checkAndSetAst(classifierContextDeclCS.getPathNameCS(), classifierContextDeclCS.getAst());
-		return classifierContext;
-	}
-
-	@Override
-	protected Constraint invOrDefCS(InvOrDefCS invOrDefCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		Constraint invOrDef = super.invOrDefCS(invOrDefCS, env);
-		((IOCLEnvironment)env).initASTMapping(invOrDef.getSpecification().getContextVariable(), invOrDefCS.getSimpleNameCS());
-		return invOrDef;
-	}
-
-	@Override
-	protected IterateExp<EClassifier, EParameter> iterateExpCS(IterateExpCS iterateExpCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		IterateExp<EClassifier, EParameter> iterateExp = super.iterateExpCS(iterateExpCS, env);
-		((IOCLEnvironment)env).initASTMapping(iterateExp, iterateExpCS.getSimpleNameCS());
-		return iterateExp;
-	}
-
-	@Override
-	protected IteratorExp<EClassifier, EParameter> iteratorExpCS(IteratorExpCS iteratorExpCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		IteratorExp<EClassifier, EParameter> iteratorExp = super.iteratorExpCS(iteratorExpCS, env);
-		((IOCLEnvironment)env).initASTMapping(iteratorExp, iteratorExpCS.getSimpleNameCS());
-		return iteratorExp;
-	}
-
-	@Override
-	protected Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> operationCS(
-			OperationCS operationCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		// TODO Auto-generated method stub
-		Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> operation = super.operationCS(operationCS, env);
-		checkAndSetAst(operationCS.getPathNameCS(), operationCS.getAst());
-		checkAndSetAst(operationCS.getSimpleNameCS(), operationCS.getAst());
-		checkAndSetAst(operationCS.getTypeCS(), operationCS.getAst());
-		return operation;
-	}
-
-	@Override
-	protected EStructuralFeature propertyContextCS(PropertyContextCS propertyContextCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			List<Constraint> constraints) {
-		EStructuralFeature propertyContext = super.propertyContextCS(propertyContextCS, env, constraints);
-		checkAndSetAst(propertyContextCS.getPathNameCS(), propertyContextCS.getAst());
-		checkAndSetAst(propertyContextCS.getSimpleNameCS(), propertyContextCS.getAst());
-		checkAndSetAst(propertyContextCS.getTypeCS(), propertyContextCS.getAst());
-		return propertyContext;
-	}
-
-	@Override
-	protected OCLExpression<EClassifier> operationCallExpCS(OperationCallExpCS operationCallExpCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		OCLExpression<EClassifier> operationCallExp = super.operationCallExpCS(operationCallExpCS, env);
-		checkAndSetAst(operationCallExpCS.getSimpleNameCS(), operationCallExpCS.getAst());
-		return operationCallExp;
-	}
-
-	@Override
-	protected void packageDeclarationCS(PackageDeclarationCS packageDeclarationCS, List<Constraint> constraints) {
-		super.packageDeclarationCS(packageDeclarationCS, constraints);
-		checkAndSetAst(packageDeclarationCS.getPathNameCS(), packageDeclarationCS.getAst());
-	}
-
-	@Override
-	protected Constraint prePostOrBodyDeclCS(
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			PrePostOrBodyDeclCS prePostOrBodyDeclCS) {
-		Constraint prePostOrBody = super.prePostOrBodyDeclCS(env, prePostOrBodyDeclCS);
-//		checkAndSetAst(prePostOrBodyDeclCS.getSimpleNameCS(), prePostOrBodyDeclCS.getAst());
-		((IOCLEnvironment)env).initASTMapping(prePostOrBody.getSpecification().getContextVariable(), prePostOrBodyDeclCS.getSimpleNameCS());
-		for (Variable<EClassifier, EParameter> variable : prePostOrBody.getSpecification().getParameterVariable())
-			((IOCLEnvironment)env).getASTNodeToCSTNodeMap().put(variable, prePostOrBodyDeclCS);
-		return prePostOrBody;
-	}
-
-	@Override
-	protected OCLExpression<EClassifier> variableExpCS(VariableExpCS variableExpCS,
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		OCLExpression<EClassifier> variableExp = super.variableExpCS(variableExpCS, env);
-		checkAndSetAst(variableExpCS, variableExp);
-		return variableExp;
 	}
 }
