aa9f95087d262130c13907a15f87189b5dd0f4f1
ewillink <ewillink>
2008-12-04 07:51:16 +0000

    Introduce ICommonKeyword to support keywords as identifiers Use OCLBacktrackingLexer

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
@@ -12,34 +12,101 @@
  * 
  * </copyright>
  *
- * $Id: OCLParseController.java,v 1.3 2008/08/24 18:58:36 ewillink Exp $
+ * $Id: OCLParseController.java,v 1.4 2008/12/04 07:51:16 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
 import org.eclipse.ocl.cst.CSTPackage;
-import org.eclipse.ocl.parser.OCLLexer;
+import org.eclipse.ocl.parser.backtracking.OCLBacktrackingLexer;
 import org.eclipse.qvt.declarative.editor.ocl.ui.OCLCreationFactory;
+import org.eclipse.qvt.declarative.editor.ui.imp.CommonKeyword;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonParseController;
+import org.eclipse.qvt.declarative.editor.ui.imp.ICommonKeyword;
 
 public class OCLParseController extends CommonParseController
 {
+	public static final String[] oclIteratorKeywords = new String[] {
+		"iterate",
+		"forAll",
+		"exists",
+		"isUnique",
+		"any",
+		"one",
+		"collect",
+		"select",
+		"reject",
+		"collectNested",
+		"sortedBy",
+		"closure"
+	};
+	
+	public static final String[] oclIdentifierKeywords = new String[] {
+		"allInstances",
+		"oclIsKindOf",
+		"oclIsTypeOf",
+		"oclAsType",
+		"oclIsNew",
+		"oclIsUndefined",
+		"oclIsInvalid",
+		"oclIsInState",
+		
+		"body",
+		"derive",
+		"init",
+		
+		"Set",
+		"Bag",
+		"Sequence",
+		"Collection",
+		"OrderedSet",
+		
+		"String",
+		"Integer",
+		"UnlimitedNatural",
+		"Real",
+		"Boolean",
+		"Tuple",
+		"OclAny",
+		"OclVoid",
+		"Invalid",
+		"OclMessage",
+
+		"OclInvalid"		
+	};
+
+	public static ICommonKeyword createOCLKeyword(String text) {
+		for (String s : oclIteratorKeywords)
+			if (s.equals(text))
+				return new CommonKeyword.Identifier(text);
+		for (String s : oclIdentifierKeywords)
+			if (s.equals(text))
+				return new CommonKeyword.Identifier(text);
+		return null;
+	}
+	
 	public OCLParseController() {
 		super(OCLCreationFactory.INSTANCE);
 		CSTPackage.eINSTANCE.getClass();
 	}
 
 	@Override
+	protected ICommonKeyword createKeyword(String text) {
+		ICommonKeyword keyword = createOCLKeyword(text);
+		return keyword != null ? keyword : super.createKeyword(text);
+	}
+
+	@Override
 	public TokenKind getTokenKind(int kind) {
 		switch (kind) {
-		case OCLLexer.TK_EOF_TOKEN:
+		case OCLBacktrackingLexer.TK_EOF_TOKEN:
 			return TokenKind.EOF;
-		case OCLLexer.TK_IDENTIFIER:
+		case OCLBacktrackingLexer.TK_IDENTIFIER:
 			return TokenKind.IDENTIFIER;
-		case OCLLexer.TK_INTEGER_LITERAL:
+		case OCLBacktrackingLexer.TK_INTEGER_LITERAL:
 			return TokenKind.INTEGER;
-		case OCLLexer.TK_REAL_LITERAL:
+		case OCLBacktrackingLexer.TK_REAL_LITERAL:
 			return TokenKind.REAL;
-        case OCLLexer.TK_STRING_LITERAL:
+        case OCLBacktrackingLexer.TK_STRING_LITERAL:
              return TokenKind.STRING;
 		default:
 			if (isKeyword(kind))
