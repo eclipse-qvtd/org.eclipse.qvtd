315ca53c6a6501a3b6f3a6dcf91d32e3b46ef059
ewillink <ewillink>
2008-12-29 17:01:02 +0000

    Use single special underscore variable per relation

---
+++ plugins/org.eclipse.qvt.declarative.ecore.qvttemplate/src/org/eclipse/qvt/declarative/ecore/QVTTemplate/util/QVTTemplateConstants.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2008 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: QVTTemplateConstants.java,v 1.1 2008/12/29 17:01:02 ewillink Exp $
+ */
+package org.eclipse.qvt.declarative.ecore.QVTTemplate.util;
+
+public interface QVTTemplateConstants
+{
+	public static final String WILDCARD_VARIABLE_NAME = "_";
+}
