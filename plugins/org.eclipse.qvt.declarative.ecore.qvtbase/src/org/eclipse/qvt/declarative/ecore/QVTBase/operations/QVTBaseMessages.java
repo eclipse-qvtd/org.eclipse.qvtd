f1ea7ca86f4879fb53cecc1d571900f298e59eb0
ewillink <ewillink>
2008-12-31 17:41:32 +0000

    Use NLS and Message classes

---
+++ plugins/org.eclipse.qvt.declarative.ecore.qvtbase/src/org/eclipse/qvt/declarative/ecore/QVTBase/operations/QVTBaseMessages.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2008 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: QVTBaseMessages.java,v 1.1 2008/12/31 17:41:32 ewillink Exp $
+ */
+package org.eclipse.qvt.declarative.ecore.QVTBase.operations;
+import org.eclipse.osgi.util.NLS;
+
+public class QVTBaseMessages extends NLS
+{
+	public static String _UI_Domain_IsNotCheckableOrEnforceable;
+	public static String _UI_Domain_TypedModelDoesNotExistWarning;
+	public static String _UI_Domain_TypedModelIsNotDefinedByTransformation;
+	public static String _UI_Function_MayHaveASideEffect;
+	public static String _UI_Function_ParameterIsNotAFunctionParameter;
+	public static String _UI_Pattern_VariableIsNotAFunctionParameter;
+	public static String _UI_Predicate_ConditionExpressionIsNotBoolean;
+	public static String _UI_Rule_DomainNameIsNotUnique;
+	public static String _UI_Rule_OverridenDomainCountIsNotCompatible;
+	public static String _UI_Rule_OverridenDomainDoesNotExist;
+	public static String _UI_Rule_OverridenDomainPackagesAreNotCompatible;
+	public static String _UI_Rule_OverridesIsNotDefinedByTransformation;
+	public static String _UI_Rule_TypedModelIsNotUnique;
+	public static String _UI_Transformation_ModelParameterUsedPackagesAreNotCompatibleWithExtension;
+	public static String _UI_Transformation_ExtendsContainsACycle;
+	public static String _UI_Transformation_UsedPackagesAreNotCompatibleWithExtends;
+	public static String _UI_Transformation_ModelParameterNameIsNotCompatibleWithExtension;
+	public static String _UI_Transformation_ModelParameterNameIsNotUnique;
+	public static String _UI_Transformation_RuleNameIsNotUnique;	
+	public static String _UI_Transformation_SynthesizedTypeIsNotOwned;
+	public static String _UI_Transformation_SynthesizedTypeIsNotUnique;
+	public static String _UI_TypedModel_DependsOnContainsACycle;
+
+	static {
+		NLS.initializeMessages(QVTBaseMessages.class.getName(), QVTBaseMessages.class);
+	}
+}
