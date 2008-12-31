dba32a3472289788415e226c4cf938c4931af19c
ewillink <ewillink>
2008-12-31 17:50:47 +0000

    Provide TemplateExp names

---
+++ plugins/org.eclipse.qvt.declarative.parser.qvtrelation/src/org/eclipse/qvt/declarative/parser/qvtrelation/QVTrFormattingHelper.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2008 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.qvt.declarative.parser.qvtrelation;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.qvt.declarative.ecore.QVTTemplate.CollectionTemplateExp;
+import org.eclipse.qvt.declarative.ecore.QVTTemplate.ObjectTemplateExp;
+import org.eclipse.qvt.declarative.ecore.QVTTemplate.PropertyTemplateItem;
+import org.eclipse.qvt.declarative.ecore.QVTTemplate.TemplateExp;
+import org.eclipse.qvt.declarative.parser.qvt.environment.QVTFormattingHelper;
+import org.eclipse.qvt.declarative.parser.qvtrelation.environment.IQVTrNodeEnvironment;
+
+public class QVTrFormattingHelper extends QVTFormattingHelper
+{	
+	public QVTrFormattingHelper(IQVTrNodeEnvironment environment) {
+		super(environment);
+	}
+
+	@Override
+	public String formatQualifiedName(Object object) {
+		if (object instanceof TemplateExp)
+            return formatTemplateExpName((TemplateExp)object, new StringBuffer());
+		else
+			return super.formatQualifiedName(object);
+	}
+
+	public String formatTemplateExpName(EObject object, StringBuffer s) {
+		EObject container = object.eContainer();
+		if ((container instanceof TemplateExp) || (container instanceof PropertyTemplateItem))
+			formatTemplateExpName(container, s);
+		else if (object instanceof ObjectTemplateExp) {
+			s.append(((ObjectTemplateExp)object).getReferredClass().getName());
+		}
+		else if (object instanceof CollectionTemplateExp) {
+			s.append(((CollectionTemplateExp)object).getReferredCollectionType().getName());
+		}
+		if (object instanceof PropertyTemplateItem) {
+			s.append(((PropertyTemplateItem)object).getReferredProperty().getName());
+		}
+		else if (object instanceof ObjectTemplateExp) {
+			s.append(".");
+		}
+		else if (object instanceof CollectionTemplateExp) {
+			s.append("[]");
+		}
+		else {
+			s.append(formatQualifiedName(object));
+		}
+		return s.toString();
+	}
+}
