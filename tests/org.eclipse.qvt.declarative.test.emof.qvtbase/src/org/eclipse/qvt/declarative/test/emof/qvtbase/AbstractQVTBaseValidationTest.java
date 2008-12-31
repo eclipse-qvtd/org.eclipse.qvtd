c19c4f91fe5147624101408726d39f0c9afff89c
ewillink <ewillink>
2008-12-31 18:00:41 +0000

    Support modularised validation tests

--- tests/org.eclipse.qvt.declarative.test.emof.qvtbase/src/org/eclipse/qvt/declarative/test/emof/qvtbase/AbstractQVTBaseValidationTest.java
+++ tests/org.eclipse.qvt.declarative.test.emof.qvtbase/src/org/eclipse/qvt/declarative/test/emof/qvtbase/AbstractQVTBaseValidationTest.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.qvt.declarative.test.emof.qvtbase;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -18,107 +17,44 @@ import java.util.Map;
 import java.util.Set;
 
 import junit.framework.AssertionFailedError;
-import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.DiagnosticChain;
-import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.EValidator;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.ocl.ecore.OCLExpression;
-import org.eclipse.ocl.ecore.StringLiteralExp;
-import org.eclipse.ocl.ecore.Variable;
-import org.eclipse.ocl.ecore.VariableExp;
-import org.eclipse.ocl.internal.l10n.OCLMessages;
-import org.eclipse.ocl.lpg.AbstractParser;
 import org.eclipse.ocl.lpg.AbstractProblemHandler;
 import org.eclipse.ocl.lpg.ProblemHandler;
 import org.eclipse.ocl.lpg.StringProblemHandler;
-import org.eclipse.qvt.declarative.ecore.QVTBase.Domain;
 import org.eclipse.qvt.declarative.ecore.QVTBase.Function;
 import org.eclipse.qvt.declarative.ecore.QVTBase.FunctionParameter;
 import org.eclipse.qvt.declarative.ecore.QVTBase.Pattern;
 import org.eclipse.qvt.declarative.ecore.QVTBase.Predicate;
 import org.eclipse.qvt.declarative.ecore.QVTBase.QVTBaseFactory;
 import org.eclipse.qvt.declarative.ecore.QVTBase.QVTBasePackage;
-import org.eclipse.qvt.declarative.ecore.QVTBase.Rule;
 import org.eclipse.qvt.declarative.ecore.QVTBase.Transformation;
 import org.eclipse.qvt.declarative.ecore.QVTBase.TypedModel;
-import org.eclipse.qvt.declarative.ecore.QVTBase.impl.DomainImpl;
-import org.eclipse.qvt.declarative.ecore.QVTBase.impl.RuleImpl;
-import org.eclipse.qvt.declarative.ecore.QVTBase.util.QVTBaseValidator;
-import org.eclipse.qvt.declarative.ecore.QVTRelation.QVTRelationFactory;
-import org.eclipse.qvt.declarative.ecore.QVTRelation.Relation;
-import org.eclipse.qvt.declarative.ecore.operations.EValidatorWithOperations;
 import org.eclipse.qvt.declarative.parser.environment.ProblemHandlerDiagnosticChain;
 import org.eclipse.qvt.declarative.parser.utils.ProblemLog;
 import org.eclipse.qvt.declarative.parser.utils.ProblemLog.Problem;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestExpression;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestQVTBaseFactory;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestDomain;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestPattern;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestRule;
+import org.eclipse.qvt.declarative.test.TestQVTBase.TestTransformation;
+import org.eclipse.qvt.declarative.test.emof.essentialocl.AbstractEssentialOCLValidationTest;
 import org.eclipse.qvt.declarative.test.parser.TestFormattingHelper;
-import org.eclipse.qvt.declarative.test.parser.AbstractParseTestCase.ProblemComparator;
 
-public abstract class AbstractValidationTest extends TestCase
+public abstract class AbstractQVTBaseValidationTest extends AbstractEssentialOCLValidationTest
 {
 	public static final QVTBaseFactory QVTb_Factory = QVTBaseFactory.eINSTANCE;
-	public static final org.eclipse.emf.ecore.EcoreFactory Ecore_Factory = org.eclipse.emf.ecore.EcoreFactory.eINSTANCE;
-	public static final org.eclipse.ocl.ecore.EcoreFactory OCL_Factory = org.eclipse.ocl.ecore.EcoreFactory.eINSTANCE;
-	
-	public static class ExpectedProblemLog extends ProblemLog
-	{
-		public void expectValidatorError(String key, String[] substitutions) {
-			EValidatorWithOperations validator = QVTBaseValidator.INSTANCE;
-			ResourceLocator resourceLocator = validator.getResourceLocator();
-			String problemMessage = substitutions == null ? resourceLocator.getString(key) : resourceLocator.getString(key, substitutions);
-			String processingContext = validator.getDiagnosticSource();
-			super.handleProblem(ProblemHandler.Severity.ERROR, ProblemHandler.Phase.VALIDATOR, problemMessage,
-					processingContext, -1, -1);
-		}
-		public void expectValidatorWarning(String key, String[] substitutions) {
-			EValidatorWithOperations validator = QVTBaseValidator.INSTANCE;
-			ResourceLocator resourceLocator = validator.getResourceLocator();
-			String problemMessage = substitutions == null ? resourceLocator.getString(key) : resourceLocator.getString(key, substitutions);
-			String processingContext = validator.getDiagnosticSource();
-			super.handleProblem(ProblemHandler.Severity.WARNING, ProblemHandler.Phase.VALIDATOR, problemMessage,
-					processingContext, -1, -1);
-		}
-	}
-
-	private final class TestProblemHandler extends StringProblemHandler
-	{
-		private TestProblemHandler() {
-			super(null);
-		}
-
-		/**
-		 * Implements the interface, invoking <code>addProblem</code> with a line comprising
-		 * <code>processingPhase-problemSeverity in processingContext; lineNumber : problemMessage</code>.
-		 */
-		@Override
-		public void handleProblem(Severity problemSeverity, Phase processingPhase,
-				String problemMessage, String processingContext, int startOffset, int endOffset) {
-			AbstractParser parser = getParser();
-			String file = parser.getFileName();
-			int lineNumber = parser.getTokenAtCharacter(startOffset).getLine();
-			String message = OCLMessages.bind(
-					 "{0}-{1} in {2}; {3}:{4} : {5}", //OCLMessages.ProblemMessage_ERROR_,
-					new Object[] {
-						processingPhase != null ? processingPhase : "?", //$NON-NLS-1$
-						problemSeverity != null ? problemSeverity : "?", //$NON-NLS-1$
-						processingContext != null ? processingContext : "?", //$NON-NLS-1$
-						file, lineNumber,
-						problemMessage != null ? problemMessage : "?"}); //$NON-NLS-1$
-			addProblem(message);
-		}
-	}
 
 	/**
 	 * Asserts that two objects are equal. If they are not
@@ -145,8 +81,8 @@ public abstract class AbstractValidationTest extends TestCase
 				for (ProblemHandler.Severity severity : actualSeverities) {
 					List<Problem> actualProblems = actualPhaseProblems.get(severity);
 					List<Problem> expectedProblems = expectedPhaseProblems.get(severity);
-					Collections.sort(actualProblems, ProblemComparator.INSTANCE);
-					Collections.sort(expectedProblems, ProblemComparator.INSTANCE);
+					Collections.sort(actualProblems);
+					Collections.sort(expectedProblems);
 					int actualProblemCount = actualProblems.size();
 					int expectedProblemCount = expectedProblems.size();
 					assertEquals("Problem count", expectedProblemCount, actualProblemCount);
@@ -176,34 +112,18 @@ public abstract class AbstractValidationTest extends TestCase
 	protected boolean copyErrorToConsole = false;	// True to put error message on console as well as JUnit failure
 	protected ResourceSet resourceSet;
 	protected Resource resource;
-
-	protected Domain createConcreteDomain(Rule rule, String name) {
-		Domain domain = new DomainImpl()
+	
+	protected Diagnostician createDiagnostician() {
+		return new Diagnostician()
 		{
+			@Override
+			protected boolean doValidateContents(EObject object, DiagnosticChain diagnostics, Map<Object, Object> context) {
+				if (object.eClass().getEPackage() == org.eclipse.ocl.ecore.EcorePackage.eINSTANCE)
+					return true;		// OCL's ValidatorVisitor does its own child traversal
+				else
+					return super.doValidateContents(object, diagnostics, context);
+			}
 		};
-		domain.setName(name);
-		if (rule != null)
-			rule.getDomain().add(domain);
-		return domain;
-	}
-
-	protected Rule createConcreteRule(Transformation transformation, String name) {
-		Rule rule = new RuleImpl()
-		{
-		};
-		rule.setName(name);
-		if (transformation != null)
-			transformation.getRule().add(rule);
-		return rule;
-	}
-
-	protected EParameter createEParameter(EOperation eOperation, String name, EClassifier eType) {
-		EParameter eParameter = Ecore_Factory.createEParameter();
-		eParameter.setName(name);
-		eParameter.setEType(eType);
-		if (eOperation != null)
-			eOperation.getEParameters().add(eParameter);
-		return eParameter;
 	}
 
 	protected Function createFunction(Transformation transformation, String name) {
@@ -237,19 +157,45 @@ public abstract class AbstractValidationTest extends TestCase
 		return predicate;
 	}
 
-	protected Relation createRelation(Transformation transformation, String name) {
-		Relation relation = QVTRelationFactory.eINSTANCE.createRelation();
-		relation.setName(name);
-		if (transformation != null)
-			transformation.getRule().add(relation);
-		return relation;
+	protected TestDomain createTestDomain(List<? super TestDomain> contents, String name, TypedModel typedModel) {
+		TestDomain domain = TestQVTBaseFactory.eINSTANCE.createTestDomain();
+		domain.setName(name);
+		if (typedModel != null)
+			domain.setTypedModel(typedModel);
+		if (contents != null)
+			contents.add(domain);
+		domain.setIsCheckable(true);
+		domain.setIsEnforceable(true);
+		return domain;
 	}
 
-	protected StringLiteralExp createStringLiteralExp(String string) {
-		StringLiteralExp expr = OCL_Factory.createStringLiteralExp();
-		expr.setStringSymbol(string);
-		expr.setEType(EcorePackage.Literals.ESTRING);
-		return expr;
+	protected TestExpression createTestExpression() {
+		TestExpression expression = TestQVTBaseFactory.eINSTANCE.createTestExpression();
+		expression.setType(getStringType());
+		return expression;
+	}
+
+	protected TestPattern createTestPattern(List<? super TestPattern> contents) {
+		TestPattern pattern = TestQVTBaseFactory.eINSTANCE.createTestPattern();
+		if (contents != null)
+			contents.add(pattern);
+		return pattern;
+	}
+
+	protected TestRule createTestRule(List<? super TestRule> contents, String name) {
+		TestRule rule = TestQVTBaseFactory.eINSTANCE.createTestRule();
+		rule.setName(name);
+		if (contents != null)
+			contents.add(rule);
+		return rule;
+	}
+
+	protected TestTransformation createTestTransformation(List<? super TestTransformation> contents, String name) {
+		TestTransformation transformation = TestQVTBaseFactory.eINSTANCE.createTestTransformation();
+		transformation.setName(name);
+		if (contents != null)
+			contents.add(transformation);
+		return transformation;
 	}
 
 	protected Transformation createTransformation(Resource resource, String name) {
@@ -260,35 +206,15 @@ public abstract class AbstractValidationTest extends TestCase
 		return transformation;
 	}
 
-	protected TypedModel createTypedModel(Transformation transformation, String name, Collection<? extends EPackage> usedPackages) {
+	protected TypedModel createTypedModel(Transformation transformation, String name, EPackage... usedPackages) {
 		TypedModel typedModel = QVTb_Factory.createTypedModel();
 		typedModel.setName(name);
 		if (usedPackages != null)
-			typedModel.getUsedPackage().addAll(usedPackages);
+			for (EPackage usedPackage : usedPackages)
+				typedModel.getUsedPackage().add(usedPackage);
 		if (transformation != null)
 			transformation.getModelParameter().add(typedModel);
 		return typedModel;
-	}
-
-	protected Variable createVariable(List<? super Variable> variables, String name, EClassifier eType) {
-		Variable variable = OCL_Factory.createVariable();
-		variable.setName(name);
-		variable.setEType(eType);
-		if (variables != null)
-			variables.add(variable);
-		return variable;
-	}
-
-	protected OCLExpression createVariableExp(Variable variable) {
-		VariableExp variableExp = OCL_Factory.createVariableExp();
-		variableExp.setReferredVariable(variable);
-		variableExp.setName(variable.getName());
-		variableExp.setEType(variable.getEType());
-		return variableExp;
-	}
-
-	protected EClassifier getBooleanType() {
-		return EcorePackage.Literals.EBOOLEAN;
 	}
 
 	protected String getQualifiedNameOf(Object object) {
@@ -297,7 +223,6 @@ public abstract class AbstractValidationTest extends TestCase
 	
 	@Override
 	protected void setUp() throws Exception {
-		// TODO Auto-generated method stub
 		super.setUp();
 		resourceSet = new ResourceSetImpl();
 		resourceSet.getResourceFactoryRegistry().getContentTypeToFactoryMap().put(QVTBasePackage.eCONTENT_TYPE, new XMIResourceFactoryImpl());
@@ -305,11 +230,7 @@ public abstract class AbstractValidationTest extends TestCase
 	}
 	
 	protected void validationTest(ProblemLog expectedProblems) {
-		AbstractProblemHandler actualProblems;
-		if (expectedProblems != null)
-			actualProblems = new ProblemLog();
-		else
-			actualProblems = new TestProblemHandler();
+		AbstractProblemHandler actualProblems = new ProblemLog();
 		DiagnosticChain diagnosticChain = new ProblemHandlerDiagnosticChain(actualProblems);
 		Diagnostician diagnostician = createDiagnostician();
 	    Map<Object, Object> context = new HashMap<Object, Object>();
@@ -334,18 +255,5 @@ public abstract class AbstractValidationTest extends TestCase
 				fail(s.toString());
 			}
 		}
-	}
-	
-	protected Diagnostician createDiagnostician() {
-		return new Diagnostician()
-		{
-			@Override
-			protected boolean doValidateContents(EObject object, DiagnosticChain diagnostics, Map<Object, Object> context) {
-				if (object.eClass().getEPackage() == org.eclipse.ocl.ecore.EcorePackage.eINSTANCE)
-					return true;		// OCL's ValidatorVisitor does its own child traversal
-				else
-					return super.doValidateContents(object, diagnostics, context);
-			}
-		};
 	}
 }
