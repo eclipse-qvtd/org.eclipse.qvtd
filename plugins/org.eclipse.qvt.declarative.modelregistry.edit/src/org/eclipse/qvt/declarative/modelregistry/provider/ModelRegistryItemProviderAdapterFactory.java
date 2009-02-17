ce77568b0e6bd1fff8ac95c5088ea57601e3f3e6
ewillink <ewillink>
2009-02-17 21:16:00 +0000

    Fix instanceof generic warnings

--- plugins/org.eclipse.qvt.declarative.modelregistry.edit/src/org/eclipse/qvt/declarative/modelregistry/provider/ModelRegistryItemProviderAdapterFactory.java
+++ plugins/org.eclipse.qvt.declarative.modelregistry.edit/src/org/eclipse/qvt/declarative/modelregistry/provider/ModelRegistryItemProviderAdapterFactory.java
@@ -196,7 +196,7 @@ public class ModelRegistryItemProviderAdapterFactory extends ModelRegistryAdapte
 	public Object adapt(Object object, Object type) {
 		if (isFactoryForType(type)) {
 			Object adapter = super.adapt(object, type);
-			if (!(type instanceof Class) || (((Class<?>)type).isInstance(adapter))) {
+			if (!(type instanceof Class<?>) || (((Class<?>)type).isInstance(adapter))) {
 				return adapter;
 			}
 		}
