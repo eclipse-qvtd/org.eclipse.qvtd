6a5a2878890447190946f20b5e8e00a5d2c2ca58
ewillink <ewillink>
2009-02-17 21:38:50 +0000

    Regenerate to fix instanceof generic warnings

--- plugins/org.eclipse.qvt.declarative.ecore.qvtcore.edit/src/org/eclipse/qvt/declarative/ecore/QVTCore/provider/QVTCoreItemProviderAdapterFactory.java
+++ plugins/org.eclipse.qvt.declarative.ecore.qvtcore.edit/src/org/eclipse/qvt/declarative/ecore/QVTCore/provider/QVTCoreItemProviderAdapterFactory.java
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: QVTCoreItemProviderAdapterFactory.java,v 1.1 2008/07/23 09:44:16 qglineur Exp $
+ * $Id: QVTCoreItemProviderAdapterFactory.java,v 1.2 2009/02/17 21:38:50 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.ecore.QVTCore.provider;
 
@@ -342,7 +342,7 @@ public class QVTCoreItemProviderAdapterFactory extends QVTCoreAdapterFactory imp
 	public Object adapt(Object object, Object type) {
 		if (isFactoryForType(type)) {
 			Object adapter = super.adapt(object, type);
-			if (!(type instanceof Class) || (((Class<?>)type).isInstance(adapter))) {
+			if (!(type instanceof Class<?>) || (((Class<?>)type).isInstance(adapter))) {
 				return adapter;
 			}
 		}
