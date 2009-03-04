3b73d1f9b38c01c6099a00ca3c26d1ef1691b07f
ewillink <ewillink>
2009-03-04 19:12:05 +0000

    Eliminate ocl.ecore.edit icons

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLImageProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLImageProvider.java
@@ -1,47 +1,39 @@
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
-import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.ocl.cst.PrePostOrBodyDeclCS;
 import org.eclipse.ocl.cst.PrePostOrBodyEnum;
 import org.eclipse.ocl.ecore.Constraint;
-import org.eclipse.ocl.ecore.OperationCallExp;
-import org.eclipse.ocl.utilities.PredefinedType;
 import org.eclipse.ocl.utilities.UMLReflection;
 import org.eclipse.qvt.declarative.editor.util.ImageProvider;
 
 public class OCLImageProvider implements ImageProvider {
+
+	public static final String EXTRA_OCL_ICONS_PATH = "/org.eclipse.qvt.declarative.editor.ocl.ui/icons/full/obj16/";
 
 	public String getImage(Object object) {
 		if (object instanceof Constraint) {
 			Constraint constraint = (Constraint) object;
 			String stereotype = constraint.getStereotype();
 			if (UMLReflection.INVARIANT.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/InvariantConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "InvariantConstraint.gif";
 			else if (UMLReflection.POSTCONDITION.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/PostconditionConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "PostconditionConstraint.gif";
 			else if (UMLReflection.PRECONDITION.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/PreconditionConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "PreconditionConstraint.gif";
 			else if (UMLReflection.DEFINITION.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/DefinitionConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "DefinitionConstraint.gif";
 			else if (UMLReflection.DERIVATION.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/DerivationConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "DerivationConstraint.gif";
 			else if (UMLReflection.INITIAL.equals(stereotype))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/InitialConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "InitialConstraint.gif";
 		}
 		else if (object instanceof PrePostOrBodyDeclCS) {
 			PrePostOrBodyDeclCS prePostOrBodyDeclCS = (PrePostOrBodyDeclCS) object;
 			PrePostOrBodyEnum kind = prePostOrBodyDeclCS.getKind();
 			if (kind == PrePostOrBodyEnum.PRE_LITERAL)
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/PreconditionConstraint.gif";
+				return EXTRA_OCL_ICONS_PATH + "PreconditionConstraint.gif";
 			else if (kind == PrePostOrBodyEnum.POST_LITERAL)
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/PostconditionConstraint.gif";
-		}
-		else if (object instanceof OperationCallExp) {
-			OperationCallExp operationCall = (OperationCallExp) object;
-			EOperation operation = operationCall.getReferredOperation();
-			String name = operation != null ? operation.getName() : null;
-			if (PredefinedType.PLUS_NAME.equals(name))
-				return "/org.eclipse.ocl.ecore.edit/icons/full/obj16/PlusOperationCallExp.gif";
+				return EXTRA_OCL_ICONS_PATH + "PostconditionConstraint.gif";
 		}
 		return null;
 	}
