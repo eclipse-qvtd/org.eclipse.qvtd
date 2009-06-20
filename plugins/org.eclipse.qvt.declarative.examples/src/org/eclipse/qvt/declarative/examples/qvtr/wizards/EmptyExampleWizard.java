7556d9a33c7b9429d2978b33635b00b9ac2eb4f8
ewillink <ewillink>
2009-06-20 09:15:23 +0000

    Facilitate creation of empty projects : Bug 280967

---
+++ plugins/org.eclipse.qvt.declarative.examples/src/org/eclipse/qvt/declarative/examples/qvtr/wizards/EmptyExampleWizard.java
@@ -0,0 +1,33 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2009 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: EmptyExampleWizard.java,v 1.1 2009/06/20 09:15:23 ewillink Exp $
+ */
+package org.eclipse.qvt.declarative.examples.qvtr.wizards;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.qvt.declarative.examples.wizards.AbstractExampleWizard;
+
+public class EmptyExampleWizard extends AbstractExampleWizard
+{	
+	@Override
+	protected Collection<ProjectDescriptor> getProjectDescriptors() {
+		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(4);
+		projects.add(new ProjectDescriptor("org.eclipse.qvt.declarative.examples", "zips/qvtr/empty.zip", "Empty QVTr Project"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$		
+		return projects;
+	}
+}
