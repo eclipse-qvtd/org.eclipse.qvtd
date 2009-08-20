90cf3002e95a81d0aa929056c45e05be7720b0a6
ewillink <ewillink>
2009-08-20 20:17:18 +0000

    Introduce ICommonParseResult

--- plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/imp/CommonDocumentationProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.ui/src/org/eclipse/qvt/declarative/editor/ui/imp/CommonDocumentationProvider.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2008 E.D.Willink and others.
+ * Copyright (c) 2008,2009 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: CommonDocumentationProvider.java,v 1.4 2009/02/17 21:48:51 ewillink Exp $
+ * $Id: CommonDocumentationProvider.java,v 1.5 2009/08/20 20:17:18 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ui.imp;
 
@@ -26,12 +26,8 @@ import org.eclipse.qvt.declarative.ecore.utils.EcoreUtils;
 
 public abstract class CommonDocumentationProvider implements IDocumentationProvider, ILanguageService
 {
-	public static String getSubstring(CommonParseController parseController, int start, int end) {
-		return new String(parseController.getLexer().getInputChars(), start, end - start + 1);
-	}
-
-	public static String getSubstring(CommonParseController parseController, CSTNode cstNode) {
-		return getSubstring(parseController, cstNode.getStartOffset(), cstNode.getEndOffset());
+	public static String getSubstring(IParseResult parseResult, CSTNode cstNode) {
+		return parseResult.getTextRange(cstNode.getStartOffset(), cstNode.getEndOffset() - cstNode.getStartOffset() + 1);
 	}
 
 	protected String getASTDocumentation(Object node) {
@@ -44,23 +40,23 @@ public abstract class CommonDocumentationProvider implements IDocumentationProvi
 		return null;
 	}
 
-	protected String getCSTDocumentation(CSTNode cstNode, CommonParseController parseController) {
-		CommonParseController.TokenKind tokenKind = parseController.getTokenKind(getTokenKindForNode(cstNode));
+	protected String getCSTDocumentation(CSTNode cstNode, ICommonParseResult parseResult) {
+		ICommonParseController.TokenKind tokenKind = parseResult.getParseController().getTokenKind(getTokenKindForNode(cstNode));
 		switch (tokenKind) {
 		case IDENTIFIER:
-			return "'" + getSubstring(parseController, cstNode) + "' is an identifier";
+			return "'" + getSubstring(parseResult, cstNode) + "' is an identifier";
 		case INTEGER:
-			return "'" + getSubstring(parseController, cstNode) + "' is an integer number";
+			return "'" + getSubstring(parseResult, cstNode) + "' is an integer number";
 		case REAL:
-			return "'" + getSubstring(parseController, cstNode) + "' is a floating point number";
+			return "'" + getSubstring(parseResult, cstNode) + "' is a floating point number";
 		case KEYWORD:
-			return "'" + getSubstring(parseController, cstNode) + "' is a keyword";
+			return "'" + getSubstring(parseResult, cstNode) + "' is a keyword";
 		case LINE_COMMENT:
-			return "'" + getSubstring(parseController, cstNode) + "' is a line comment";
+			return "'" + getSubstring(parseResult, cstNode) + "' is a line comment";
 		case PARAGRAPH_COMMENT:
-			return "'" + getSubstring(parseController, cstNode) + "' is a paragraph comment";
+			return "'" + getSubstring(parseResult, cstNode) + "' is a paragraph comment";
 		case STRING:
-			return "'" + getSubstring(parseController, cstNode) + "' is a string";
+			return "'" + getSubstring(parseResult, cstNode) + "' is a string";
 		default:
 			//return "No documentation available for token kind = " + tokenKind;
 			return null;
@@ -70,8 +66,12 @@ public abstract class CommonDocumentationProvider implements IDocumentationProvi
 	public String getDocumentation(Object node, IParseController controller) {
 		if (node == null)
 			return null;
-		if (node instanceof CSTNode)
-			return getCSTDocumentation((CSTNode)node, (CommonParseController)controller);
+		if (node instanceof CSTNode) {
+			ICommonParseResult parseResult = ((CommonParseController)controller).getCurrentResult();
+			if (parseResult == null)
+				return null;
+			return getCSTDocumentation((CSTNode)node, parseResult);
+		}
 		return getASTDocumentation(node);
 	}
 
