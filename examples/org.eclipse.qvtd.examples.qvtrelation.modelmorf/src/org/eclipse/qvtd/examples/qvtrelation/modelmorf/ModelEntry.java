95d2895eea8a0c1ca75fd770ecf771752a29bac9
Horacio Hoyos <arcanefoam@gmail.com>
2009-10-20 17:24:45 +0100

    [fb45] First implementation of the MWE2 script with the HstmToStm example.

---
+++ examples/org.eclipse.qvtd.examples.qvtrelation.modelmorf/src/org/eclipse/qvtd/examples/qvtrelation/modelmorf/ModelEntry.java
@@ -0,0 +1,52 @@
+package org.eclipse.qvtd.examples.qvtrelation.modelmorf;
+
+public class ModelEntry {
+	private String variable;
+	private String file;
+	private String variableRef;
+	private String deltaFile;
+	private boolean bound  = false;
+	private boolean delta = false;
+	public String getVariable() {
+		return variable;
+	}
+	public void setVariable(String variable) {
+		this.variable = variable;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+		setBound(false);
+	}
+	public String getVariableRef() {
+		return variableRef;
+	}
+	public void setVariableRef(String variableRef) {
+		this.variableRef = variableRef;
+		setBound(true);
+	}
+	public String getDeltaFile() {
+		return deltaFile;
+	}
+	public void setDeltaFile(String deltaFile) {
+		this.deltaFile = deltaFile;
+		setDelta(true);
+	}
+	public boolean isBound() {
+		return bound;
+	}
+	public void setBound(boolean bound) {
+		this.bound = bound;
+	}
+	public boolean hasDelta() {
+		return delta;
+	}
+	public void setDelta(boolean delta) {
+		this.delta = delta;
+	}
+	
+	
+	
+}
