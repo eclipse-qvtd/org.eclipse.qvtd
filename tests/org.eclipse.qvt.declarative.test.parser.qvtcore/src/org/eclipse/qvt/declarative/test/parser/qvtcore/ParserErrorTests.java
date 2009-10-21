96e9608d64c16730253607d6dcd6a6faf975666b
ewillink <ewillink>
2009-10-21 07:50:43 +0000

    Track OCL bugs 184048 287993 292112

--- tests/org.eclipse.qvt.declarative.test.parser.qvtcore/src/org/eclipse/qvt/declarative/test/parser/qvtcore/ParserErrorTests.java
+++ tests/org.eclipse.qvt.declarative.test.parser.qvtcore/src/org/eclipse/qvt/declarative/test/parser/qvtcore/ParserErrorTests.java
@@ -21,7 +21,7 @@ public class ParserErrorTests extends AbstractQVTcTestCase
 	public void testParserErrorNoTxName() throws IOException, CoreException {
 		ProblemLog expectedProblems = new ProblemLog();
 		expectedProblems.handleProblem(ProblemHandler.Severity.ERROR, ProblemHandler.Phase.PARSER,
-				"\"<pathName>\" expected after \"transformation\"", null, -1, -1);
+				"\"<simpleName>\" expected after \"transformation\"", null, -1, -1);
 		problemTest("ParserErrorNoTxName", expectedProblems);
 	}
 	
