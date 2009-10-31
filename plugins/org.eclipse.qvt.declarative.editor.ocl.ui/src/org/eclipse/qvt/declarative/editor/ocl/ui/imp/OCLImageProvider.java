4e1c4409942ab7a3f3d4f18ff9833c33de85d96d
ewillink <ewillink>
2009-10-31 17:48:28 +0000

    Add copyrights

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLImageProvider.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLImageProvider.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2009 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: OCLImageProvider.java,v 1.3 2009/10/31 17:48:28 ewillink Exp $
+ */
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
 import org.eclipse.ocl.cst.PrePostOrBodyDeclCS;
