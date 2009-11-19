a10bc6cd8ee8b0a4594c4c91d1db35156a9b873c
ewillink <ewillink>
2009-11-19 10:26:48 +0000

    Use org.eclipse.ocl.edit from MDT/OCL

--- plugins/org.eclipse.qvt.declarative.ecore.qvttemplate.editor/src/org/eclipse/qvt/declarative/ecore/QVTTemplate/presentation/QVTTemplateEditorPlugin.java
+++ plugins/org.eclipse.qvt.declarative.ecore.qvttemplate.editor/src/org/eclipse/qvt/declarative/ecore/QVTTemplate/presentation/QVTTemplateEditorPlugin.java
@@ -12,21 +12,16 @@
  * 
  * </copyright>
  *
- * $Id: QVTTemplateEditorPlugin.java,v 1.1 2008/07/23 09:50:18 qglineur Exp $
+ * $Id: QVTTemplateEditorPlugin.java,v 1.2 2009/11/19 10:26:48 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.ecore.QVTTemplate.presentation;
 
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
-
-import org.eclipse.ocl.ecore.provider.OCLEcoreEditPlugin;
-
-import org.eclipse.ocl.edit.internal.OCLEditPlugin;
+import org.eclipse.ocl.ecore.edit.OCLEcoreEditPlugin;
+import org.eclipse.ocl.edit.OCLEditPlugin;
 
 /**
  * This is the central singleton for the QVTTemplate editor plugin.
