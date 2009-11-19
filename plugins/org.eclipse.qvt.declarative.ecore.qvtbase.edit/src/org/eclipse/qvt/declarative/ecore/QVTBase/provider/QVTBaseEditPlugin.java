e21753e1cbf73ef4c8e982d187fd6cfb894a98fd
ewillink <ewillink>
2009-11-19 10:26:43 +0000

    Use org.eclipse.ocl.edit from MDT/OCL

--- plugins/org.eclipse.qvt.declarative.ecore.qvtbase.edit/src/org/eclipse/qvt/declarative/ecore/QVTBase/provider/QVTBaseEditPlugin.java
+++ plugins/org.eclipse.qvt.declarative.ecore.qvtbase.edit/src/org/eclipse/qvt/declarative/ecore/QVTBase/provider/QVTBaseEditPlugin.java
@@ -12,19 +12,15 @@
  * 
  * </copyright>
  *
- * $Id: QVTBaseEditPlugin.java,v 1.1 2008/07/23 09:41:12 qglineur Exp $
+ * $Id: QVTBaseEditPlugin.java,v 1.2 2009/11/19 10:26:43 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.ecore.QVTBase.provider;
 
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
-
-import org.eclipse.ocl.ecore.provider.OCLEcoreEditPlugin;
-
-import org.eclipse.ocl.edit.internal.OCLEditPlugin;
+import org.eclipse.ocl.ecore.edit.OCLEcoreEditPlugin;
+import org.eclipse.ocl.edit.OCLEditPlugin;
 
 /**
  * This is the central singleton for the QVTBase edit plugin.
