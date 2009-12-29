2850044b5efcb017c407b5747b25897fca94f117
ewillink <ewillink>
2009-12-29 07:34:15 +0000

    Regenerated on M4

--- plugins/org.eclipse.qvt.declarative.ecore.qvtbase/src/org/eclipse/qvt/declarative/ecore/QVTBase/impl/QVTBasePackageImpl.java
+++ plugins/org.eclipse.qvt.declarative.ecore.qvtbase/src/org/eclipse/qvt/declarative/ecore/QVTBase/impl/QVTBasePackageImpl.java
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: QVTBasePackageImpl.java,v 1.7 2009/01/27 21:17:57 ewillink Exp $
+ * $Id: QVTBasePackageImpl.java,v 1.8 2009/12/29 07:34:15 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.ecore.QVTBase.impl;
 
@@ -130,20 +130,10 @@ public class QVTBasePackageImpl extends EPackageImpl implements QVTBasePackage {
 	private static boolean isInited = false;
 
 	/**
-	 * Creates, registers, and initializes the <b>Package</b> for this
-	 * model, and for any others upon which it depends.  Simple
-	 * dependencies are satisfied by calling this method on all
-	 * dependent packages before doing anything else.  This method drives
-	 * initialization for interdependent packages directly, in parallel
-	 * with this package, itself.
-	 * <p>Of this package and its interdependencies, all packages which
-	 * have not yet been registered by their URI values are first created
-	 * and registered.  The packages are then initialized in two steps:
-	 * meta-model objects for all of the packages are created before any
-	 * are initialized, since one package's meta-model objects may refer to
-	 * those of another.
-	 * <p>Invocation of this method will not affect any packages that have
-	 * already been initialized.
+	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+	 * 
+	 * <p>This method is used to initialize {@link QVTBasePackage#eINSTANCE} when that field is accessed.
+	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @see #eNS_URI
@@ -155,7 +145,7 @@ public class QVTBasePackageImpl extends EPackageImpl implements QVTBasePackage {
 		if (isInited) return (QVTBasePackage)EPackage.Registry.INSTANCE.getEPackage(QVTBasePackage.eNS_URI);
 
 		// Obtain or create and register package
-		QVTBasePackageImpl theQVTBasePackage = (QVTBasePackageImpl)(EPackage.Registry.INSTANCE.getEPackage(eNS_URI) instanceof QVTBasePackageImpl ? EPackage.Registry.INSTANCE.getEPackage(eNS_URI) : new QVTBasePackageImpl());
+		QVTBasePackageImpl theQVTBasePackage = (QVTBasePackageImpl)(EPackage.Registry.INSTANCE.get(eNS_URI) instanceof QVTBasePackageImpl ? EPackage.Registry.INSTANCE.get(eNS_URI) : new QVTBasePackageImpl());
 
 		isInited = true;
 
@@ -180,6 +170,9 @@ public class QVTBasePackageImpl extends EPackageImpl implements QVTBasePackage {
 		// Mark meta-data to indicate it can't be changed
 		theQVTBasePackage.freeze();
 
+  
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(QVTBasePackage.eNS_URI, theQVTBasePackage);
 		return theQVTBasePackage;
 	}
 
