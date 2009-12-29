3a89ed12d5d016d6a927aa249e1b4820f316a724
ewillink <ewillink>
2009-12-29 07:34:19 +0000

    Regenerated on M4

--- plugins/org.eclipse.qvt.declarative.ecore.qvttemplate/src/org/eclipse/qvt/declarative/ecore/QVTTemplate/impl/QVTTemplatePackageImpl.java
+++ plugins/org.eclipse.qvt.declarative.ecore.qvttemplate/src/org/eclipse/qvt/declarative/ecore/QVTTemplate/impl/QVTTemplatePackageImpl.java
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: QVTTemplatePackageImpl.java,v 1.4 2009/01/27 21:17:48 ewillink Exp $
+ * $Id: QVTTemplatePackageImpl.java,v 1.5 2009/12/29 07:34:19 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.ecore.QVTTemplate.impl;
 
@@ -93,20 +93,10 @@ public class QVTTemplatePackageImpl extends EPackageImpl implements QVTTemplateP
 	private static boolean isInited = false;
 
 	/**
-	 * Creates, registers, and initializes the <b>Package</b> for this
-	 * model, and for any others upon which it depends.  Simple
-	 * dependencies are satisfied by calling this method on all
-	 * dependent packages before doing anything else.  This method drives
-	 * initialization for interdependent packages directly, in parallel
-	 * with this package, itself.
-	 * <p>Of this package and its interdependencies, all packages which
-	 * have not yet been registered by their URI values are first created
-	 * and registered.  The packages are then initialized in two steps:
-	 * meta-model objects for all of the packages are created before any
-	 * are initialized, since one package's meta-model objects may refer to
-	 * those of another.
-	 * <p>Invocation of this method will not affect any packages that have
-	 * already been initialized.
+	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+	 * 
+	 * <p>This method is used to initialize {@link QVTTemplatePackage#eINSTANCE} when that field is accessed.
+	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @see #eNS_URI
@@ -118,7 +108,7 @@ public class QVTTemplatePackageImpl extends EPackageImpl implements QVTTemplateP
 		if (isInited) return (QVTTemplatePackage)EPackage.Registry.INSTANCE.getEPackage(QVTTemplatePackage.eNS_URI);
 
 		// Obtain or create and register package
-		QVTTemplatePackageImpl theQVTTemplatePackage = (QVTTemplatePackageImpl)(EPackage.Registry.INSTANCE.getEPackage(eNS_URI) instanceof QVTTemplatePackageImpl ? EPackage.Registry.INSTANCE.getEPackage(eNS_URI) : new QVTTemplatePackageImpl());
+		QVTTemplatePackageImpl theQVTTemplatePackage = (QVTTemplatePackageImpl)(EPackage.Registry.INSTANCE.get(eNS_URI) instanceof QVTTemplatePackageImpl ? EPackage.Registry.INSTANCE.get(eNS_URI) : new QVTTemplatePackageImpl());
 
 		isInited = true;
 
@@ -143,6 +133,9 @@ public class QVTTemplatePackageImpl extends EPackageImpl implements QVTTemplateP
 		// Mark meta-data to indicate it can't be changed
 		theQVTTemplatePackage.freeze();
 
+  
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(QVTTemplatePackage.eNS_URI, theQVTTemplatePackage);
 		return theQVTTemplatePackage;
 	}
 
