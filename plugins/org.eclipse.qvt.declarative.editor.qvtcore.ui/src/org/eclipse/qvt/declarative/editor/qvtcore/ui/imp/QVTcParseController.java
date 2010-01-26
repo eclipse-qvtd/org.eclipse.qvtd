3bb45d09ec38a92376d68712bc03180860c16b35
ewillink <ewillink>
2010-01-26 22:03:08 +0000

    [299396,300534] Track MDT/OCL LPG2 enhancements

--- plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcParseController.java
+++ plugins/org.eclipse.qvt.declarative.editor.qvtcore.ui/src/org/eclipse/qvt/declarative/editor/qvtcore/ui/imp/QVTcParseController.java
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: QVTcParseController.java,v 1.6 2009/08/20 20:22:08 ewillink Exp $
+ * $Id: QVTcParseController.java,v 1.7 2010/01/26 22:03:08 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.qvtcore.ui.imp;
 
@@ -20,7 +20,7 @@ import org.eclipse.qvt.declarative.editor.ocl.ui.imp.OCLParseController;
 import org.eclipse.qvt.declarative.editor.qvtcore.ui.QVTcCreationFactory;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonParseController;
 import org.eclipse.qvt.declarative.editor.ui.imp.ICommonKeyword;
-import org.eclipse.qvt.declarative.parser.qvtcore.QVTcLexer;
+import org.eclipse.qvt.declarative.parser.qvtcore.QVTcParsersym;
 import org.eclipse.qvt.declarative.parser.qvtcore.cst.QVTcCSTPackage;
 
 public class QVTcParseController extends CommonParseController
@@ -38,19 +38,19 @@ public class QVTcParseController extends CommonParseController
 
 	public TokenKind getTokenKind(int kind) {
 		switch (kind) {
-		case QVTcLexer.TK_EOF_TOKEN:
+		case QVTcParsersym.TK_EOF_TOKEN:
 			return TokenKind.EOF;
-		case QVTcLexer.TK_IDENTIFIER:
+		case QVTcParsersym.TK_IDENTIFIER:
 			return TokenKind.IDENTIFIER;
-		case QVTcLexer.TK_INTEGER_LITERAL:
+		case QVTcParsersym.TK_INTEGER_LITERAL:
 			return TokenKind.INTEGER;
-		case QVTcLexer.TK_REAL_LITERAL:
+		case QVTcParsersym.TK_REAL_LITERAL:
 			return TokenKind.REAL;
-        case QVTcLexer.TK_STRING_LITERAL:
+        case QVTcParsersym.TK_STRING_LITERAL:
              return TokenKind.STRING;
-        case QVTcLexer.TK_SINGLE_LINE_COMMENT:
+        case QVTcParsersym.TK_SINGLE_LINE_COMMENT:
             return TokenKind.LINE_COMMENT;
-        case QVTcLexer.TK_MULTI_LINE_COMMENT:
+        case QVTcParsersym.TK_MULTI_LINE_COMMENT:
             return TokenKind.PARAGRAPH_COMMENT;
 		default:
 			if (isKeyword(kind))
