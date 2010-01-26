0e351e62790dc629a07c12be19cdb03aa5a266b2
ewillink <ewillink>
2010-01-26 22:03:12 +0000

    [299396,300534] Track MDT/OCL LPG2 enhancements

--- plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
+++ plugins/org.eclipse.qvt.declarative.editor.ocl.ui/src/org/eclipse/qvt/declarative/editor/ocl/ui/imp/OCLParseController.java
@@ -12,12 +12,12 @@
  * 
  * </copyright>
  *
- * $Id: OCLParseController.java,v 1.6 2009/08/20 20:22:07 ewillink Exp $
+ * $Id: OCLParseController.java,v 1.7 2010/01/26 22:03:12 ewillink Exp $
  */
 package org.eclipse.qvt.declarative.editor.ocl.ui.imp;
 
 import org.eclipse.ocl.cst.CSTPackage;
-import org.eclipse.ocl.parser.backtracking.OCLBacktrackingLexer;
+import org.eclipse.ocl.parser.backtracking.OCLBacktrackingParsersym;
 import org.eclipse.qvt.declarative.editor.ocl.ui.OCLCreationFactory;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonKeyword;
 import org.eclipse.qvt.declarative.editor.ui.imp.CommonParseController;
@@ -100,19 +100,19 @@ public class OCLParseController extends CommonParseController
 
 	public TokenKind getTokenKind(int kind) {
 		switch (kind) {
-		case OCLBacktrackingLexer.TK_EOF_TOKEN:
+		case OCLBacktrackingParsersym.TK_EOF_TOKEN:
 			return TokenKind.EOF;
-		case OCLBacktrackingLexer.TK_IDENTIFIER:
+		case OCLBacktrackingParsersym.TK_IDENTIFIER:
 			return TokenKind.IDENTIFIER;
-		case OCLBacktrackingLexer.TK_INTEGER_LITERAL:
+		case OCLBacktrackingParsersym.TK_INTEGER_LITERAL:
 			return TokenKind.INTEGER;
-		case OCLBacktrackingLexer.TK_REAL_LITERAL:
+		case OCLBacktrackingParsersym.TK_REAL_LITERAL:
 			return TokenKind.REAL;
-        case OCLBacktrackingLexer.TK_STRING_LITERAL:
+        case OCLBacktrackingParsersym.TK_STRING_LITERAL:
             return TokenKind.STRING;
-        case OCLBacktrackingLexer.TK_SINGLE_LINE_COMMENT:
+        case OCLBacktrackingParsersym.TK_SINGLE_LINE_COMMENT:
             return TokenKind.LINE_COMMENT;
-        case OCLBacktrackingLexer.TK_MULTI_LINE_COMMENT:
+        case OCLBacktrackingParsersym.TK_MULTI_LINE_COMMENT:
             return TokenKind.PARAGRAPH_COMMENT;
 		default:
 			if (isKeyword(kind))
