6e0500effdd854a90afeb46d982daa7d050e5bc3
ewillink <ewillink>
2010-07-10 09:35:46 +0000

    Use OCL Model Registry

--- tests/org.eclipse.qvt.declarative.test.editor.qvtcore/src/org/eclipse/qvt/declarative/test/editor/qvtcore/QVTcEditorTestCase.java
+++ tests/org.eclipse.qvt.declarative.test.editor.qvtcore/src/org/eclipse/qvt/declarative/test/editor/qvtcore/QVTcEditorTestCase.java
@@ -2,9 +2,9 @@ package org.eclipse.qvt.declarative.test.editor.qvtcore;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ocl.examples.modelregistry.environment.ProjectHandle;
 import org.eclipse.qvt.declarative.editor.qvtcore.ui.QVTcCreationFactory;
 import org.eclipse.qvt.declarative.editor.ui.ICreationFactory;
-import org.eclipse.qvt.declarative.modelregistry.environment.AbstractProjectHandle;
 import org.eclipse.qvt.declarative.test.editor.EditorTestCase;
 
 public class QVTcEditorTestCase extends EditorTestCase
@@ -17,7 +17,7 @@ public class QVTcEditorTestCase extends EditorTestCase
 			"    <entry accessor='Ecore' kind='Model Name' uri='platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore'/>\n" +
 			"  </resource>\n" +
 			"</mreg:ModelRegistrySettings>\n";
-		final String testFileName = AbstractProjectHandle.DEFAULT_MODEL_REGISTRY_PATH;
+		final String testFileName = ProjectHandle.DEFAULT_MODEL_REGISTRY_PATH;
 		IFile file = createFile(testFileName, contents);
 		return file;
 	}
