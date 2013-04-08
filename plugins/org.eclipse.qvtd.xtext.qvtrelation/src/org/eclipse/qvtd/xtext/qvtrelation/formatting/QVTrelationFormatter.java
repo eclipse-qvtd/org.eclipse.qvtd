50e80a58b2b6e8c386526f0f027173bdd937d4fb
Ed.Willink <ed@willink.me.uk>
2013-04-08 12:57:47 +0100

    [unrelated] Add QVTr formatting

--- plugins/org.eclipse.qvtd.xtext.qvtrelation/src/org/eclipse/qvtd/xtext/qvtrelation/formatting/QVTrelationFormatter.java
+++ plugins/org.eclipse.qvtd.xtext.qvtrelation/src/org/eclipse/qvtd/xtext/qvtrelation/formatting/QVTrelationFormatter.java
@@ -3,7 +3,25 @@
  */
 package org.eclipse.qvtd.xtext.qvtrelation.formatting;
 
-import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
+import org.eclipse.ocl.examples.xtext.essentialocl.formatting.AbstractEssentialOCLFormatter;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.CollectionTemplateCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.DefaultValueCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.DomainCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.KeyDeclCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.LibraryCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.ModelDeclCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.ObjectTemplateCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.PredicateCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.PrimitiveTypeDomainCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.QueryCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.RelationCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.TemplateCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.TransformationCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.UnitCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.VarDeclarationCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.WhenCSElements;
+import org.eclipse.qvtd.xtext.qvtrelation.services.QVTrelationGrammarAccess.WhereCSElements;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
 
 /**
@@ -14,14 +32,162 @@ import org.eclipse.xtext.formatting.impl.FormattingConfig;
  * 
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
-public class QVTrelationFormatter extends AbstractDeclarativeFormatter {
-	
+public class QVTrelationFormatter extends AbstractEssentialOCLFormatter
+{
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-// It's usually a good idea to activate the following three statements.
-// They will add and preserve newlines around comments
-//		c.setLinewrap(0, 1, 2).before(getGrammarAccess().getSL_COMMENTRule());
-//		c.setLinewrap(0, 1, 2).before(getGrammarAccess().getML_COMMENTRule());
-//		c.setLinewrap(0, 1, 1).after(getGrammarAccess().getML_COMMENTRule());
+	    c.setAutoLinewrap(120);
+
+	    QVTrelationGrammarAccess f = getGrammarAccess();
+	    
+		configureCollectionLiteralExpCS(c, f.getCollectionLiteralExpCSAccess());
+		configureCollectionTypeCS(c, f.getCollectionTypeCSAccess());
+	    configureEssentialOCLNavigationOperatorCS(c, f.getEssentialOCLNavigationOperatorCSAccess());
+		configureIfExpCS(c, f.getIfExpCSAccess());
+		configureLetExpCS(c, f.getLetExpCSAccess());
+		configureMultiplicityBoundsCS(c, f.getMultiplicityBoundsCSAccess());
+		configureMultiplicityCS(c, f.getMultiplicityCSAccess());
+		configureMultiplicityStringCS(c, f.getMultiplicityStringCSAccess());
+	    configureNavigatingCommaArgCS(c, f.getNavigatingCommaArgCSAccess());
+	    configureNavigatingSemiArgCS(c, f.getNavigatingSemiArgCSAccess());
+	    configureNestedExpCS(c, f.getNestedExpCSAccess());
+	    configurePathNameCS(c, f.getPathNameCSAccess());
+	    configurePrimaryExpCS(c, f.getPrimaryExpCSAccess());
+	    configureTupleLiteralExpCS(c, f.getTupleLiteralExpCSAccess());
+	    configureTupleTypeCS(c, f.getTupleTypeCSAccess());
+	    configureURIPathNameCS(c, f.getURIPathNameCSAccess());
+
+	    c.setLinewrap(2).before(f.getML_COMMENTRule());
+	    c.setLinewrap(1).after(f.getML_COMMENTRule());
+
+	    c.setLinewrap(1).between(f.getCollectionTemplateCSRule(), f.getCollectionTemplateCSRule());
+	    c.setLinewrap(1).between(f.getCollectionTemplateCSRule(), f.getObjectTemplateCSRule());
+	    c.setLinewrap(1).between(f.getDomainCSRule(), f.getDomainCSRule());
+	    c.setLinewrap(1).between(f.getDomainCSRule(), f.getPrimitiveTypeDomainCSRule());
+	    c.setLinewrap(2).between(f.getKeyDeclCSRule(), f.getQueryCSRule());
+	    c.setLinewrap(2).between(f.getLibraryCSRule(), f.getTransformationCSRule());
+	    c.setLinewrap(1).between(f.getObjectTemplateCSRule(), f.getCollectionTemplateCSRule());
+	    c.setLinewrap(1).between(f.getObjectTemplateCSRule(), f.getObjectTemplateCSRule());
+	    c.setLinewrap(1).between(f.getPrimitiveTypeDomainCSRule(), f.getDomainCSRule());
+	    c.setLinewrap(1).between(f.getPrimitiveTypeDomainCSRule(), f.getPrimitiveTypeDomainCSRule());
+	    c.setLinewrap(2).between(f.getQueryCSRule(), f.getKeyDeclCSRule());
+	    c.setLinewrap(2).between(f.getQueryCSRule(), f.getQueryCSRule());
+	    c.setLinewrap(2).between(f.getQueryCSRule(), f.getRelationCSRule());
+	    c.setLinewrap(2).between(f.getRelationCSRule(), f.getQueryCSRule());
+	    c.setLinewrap(2).between(f.getRelationCSRule(), f.getRelationCSRule());
+	    c.setLinewrap(2).between(f.getTransformationCSRule(), f.getTransformationCSRule());
+	    c.setLinewrap(2).between(f.getUnitCSRule(), f.getTransformationCSRule());
+
+	    {
+	    	CollectionTemplateCSElements a = f.getCollectionTemplateCSAccess();
+		    c.setLinewrap(1).before(a.getMemberIdentifiersAssignment_4_0());
+		    c.setLinewrap(1).before(a.getMemberIdentifiersAssignment_4_1_1());
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_3(), a.getRightCurlyBracketKeyword_5());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_3(), a.getRightCurlyBracketKeyword_5());
+			c.setNoSpace().before(a.getCommaKeyword_4_1_0());
+	    }
+	    {
+	    	DefaultValueCSElements a = f.getDefaultValueCSAccess();
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_3());
+	    }
+	    {
+	    	DomainCSElements a = f.getDomainCSAccess();
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_5_1(), a.getRightCurlyBracketKeyword_5_3());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_5_1(), a.getRightCurlyBracketKeyword_5_3());
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_6());
+	    }
+	    {
+	    	KeyDeclCSElements a = f.getKeyDeclCSAccess();
+			c.setNoSpace().after(a.getLeftCurlyBracketKeyword_2());
+			c.setNoSpace().around(a.getRightCurlyBracketKeyword_5());
+			c.setNoSpace().before(a.getCommaKeyword_4_0());
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_6());
+	    }
+	    {
+	    	LibraryCSElements a = f.getLibraryCSAccess();
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_2());
+	    }
+	    {
+	    	ModelDeclCSElements a = f.getModelDeclCSAccess();
+			c.setNoSpace().after(a.getLeftCurlyBracketKeyword_2_1_0());
+			c.setNoSpace().before(a.getRightCurlyBracketKeyword_2_1_3());
+			c.setNoSpace().before(a.getCommaKeyword_2_1_2_0());
+	    }
+	    {
+	    	ObjectTemplateCSElements a = f.getObjectTemplateCSAccess();
+		    c.setLinewrap(1).before(a.getPropertyTemplatesAssignment_4_0());
+		    c.setLinewrap(1).before(a.getPropertyTemplatesAssignment_4_1_1());
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_3(), a.getRightCurlyBracketKeyword_5());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_3(), a.getRightCurlyBracketKeyword_5());
+			c.setNoSpace().before(a.getCommaKeyword_4_1_0());
+	    }
+	    {
+	    	PredicateCSElements a = f.getPredicateCSAccess();
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_1());
+	    }
+	    {
+	    	PrimitiveTypeDomainCSElements a = f.getPrimitiveTypeDomainCSAccess();
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_5());
+	    }
+	    {
+	    	QueryCSElements a = f.getQueryCSAccess();
+			c.setNoSpace().around(a.getLeftParenthesisKeyword_2());	
+			c.setNoSpace().before(a.getCommaKeyword_3_1_0());
+			c.setNoSpace().before(a.getRightParenthesisKeyword_4());	
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_7_0());
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_7_1_0(), a.getRightCurlyBracketKeyword_7_1_2());
+			setBraces(c, a.getLeftCurlyBracketKeyword_7_1_0(), a.getRightCurlyBracketKeyword_7_1_2());
+		    c.setIndentation(a.getLeftParenthesisKeyword_2(), a.getRightParenthesisKeyword_4());
+	    }
+	    {
+	    	RelationCSElements a = f.getRelationCSAccess();
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_4(), a.getRightCurlyBracketKeyword_9());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_4(), a.getRightCurlyBracketKeyword_9());
+	    }
+	    {
+	    	TemplateCSElements a = f.getTemplateCSAccess();
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_1_0(), a.getRightCurlyBracketKeyword_1_2());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_1_0(), a.getRightCurlyBracketKeyword_1_2());
+	    }
+	    {
+	    	TransformationCSElements a = f.getTransformationCSAccess();
+			c.setNoSpace().around(a.getLeftParenthesisKeyword_2());	
+			c.setNoSpace().before(a.getCommaKeyword_4_0());
+			c.setNoSpace().before(a.getRightParenthesisKeyword_5());	
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_7(), a.getRightCurlyBracketKeyword_10());
+			setBraces(c, a.getLeftCurlyBracketKeyword_7(), a.getRightCurlyBracketKeyword_10());
+		    c.setIndentation(a.getLeftParenthesisKeyword_2(), a.getRightParenthesisKeyword_5());
+	    }
+	    {
+	    	UnitCSElements a = f.getUnitCSAccess();
+			c.setNoSpace().around(a.getColonColonKeyword_3_0());	
+			c.setNoSpace().around(a.getAllAsteriskKeyword_3_1_0());	
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_4());
+	    }
+	    {
+	    	VarDeclarationCSElements a = f.getVarDeclarationCSAccess();
+			c.setNoSpace().before(a.getCommaKeyword_1_0());
+			setNoSpaceLineWrap(c, a.getSemicolonKeyword_4());
+	    }
+	    {
+	    	WhenCSElements a = f.getWhenCSAccess();
+		    c.setLinewrap(1).before(a.getPredicatesAssignment_3());
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_2(), a.getRightCurlyBracketKeyword_4());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_2(), a.getRightCurlyBracketKeyword_4());
+	    }
+	    {
+	    	WhereCSElements a = f.getWhereCSAccess();
+		    c.setLinewrap(1).before(a.getPredicatesAssignment_3());
+			c.setNoSpace().between(a.getLeftCurlyBracketKeyword_2(), a.getRightCurlyBracketKeyword_4());
+			setAppendedBraces(c, a.getLeftCurlyBracketKeyword_2(), a.getRightCurlyBracketKeyword_4());
+	    }
+	    {	// comments
+	    	c.setNoLinewrap().before(f.getSL_COMMENTRule());
+	    }
+	}
+
+	@Override
+	public QVTrelationGrammarAccess getGrammarAccess() {
+		return (QVTrelationGrammarAccess) super.getGrammarAccess();
 	}
 }
