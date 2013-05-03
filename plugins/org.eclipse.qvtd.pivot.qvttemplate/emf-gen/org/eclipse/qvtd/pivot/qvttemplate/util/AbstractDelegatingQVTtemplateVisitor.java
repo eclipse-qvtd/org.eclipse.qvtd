789ab9d3101c0d7e260edbad6930778188295d7c
Ed Willink <ed@willink.me.uk>
2013-05-03 19:15:12 +0100

    [unrelated] Track changed OCL Visitor Generator

--- plugins/org.eclipse.qvtd.pivot.qvttemplate/emf-gen/org/eclipse/qvtd/pivot/qvttemplate/util/AbstractDelegatingQVTtemplateVisitor.java
+++ plugins/org.eclipse.qvtd.pivot.qvttemplate/emf-gen/org/eclipse/qvtd/pivot/qvttemplate/util/AbstractDelegatingQVTtemplateVisitor.java
@@ -1,19 +1,19 @@
 /**
  * <copyright>
- *
- * Copyright (c) 2010,2011 E.D.Willink and others.
+ * 
+ * Copyright (c) 2013 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     E.D.Willink - initial API and implementation
- *
+ *   E.D.Willink - Initial API and implementation
+ * 
  * </copyright>
  *
  * This code is auto-generated
- * from: /org.eclipse.qvtd.pivot.qvttemplate/model/QVTtemplate.ecore
+ * from: model/QVTtemplate.genmodel
  * by: org.eclipse.ocl.examples.build.acceleo.GenerateVisitor
  * defined by: org.eclipse.ocl.examples.build.acceleo.generateVisitors.mtl
  * invoked by: org.eclipse.ocl.examples.build.utilities.*
