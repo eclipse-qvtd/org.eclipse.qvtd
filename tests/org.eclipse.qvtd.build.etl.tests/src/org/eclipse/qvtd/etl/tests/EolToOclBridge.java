7b71b6b4d3a4c480d03155c5d58c3621160a24c4
Horacio Hoyos <arcanefoam@gmail.com>
2013-11-07 18:11:55 +0000

    [unrelated] Additional tests on the MTC. Test on unresolved proxies

---
+++ tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/etl/tests/EolToOclBridge.java
@@ -0,0 +1,21 @@
+package org.eclipse.qvtd.etl.tests;
+
+import org.eclipse.emf.ecore.impl.EClassImpl;
+import org.eclipse.ocl.examples.pivot.internal.impl.TypeImpl;
+import org.eclipse.ocl.examples.pivot.utilities.PivotUtil;
+
+public class EolToOclBridge {
+	
+	public void test(EClassImpl c) {
+		
+		System.out.println("OclBridge " + c);
+		
+	}
+	
+	public void test2(TypeImpl c) {
+		
+		System.out.println("OclBridge 2 " + c);
+		System.out.println("OclBridge 2 " + c.getInstanceClassName());
+	}
+
+}
