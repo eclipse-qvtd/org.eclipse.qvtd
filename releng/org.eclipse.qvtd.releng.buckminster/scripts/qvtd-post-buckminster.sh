4f9a5d4b881cb5d333fb64379b1d3c1d15f4a029
Ed Willink <ed@willink.me.uk>
2013-11-11 17:16:32 +0000

    [releng] Investigate promotion in post

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -2,3 +2,5 @@
 
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/site.p2 MMT-QVTd.p2.repository
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/zips MMT-QVTd.downloads
+
+set
