3e3824976cad91fa8bbb7d56302af608f2888bcf
Ed Willink <ed@willink.me.uk>
2013-11-11 20:28:40 +0000

    [releng] Correct build.archives path

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -8,5 +8,5 @@ set
 echo cp MMT-QVTd.downloads /home/data/httpd/download.eclipse.org/mmt/qvtd/downloads/drops/0.11.0/
 echo cp MMT-QVTd.p2.repository /home/data/httpd/download.eclipse.org/mmt/qvtd/updates/milestones/0.11.0
 
-/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=. &> qvtd-publishing-luna.log 
+/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=${WORKSPACE} &> qvtd-publishing-luna.log 
 
