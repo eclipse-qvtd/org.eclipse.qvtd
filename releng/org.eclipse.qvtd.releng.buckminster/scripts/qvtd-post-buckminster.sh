4494de0e12217a941ac0728dbf637f87628ee6bb
Ed Willink <ed@willink.me.uk>
2013-11-11 20:40:13 +0000

    [releng] Clean up unnecessary changes

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -3,10 +3,4 @@
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/site.p2 MMT-QVTd.p2.repository
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/zips MMT-QVTd.downloads
 
-set
-
-echo cp MMT-QVTd.downloads /home/data/httpd/download.eclipse.org/mmt/qvtd/downloads/drops/0.11.0/
-echo cp MMT-QVTd.p2.repository /home/data/httpd/download.eclipse.org/mmt/qvtd/updates/milestones/0.11.0
-
-/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=${WORKSPACE} &> qvtd-publishing-luna.log 
-
+/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=${WORKSPACE} 
