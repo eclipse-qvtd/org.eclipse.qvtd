21f32ccec49387706de5a685019cf6a19a5bea3e
Ed Willink <ed@willink.me.uk>
2013-11-11 17:57:43 +0000

    [releng] Use explicit ant path

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -8,7 +8,5 @@ set
 echo cp MMT-QVTd.downloads /home/data/httpd/download.eclipse.org/mmt/qvtd/downloads/drops/0.11.0/
 echo cp MMT-QVTd.p2.repository /home/data/httpd/download.eclipse.org/mmt/qvtd/updates/milestones/0.11.0
 
-ls /opt/public/common
-
-ant -f publishroot/publisher.ant -Dbuild.archives=publishroot &> qvtd-publishing-luna.log 
+/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=publishroot &> qvtd-publishing-luna.log 
 
