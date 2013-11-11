33f3d9ebb19356aa7d242b1785e2e0feba478bbf
Ed Willink <ed@willink.me.uk>
2013-11-11 17:33:49 +0000

    [releng] Try ant from post

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -4,3 +4,9 @@ mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/zips MMT-QVTd.downloads
 
 set
+
+echo cp MMT-QVTd.downloads /home/data/httpd/download.eclipse.org/mmt/qvtd/downloads/drops/0.11.0/
+echo cp MMT-QVTd.p2.repository /home/data/httpd/download.eclipse.org/mmt/qvtd/updates/milestones/0.11.0
+
+ant -f publishroot/publisher.ant -Dbuild.archives=publishroot &> qvtd-publishing-luna.log 
+
