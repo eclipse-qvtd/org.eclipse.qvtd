5e2a3f11ebbf799db46a38195f2e2f5e9d18b5c9
Horacio Hoyos <arcanefoam@gmail.com>
2013-11-19 13:22:33 +0000

    [unrelated] Hierarchy tests

--- tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/etl/tests/EolToOclBridge.java
+++ tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/etl/tests/EolToOclBridge.java
@@ -1,6 +1,9 @@
 package org.eclipse.qvtd.etl.tests;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.EClassImpl;
+import org.eclipse.emf.ecore.impl.EReferenceImpl;
+import org.eclipse.ocl.examples.pivot.internal.impl.ClassImpl;
 import org.eclipse.ocl.examples.pivot.internal.impl.TypedElementImpl;
 
 import java.util.Collections;
@@ -15,7 +18,7 @@ public class EolToOclBridge {
 		
 	}
 	
-	public void test2(org.eclipse.ocl.examples.pivot.internal.impl.ClassImpl c) {
+	public void test2(ClassImpl c) {
 		
 		System.out.println("OclBridge 2 " + c);
 		System.out.println("OclBridge 2 " + c.getInstanceClassName());
@@ -24,10 +27,17 @@ public class EolToOclBridge {
 		System.out.println("OclBridge 2 " + c.getSuperClass());
 	}
 	
+	// leastCommonAncestor
 	public TypedElementImpl test3(org.eclipse.epsilon.eol.types.EolSet<TypedElementImpl> vars) {
 		
+		System.out.println("OclBridge 3 ");
 		for(TypedElementImpl t : vars) {
 			System.out.println(t);
+			System.out.println(t.getClass());
+			EStructuralFeature typeFeat = t.eClass().getEStructuralFeature("type");
+			System.out.println(typeFeat);
+			System.out.println(t.eGet(typeFeat));
+			System.out.println(((ClassImpl)t.eGet(typeFeat)).getSuperClass());
 		}
 		return null;
 	}
