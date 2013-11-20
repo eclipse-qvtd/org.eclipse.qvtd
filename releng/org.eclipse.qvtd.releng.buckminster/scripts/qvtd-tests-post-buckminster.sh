44f27109a0643cac34c1544551af8df063c950f5
Ed Willink <ed@willink.me.uk>
2013-11-20 11:57:00 +0000

    [releng] Don't promote test builds

---
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-tests-post-buckminster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/site.p2 MMT-QVTd.p2.repository
+mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/zips MMT-QVTd.downloads
