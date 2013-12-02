c1220d7b4e74d93ee683c86c73663ab4bf878d5f
Ed Willink <ed@willink.me.uk>
2013-12-02 07:21:26 +0000

    [unrelated] Use existing value as file selection default

--- plugins/org.eclipse.qvtd.debug.ui/src/org/eclipse/qvtd/debug/ui/launching/LaunchingUtils.java
+++ plugins/org.eclipse.qvtd.debug.ui/src/org/eclipse/qvtd/debug/ui/launching/LaunchingUtils.java
@@ -15,6 +15,8 @@
 package org.eclipse.qvtd.debug.ui.launching;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
@@ -24,6 +26,8 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.ui.dialogs.WorkspaceResourceDialog;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -50,9 +54,7 @@ public class LaunchingUtils
 				String filterPath = fileDialog.getFilterPath();
 				String fileName = fileDialog.getFileName();
 				if (fileName != null) {
-					uriField.setText(URI.createFileURI(
-							filterPath + File.separator + fileName)
-							.toString());
+					uriField.setText(URI.createFileURI(filterPath + File.separator + fileName).toString());
 				}
 			}
 		});
@@ -72,7 +74,14 @@ public class LaunchingUtils
 				IFile file = null;
 				if (isSave) {
 					String path = getContextPath();
-					file = WorkspaceResourceDialog.openNewFile(shell, null, null, path != null ? new Path(path) : null, null);
+					List<ViewerFilter> filters = new ArrayList<ViewerFilter>();
+					filters.add(new ViewerFilter(){
+						@Override
+						public boolean select(Viewer viewer, Object parentElement, Object element) {
+							return true;
+						}
+					});
+					file = WorkspaceResourceDialog.openNewFile(shell, null, null, path != null ? new Path(path) : null, filters);
 				} else {
 					IFile[] files = WorkspaceResourceDialog.openFileSelection(shell, null, null, false, getContextSelection(), null);
 					if (files.length != 0) {
@@ -85,15 +94,21 @@ public class LaunchingUtils
 			}
 
 			private String getContextPath() {
-//				return context != null && context.isPlatformResource() ? URI.createURI(".").resolve(context).path().substring(9) : null;
-				return null;
+				String text = uriField.getText();
+				try {
+					URI context = text != null ? URI.createURI(text) : null;
+//					return context != null && context.isPlatformResource() ? URI.createURI(".").resolve(context).path().substring(9) : null;
+					return context != null && context.isPlatformResource() ? context.path().substring(9) : null;
+				}
+				catch (Exception e) {
+					return null;
+				}
 			}
 
 			private Object[] getContextSelection() {
 				String path = getContextPath();
 				if (path != null) {
-					IWorkspaceRoot root = ResourcesPlugin.getWorkspace()
-							.getRoot();
+					IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 					IResource resource = root.findMember(path);
 					if (resource != null && resource.isAccessible()) {
 						return new Object[] { resource };
