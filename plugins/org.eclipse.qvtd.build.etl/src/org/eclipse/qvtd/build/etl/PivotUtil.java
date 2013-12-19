0718f5dc3bd8752b7184a2bc7a92589d60139195
Horacio Hoyos <arcanefoam@gmail.com>
2013-12-19 11:50:03 +0000

    [unrelated] Cleaned up the code and added documentation and copyrights

--- plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/PivotUtil.java
+++ plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/PivotUtil.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2013 The University of York, Willink Transformations and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Horacio Hoyos - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.qvtd.build.etl;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.ocl.examples.pivot.internal.impl.ClassImpl;
@@ -9,7 +20,7 @@ import org.eclipse.ocl.examples.pivot.internal.impl.TypedElementImpl;
 
 
 /**
-* The Class PivotUtil.
+* The Class PivotUtil. Provides helper methods for analysing pivot model elements
 *
 * @author Horacio Hoyos
 */
@@ -22,24 +33,28 @@ public class PivotUtil {
     * @param vars the variables
     * @return The lowest ranking variable
     */
-    public TypedElementImpl getLowestRankVariable(HashSet<TypedElementImpl> vars) {
+    public TypedElementImpl getLowestRankVariable(Set<TypedElementImpl> vars) {
 
         return getLowestRankVariable(new ArrayList<TypedElementImpl>(vars));
-
     }
 
-    public TypedElementImpl getLowestRankVariable(ArrayList<TypedElementImpl> vars) {
+    
+    /**
+     * Returns the variable with the lowest ranking in the class hierarchy of
+    * all the variables in the list.
+     *
+     * @param vars the variables
+     * @return The lowest rank variable
+     */
+    public TypedElementImpl getLowestRankVariable(List<TypedElementImpl> vars) {
 
-        //System.out.println("getLowestRankVariable");
         TypedElementImpl min = vars.get(0);
-        //System.out.println("Min " + min);
         EStructuralFeature typeFeat = min.eClass().getEStructuralFeature("type");
         ClassImpl minType = null;
         for(int i = 1; i < vars.size(); ++i) {
             minType = (ClassImpl) min.eGet(typeFeat);
             if (((ClassImpl)vars.get(i).eGet(typeFeat)).getSuperClass().contains(minType)) {
                 min = vars.get(i);
-                //System.out.println("Min " + min);
             }
         }
         // Verify that the min is actually the min, i.e. all the other variables are superiors
--- plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/ant/PivotSetup.java
+++ plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/ant/PivotSetup.java
@@ -1,28 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2013 The University of York, Willink Transformations and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Horacio Hoyos - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.qvtd.build.etl.ant;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.ocl.examples.domain.utilities.DomainUtil;
 import org.eclipse.ocl.examples.pivot.PivotStandaloneSetup;
-import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
-import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter;
 
+/**
+ * The Class PivotSetup.
+ * @author Horacio Hoyos
+ */
 public class PivotSetup extends Task {
     
-    private MetaModelManager metaModelManager;
+    /** The resource set. */
     protected ResourceSet resourceSet;
 
+    /* (non-Javadoc)
+     * @see org.apache.tools.ant.Task#execute()
+     */
     public void execute() throws BuildException {
         
         PivotStandaloneSetup.doSetup();
-        //metaModelManager = new MetaModelManager();
-        //MetaModelManagerResourceSetAdapter.getAdapter(DomainUtil.nonNullState(resourceSet), metaModelManager);
-        //resourceSet = new ResourceSetImpl();
-        //MetaModelManager.initializeASResourceSet(resourceSet);
-        System.out.println("Pivot Setup done.");
-        
     }
 
 }
