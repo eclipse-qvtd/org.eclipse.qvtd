9cdf60055d3d5b11505ad6ce1e5d4b79cd791ee8
Ed Willink <ed@willink.me.uk>
2014-02-22 14:18:07 +0000

    [omg] Generate all QVT 1.2 model files

--- plugins/org.eclipse.qvt/src/org/eclipse/qvt/tools/Ecore2UML.java
+++ plugins/org.eclipse.qvt/src/org/eclipse/qvt/tools/Ecore2UML.java
@@ -2,34 +2,35 @@ package org.eclipse.qvt.tools;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.emf.common.notify.Notifier;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.ocl.examples.domain.utilities.ProjectMap;
 import org.eclipse.uml2.uml.Association;
-import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Generalization;
@@ -45,6 +46,11 @@ import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
 import org.eclipse.uml2.uml.util.UMLSwitch;
 import org.eclipse.uml2.uml.util.UMLUtil;
 
+/**
+ * Translator from the QVT 1.2 *.ecore master to the QVT 1.2 distribution
+ * @author Ed.Willink
+ *
+ */
 public class Ecore2UML {
 	
 	public static class MyConverter extends UMLUtil.Ecore2UMLConverter {
@@ -78,6 +84,9 @@ public class Ecore2UML {
 				allEcoreObjects.add((EObject) next);
 			}
 		}
+		//
+		//	Convert *.ecore to *.uml
+		//
 		MyConverter ecore2umlConverter = new MyConverter();
 		Map<String, String> convertOptions = new HashMap<String, String>();
 		convertOptions.put(UMLUtil.Ecore2UMLConverter.OPTION__ECORE_TAGGED_VALUES, UMLUtil.OPTION__IGNORE);
@@ -89,17 +98,7 @@ public class Ecore2UML {
 		convertOptions.put(UMLUtil.Ecore2UMLConverter.OPTION__DOCUMENTATION_ANNOTATIONS, UMLUtil.OPTION__IGNORE);
 		convertOptions.put(UMLUtil.Ecore2UMLConverter.OPTION__XMI_IDENTIFIERS, UMLUtil.OPTION__IGNORE);
 		convertOptions.put(UMLUtil.Ecore2UMLConverter.OPTION__OPPOSITE_ROLE_NAMES, UMLUtil.OPTION__PROCESS);
-		Collection<? extends EObject> umlElements = ecore2umlConverter.convert(allEcoreObjects, convertOptions, null, null);
-		for (EObject umlElement : umlElements) {
-			if (umlElement instanceof Generalization) {
-				EList<Element> sources = ((Generalization)umlElement).getSources();
-				EList<Element> targets = ((Generalization)umlElement).getTargets();
-				Classifier specific = ((Generalization)umlElement).getSpecific();
-				Classifier general = ((Generalization)umlElement).getGeneral();
-				String string = "G_" + safeNameOf(specific) + "_" + safeNameOf(general);
-				System.out.println(string);
-			}
-		}
+		ecore2umlConverter.convert(allEcoreObjects, convertOptions, null, null);
 		ResourceSet umlResourceSet = new ResourceSetImpl();
 		UMLResourcesUtil.init(umlResourceSet);
 		for (Resource ecoreResource : new ArrayList<Resource>(ecoreResourceSet.getResources())) {	// Ignore any profiles that aappear
@@ -113,6 +112,9 @@ public class Ecore2UML {
 				}
 			}
 		}
+		//
+		//	Prettify *.uml
+		//
 		for (Resource umlResource : umlResourceSet.getResources()) {
 			disorderNonCollections(umlResource);
 			nameAssociations(umlResource);
@@ -125,6 +127,75 @@ public class Ecore2UML {
 		for (Resource umlResource : umlResourceSet.getResources()) {
 			umlResource.save(saveOptions);
 		}
+		//
+		//	Resave *.uml as *.xmi
+		//
+		ResourceSet xmiResourceSet = new ResourceSetImpl();
+		xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		for (Resource umlResource : umlResourceSet.getResources()) {
+			URI xmiURI = umlResource.getURI().trimFileExtension().appendFileExtension("xmi");
+			XMIResource xmiResource = (XMIResource) xmiResourceSet.createResource(xmiURI);
+		    xmiResource.setXMIVersion("20110701");
+			xmiResource.getContents().addAll(umlResource.getContents());
+		}
+		for (Resource xmiResource : xmiResourceSet.getResources()) {
+			assignIDs(xmiResource);
+		}
+		Map<String, Object> xmiSaveOptions = new HashMap<String, Object>();
+		xmiSaveOptions.put(XMLResource.OPTION_LINE_WIDTH, 132);
+		xmiSaveOptions.put(XMLResource.OPTION_LINE_DELIMITER, "\n");
+		xmiSaveOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, new BasicExtendedMetaData(xmiResourceSet.getPackageRegistry())
+		{
+			@Override
+			public String getNamespace(EPackage ePackage) {
+				if (ePackage == UMLPackage.eINSTANCE) {
+					return "http://www.omg.org/spec/UML/20131001";
+				}
+				else {
+					return super.getNamespace(ePackage);
+				}
+			}
+		});
+		for (Resource xmiResource : xmiResourceSet.getResources()) {
+			xmiResource.save(xmiSaveOptions);
+		}
+		//
+		//	Create QVT.ecore
+		//
+		List<EObject> rootObjects = new ArrayList<EObject>();
+		for (Resource ecoreResource : ecoreResourceSet.getResources()) {
+			rootObjects.addAll(ecoreResource.getContents());
+		}
+		Resource qvtResource = ecoreResourceSet.createResource(URI.createPlatformResourceURI("/org.eclipse.qvt/model/ecore/QVT.ecore", true));
+		qvtResource.getContents().addAll(rootObjects);
+		qvtResource.save(saveOptions);
+		//
+		//	Create FlatQVT.ecore
+		//
+		List<EClassifier> flattenedObjects = new ArrayList<EClassifier>();
+		for (EObject rootEObject : rootObjects) {
+			for (EObject eObject : rootEObject.eContents()) {
+				if (eObject instanceof EClassifier) {
+					flattenedObjects.add((EClassifier) eObject);
+				}
+			}
+		}
+		Collections.sort(flattenedObjects, new Comparator<EClassifier>(){
+			@Override
+			public int compare(EClassifier o1, EClassifier o2) {
+				String n1 = o1.getName();
+				String n2 = o2.getName();
+				return n1.compareTo(n2);
+			}
+		});
+		Resource flatResource = ecoreResourceSet.createResource(URI.createPlatformResourceURI("/org.eclipse.qvt/model/ecore/FlatQVT.ecore", true));
+		EPackage flatEPackage = EcoreFactory.eINSTANCE.createEPackage();
+		flatEPackage.setName("FlatQVT");
+		flatEPackage.setNsPrefix("qvt");
+		flatEPackage.setNsURI("http://schema.omg.org/spec/QVT/20120401/FlatQVT");
+		flatEPackage.getEClassifiers().addAll(flattenedObjects);
+		flatResource.getContents().add(flatEPackage);
+		flatResource.save(saveOptions);
 		return;
 	}
 
@@ -206,8 +277,6 @@ public class Ecore2UML {
 
 			@Override
 			public String caseGeneralization(Generalization object) {
-				EList<Element> sources = object.getSources();
-				EList<Element> targets = object.getTargets();
 				Classifier specific = object.getSpecific();
 				Classifier general = object.getGeneral();
 				String string = "G_" + safeNameOf(specific) + "_" + safeNameOf(general);
@@ -216,7 +285,6 @@ public class Ecore2UML {
 
 			@Override
 			public String caseParameter(Parameter object) {
-				// TODO Auto-generated method stub
 				return doSwitch(object.getOperation()) + "." + safeNameOf(object);
 			}
 
