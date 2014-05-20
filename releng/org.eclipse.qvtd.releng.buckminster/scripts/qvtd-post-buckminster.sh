efb1cc1617674fce5e6cb93724250e1fa52cac92
Ed Willink <ed@willink.me.uk>
2014-05-20 12:11:39 +0100

    [releng] Fix copyrights

--- releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
+++ releng/org.eclipse.qvtd.releng.buckminster/scripts/qvtd-post-buckminster.sh
@@ -1,3 +1,13 @@
+#*******************************************************************************
+# Copyright (c) 2014 E.D.Willink and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     E.D.Willink - initial API and implementation
+#*******************************************************************************
 #!/bin/bash
 
 mv buildroot/buckminster.output/org.eclipse.qvtd.releng.build_*-eclipse.feature/site.p2 MMT-QVTd.p2.repository
