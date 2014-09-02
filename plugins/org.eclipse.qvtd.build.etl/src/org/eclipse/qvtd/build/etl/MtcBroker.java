0754ddc8613c3d82a5f38fe2fe745282ae637295
Horacio Hoyos <arcanefoam@gmail.com>
2014-09-02 16:46:23 +0100

    [fb33] Initial version of the MTC Broker. Reads the Configuration model and generates the Containment Trees.

--- plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/MtcBroker.java
+++ plugins/org.eclipse.qvtd.build.etl/src/org/eclipse/qvtd/build/etl/MtcBroker.java
@@ -1,7 +1,28 @@
 package org.eclipse.qvtd.build.etl;
 
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.core.resources.IContainer; 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
@@ -10,42 +31,242 @@ public class MtcBroker {
 	
 	
 	private static final String CONFIG_MODEL_NAME = "config";
+	private static final String CONFIG_MM = "/QVTcConfig.ecore";
+	private static final String CONFIG_URI = "http://www.eclipse.org/qvt/examples/0.1/QVTcConfig";
+	private static final String CONFIG_QUERIES_EOL = "helpers/ConfigQueries.eol";
+	
+	private static final String ECORE_CONTAINMENT_MM = "/ECoreContainmentTree.ecore";
+	private static final String ECORE_CONTAINMENT_URI = "http://www.eclipse.org/qvt/examples/0.1/ECoreContainemntTree";
+	private static final String ECORE_TO_TREE_EOL = "utils/EcoreToContainmentTree.eol";
+	
 	private static final String ECORE_URI = "http://www.eclipse.org/emf/2002/Ecore";
 	
 	
+	
 	private String configResourcePath;
+	private String qvtcasUri;
+	private String qvtuUri;
+	private String qvtmUri;
+	private String partitionUri;
+	private String qvtiUri;
+	private String configUri;
+	private String scheduleUri;
+	private EmfModel configModel;
+	private Class owner;
 
 	
 	
-	public MtcBroker() {
+	public MtcBroker(String qvtcasUri, Class owner) {
 	
-		// TODO Auto-generated constructor stub
+		// 1. Derive all the required paths
+		// QVTu *.qvtu.qvtas
+		// QVTm *.qvtm.qvtas
+		// Partition *.qvtp.qvtias
+		// QVTi *.qvtias
+		// Config *Config.xmi
+		// Schedule *Schedule.xmi
+		
+		this.qvtcasUri = qvtcasUri;
+		this.owner = owner;
+		System.out.println(owner);
+		URI baseUri = URI.createURI(qvtcasUri).trimFileExtension();
+		this.qvtuUri = baseUri.appendFileExtension("qvtu.qvtcas").toString();
+		this.qvtmUri = baseUri.appendFileExtension("qvtm.qvtcas").toString();
+		this.partitionUri = baseUri.appendFileExtension("qvtp.qvtias").toString();
+		this.qvtiUri = baseUri.appendFileExtension("qvtias").toString();
+
+		this.configUri = URI.createURI(baseUri.toString() + "Config").appendFileExtension("xmi").toString();
+		this.scheduleUri = URI.createURI(baseUri.toString() + "Schdule").appendFileExtension("xmi").toString();
+		registerMetamodels();
+	}
+	
+	public void execute() throws EolModelLoadingException, EpsilonExecutionException, URISyntaxException, EpsilonSourceLoadException, EpsilonParseException {
+		
+		loadConfigurationModel();
+		createContainmentTrees();
+	}
+	
+	private void createContainmentTrees() {
+		
+		List<String> mms = getCandidateMetamodels();
+		Map<EmfModel, EmfModel> emfModels = new HashMap<EmfModel, EmfModel>();
+		for (String mm : mms) {
+			//TODO Check if the containment Tree exist?
+			String modelUri = null;
+			try {
+				modelUri = getResourceUriFromClass(this.owner, mm);
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (modelUri != null) {
+					EmfModel mmModel = null;
+					EmfModel cgModel = null;
+					try {
+						mmModel = createEmfModel(changeResourceToSource(modelUri), "mm", ECORE_URI, true, false);
+						String cgUri = mmModel.getModelFileUri().trimFileExtension().toString() + "ContainmentTree.xmi";
+						cgModel = createEmfModel(cgUri, "tree", ECORE_CONTAINMENT_URI, false, true);
+					} catch (EolModelLoadingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} finally {
+						if (mmModel != null && cgModel != null  ) {
+							emfModels.put(mmModel, cgModel);
+						}
+					}
+				} 
+			}		
+		}
+		EolTask eol = null;
+		try {
+			eol = new EolTask(java.net.URI.create(getResourceURI(ECORE_TO_TREE_EOL)));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally  {
+			if (eol != null) {
+				for (EmfModel em : emfModels.keySet()) {
+					eol.models.add(em);
+					eol.models.add(emfModels.get(em));
+					try {
+						eol.execute();
+					} catch (EpsilonParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (EpsilonSourceLoadException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (EpsilonExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} finally {
+						eol.models.remove(em);
+						eol.models.remove(emfModels.get(em));
+					}
+				}
+			}
+		}
 	}
 
-	private void loadMetamodelAsModels() throws EolModelLoadingException, URISyntaxException {
+	private List<String> getCandidateMetamodels() {
+		// 2. Run the EOL operations to get the candidate models and generate the Containment Tree
+		List<String> result = new ArrayList();
+		EolTask eol = null;
+		try {
+			eol = new EolTask(java.net.URI.create(getResourceURI(CONFIG_QUERIES_EOL)));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (eol != null) {
+				eol.addModel(configModel);
+				try {
+					eol.executeOperation("getAllMetamodelFiles");
+				} catch (EpsilonExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (EpsilonSourceLoadException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (EpsilonParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Object mmList = eol.getResult();
+				System.out.println(mmList);
+				System.out.println(mmList.getClass());
+				if (mmList instanceof ArrayList) {
+					for (String mm : (ArrayList<String>)mmList) {
+						result.add(mm);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	private void loadConfigurationModel() {
 		
 		// 1. Get the metamodels from the configuration
-		EmfModel configModel = createEmfModel(configResourcePath, CONFIG_MODEL_NAME, ECORE_URI, true, false);
-		
-		// 2. Run the EOL operations to get the candidate models
+		try {
+			configModel = createEmfModel(configUri, CONFIG_MODEL_NAME, CONFIG_URI, true, false);
+		} catch (EolModelLoadingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
 	
-	
-	private static EmfModel createEmfModel(String resourcePath, String modelName, String metamodelUri, boolean readOnLoad, boolean storeOnDispoal) 
-			throws EolModelLoadingException, URISyntaxException {
+	private static EmfModel createEmfModel(String modeUri, String modelName, String metamodelUri, boolean readOnLoad, boolean storeOnDispoal) 
+			throws EolModelLoadingException {
 	
 		EmfModel emfModel = new EmfModel();
 		StringProperties properties = new StringProperties();
 		properties.put(EmfModel.PROPERTY_NAME, modelName);
-		properties.put(EmfModel.PROPERTY_METAMODEL_URI, ECORE_URI);
-		properties.put(EmfModel.PROPERTY_MODEL_URI, 
-				getURI(resourcePath));
+		properties.put(EmfModel.PROPERTY_METAMODEL_URI, metamodelUri);
+		properties.put(EmfModel.PROPERTY_MODEL_URI, modeUri);
 		properties.put(EmfModel.PROPERTY_READONLOAD, readOnLoad);
 		properties.put(EmfModel.PROPERTY_STOREONDISPOSAL, storeOnDispoal);
 		emfModel.load(properties, null);
 		return emfModel;
+	}
+	
+	private void registerMetamodels() {
+		// register globally the Ecore Resource Factory to the ".ecore" extension
+		// weird that we need to do this, but well...
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+		    "ecore", new EcoreResourceFactoryImpl());
+		ResourceSet rs = new ResourceSetImpl();
+		// Enable extended metadata
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(rs.getPackageRegistry());
+		rs.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,
+		    extendedMetaData);
+		String path = null;
+		try {
+			path = getResourceURI(CONFIG_MM);
+		} catch (URISyntaxException e) {
+			// TODO Re-throw so test fails!
+			e.printStackTrace();
+		} finally {
+			if (path != null) {
+				Resource r = rs.getResource(URI.createURI(path, false), true);
+				EObject eObject = r.getContents().get(0);
+				if (eObject instanceof EPackage) {
+				    EPackage p = (EPackage)eObject;
+				    EPackage.Registry.INSTANCE.put(p.getNsURI(), p);
+				}
+				try {
+					path = getResourceURI(ECORE_CONTAINMENT_MM);
+				} catch (URISyntaxException e) {
+					// TODO Re-throw so test fails!
+					e.printStackTrace();
+				} finally {
+					if (path != null) {
+						r = rs.getResource(URI.createURI(path, false), true);
+						eObject = r.getContents().get(0);
+						if (eObject instanceof EPackage) {
+						    EPackage p = (EPackage)eObject;
+						    EPackage.Registry.INSTANCE.put(p.getNsURI(), p);
+						}
+					}
+					
+				}
+			}
+			
+		}
+	}
+	
+	private String changeResourceToSource(String resourcePath) {
+		
+		String result;
+		if (resourcePath.indexOf("/bin/") > -1) {
+	      result = resourcePath.replaceAll("/bin/", "/src/");
+	    }
+	    else {
+	      result= resourcePath;
+	    }
+		return result;
 	}
 	
 	/**
@@ -54,8 +275,8 @@ public class MtcBroker {
 	 * @param fileName filename to obtain the URI
 	 * @return URI for the provided filename
 	 */
-	private static java.net.URI getURI(String fileName)  {
-	    return java.net.URI.create(URI.createFileURI(fileName).toString());
+	private java.net.URI getURI(String fileName)  {
+	    return java.net.URI.create(URI.createPlatformResourceURI(fileName, false).toString());
 	}
 	
 	/**
@@ -65,11 +286,15 @@ public class MtcBroker {
 	 * @return the URI for the provided resource
 	 * @throws URISyntaxException
 	 */
-	private static String getResourceURI(String resource) throws URISyntaxException {
-		String uri = MtcBroker.class.getResource(resource).toURI().toString();
-	    if (uri.toString().indexOf("bin") > -1) {
-	      uri = uri.toString().replaceAll("bin", "src");
-	    }
+	private String getResourceURI(String resource) throws URISyntaxException {
+		URL r = this.getClass().getResource(resource);
+		String uri = r.toURI().toString();
+	    return uri;
+	}
+	
+	private String getResourceUriFromClass(Class c, String resource) throws URISyntaxException {
+		URL r = c.getResource(resource);
+		String uri = r.toURI().toString();
 	    return uri;
 	}
 	
---
+++ tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/build/etl/tests/QVTdMtcTests.java
@@ -0,0 +1,50 @@
+package org.eclipse.qvtd.build.etl.tests;
+
+import java.net.URL;
+
+import org.eclipse.ocl.examples.domain.utilities.DomainUtil;
+import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
+import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter;
+import org.eclipse.ocl.examples.xtext.essentialocl.services.EssentialOCLLinkingService;
+import org.eclipse.qvtd.build.etl.MtcBroker;
+import org.eclipse.qvtd.xtext.qvtbase.tests.LoadTestCase;
+import org.eclipse.qvtd.xtext.qvtimperative.QVTimperativeStandaloneSetup;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QVTdMtcTests extends LoadTestCase {
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.qvtd.xtext.qvtbase.tests.LoadTestCase#setUp()
+	 */
+	@Before
+    public void setUp() throws Exception {
+	    
+		EssentialOCLLinkingService.DEBUG_RETRY = true;
+		super.setUp();
+		QVTimperativeStandaloneSetup.doSetup();
+		metaModelManager = new MetaModelManager();
+        MetaModelManagerResourceSetAdapter.getAdapter(DomainUtil.nonNullState(resourceSet), metaModelManager);
+    }
+	
+	 /* (non-Javadoc)
+     * @see org.eclipse.qvtd.xtext.qvtbase.tests.LoadTestCase#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+		super.tearDown();
+    }
+    
+    
+    @Test
+    public void testUmlToRdbms() throws Exception {
+    	
+    	URL r = this.getClass().getResource("UmlToRdbms/UmlToRdbms.qvtcas");
+		String qvtcasUri = r.toURI().toString();
+    	MtcBroker mtc = new MtcBroker(qvtcasUri, this.getClass());
+    	mtc.execute();
+    }
+	
+}
