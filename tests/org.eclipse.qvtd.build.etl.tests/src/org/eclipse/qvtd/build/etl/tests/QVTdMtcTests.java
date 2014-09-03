88ddf074239d13af63a3c913a5e1fded5171ca2b
Horacio Hoyos <arcanefoam@gmail.com>
2014-09-03 11:13:04 +0100

    [fb35] The MTC tests now runs on UML2RDBMS, UpperToLower and HSV2HLS

--- tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/build/etl/tests/QVTdMtcTests.java
+++ tests/org.eclipse.qvtd.build.etl.tests/src/org/eclipse/qvtd/build/etl/tests/QVTdMtcTests.java
@@ -8,6 +8,7 @@ import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter
 import org.eclipse.ocl.examples.xtext.essentialocl.services.EssentialOCLLinkingService;
 import org.eclipse.qvtd.build.etl.MtcBroker;
 import org.eclipse.qvtd.xtext.qvtbase.tests.LoadTestCase;
+import org.eclipse.qvtd.xtext.qvtcore.QVTcoreStandaloneSetup;
 import org.eclipse.qvtd.xtext.qvtimperative.QVTimperativeStandaloneSetup;
 import org.junit.After;
 import org.junit.Before;
@@ -24,6 +25,7 @@ public class QVTdMtcTests extends LoadTestCase {
 	    
 		EssentialOCLLinkingService.DEBUG_RETRY = true;
 		super.setUp();
+		QVTcoreStandaloneSetup.doSetup();
 		QVTimperativeStandaloneSetup.doSetup();
 		metaModelManager = new MetaModelManager();
         MetaModelManagerResourceSetAdapter.getAdapter(DomainUtil.nonNullState(resourceSet), metaModelManager);
@@ -42,6 +44,24 @@ public class QVTdMtcTests extends LoadTestCase {
     public void testUmlToRdbms() throws Exception {
     	
     	URL r = this.getClass().getResource("UmlToRdbms/UmlToRdbms.qvtcas");
+		String qvtcasUri = r.toURI().toString();
+    	MtcBroker mtc = new MtcBroker(qvtcasUri, this.getClass());
+    	mtc.execute();
+    }
+    
+    @Test
+    public void testUpperToLower() throws Exception {
+    	
+    	URL r = this.getClass().getResource("UpperToLower/UpperToLower.qvtcas");
+		String qvtcasUri = r.toURI().toString();
+    	MtcBroker mtc = new MtcBroker(qvtcasUri, this.getClass());
+    	mtc.execute();
+    }
+    
+    @Test
+    public void testHSVToHLS() throws Exception {
+    	
+    	URL r = this.getClass().getResource("HSV2HLS/HSV2HLS.qvtc");
 		String qvtcasUri = r.toURI().toString();
     	MtcBroker mtc = new MtcBroker(qvtcasUri, this.getClass());
     	mtc.execute();
