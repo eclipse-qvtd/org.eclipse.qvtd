b3b4449f148a9274667012619ddda0a635035158
Ed Willink <ed@willink.me.uk>
2014-10-21 14:24:41 +0100

    [447935] Add derived ASSaverNormalize visitors

---
+++ plugins/org.eclipse.qvtd.pivot.qvttemplate/emf-gen/org/eclipse/qvtd/pivot/qvttemplate/util/AbstractQVTtemplateASSaverNormalizeVisitor.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * <copyright>
+ * 
+ * Copyright (c) 2013 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   E.D.Willink - Initial API and implementation
+ * 
+ * </copyright>
+ *
+ * This code is auto-generated
+ * from: org.eclipse.qvtd.pivot.qvttemplate/model/QVTtemplate.genmodel
+ *
+ * Only the copyright statement is editable.
+ *******************************************************************************/
+package	org.eclipse.qvtd.pivot.qvttemplate.util;
+
+import java.lang.Object;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.ocl.examples.pivot.utilities.ASSaver;
+import org.eclipse.qvtd.pivot.qvtbase.utilities.QVTbaseASSaverNormalizeVisitor;
+
+/**
+ * An AbstractQVTtemplateASSaverNormalizeVisitor provides a default implementation for each
+ * visitXxx method that delegates to the visitYyy method of the first
+ * super class, (or transitively its first super class' first super class
+ * until a non-interface super-class is found). In the absence of any
+ * suitable first super class, the method delegates to visiting().
+ */
+public abstract class AbstractQVTtemplateASSaverNormalizeVisitor
+	extends QVTbaseASSaverNormalizeVisitor
+	implements QVTtemplateVisitor<Object>
+{
+	/**
+	 * Initializes me with an initial value for my result.
+	 * 
+	 * @param context my initial result value
+	 */
+	protected AbstractQVTtemplateASSaverNormalizeVisitor(@NonNull ASSaver context) {
+		super(context);
+	}	
+
+	public @Nullable Object visitCollectionTemplateExp(@NonNull org.eclipse.qvtd.pivot.qvttemplate.CollectionTemplateExp object) {
+		return visitTemplateExp(object);
+	}
+
+	public @Nullable Object visitObjectTemplateExp(@NonNull org.eclipse.qvtd.pivot.qvttemplate.ObjectTemplateExp object) {
+		return visitTemplateExp(object);
+	}
+
+	public @Nullable Object visitPropertyTemplateItem(@NonNull org.eclipse.qvtd.pivot.qvttemplate.PropertyTemplateItem object) {
+		return visitElement(object);
+	}
+
+	public @Nullable Object visitTemplateExp(@NonNull org.eclipse.qvtd.pivot.qvttemplate.TemplateExp object) {
+		return visitLiteralExp(object);
+	}
+}
