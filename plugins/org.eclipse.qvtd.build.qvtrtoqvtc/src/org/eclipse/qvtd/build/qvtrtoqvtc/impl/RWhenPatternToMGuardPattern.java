4f978ff558dd7dd4efae58080ad1fed11efec721
Horacio Hoyos <arcanefoam@gmail.com>
2014-11-19 14:22:36 +0000

    [unrelated] Added copyright

--- plugins/org.eclipse.qvtd.build.qvtrtoqvtc/src/org/eclipse/qvtd/build/qvtrtoqvtc/impl/RWhenPatternToMGuardPattern.java
+++ plugins/org.eclipse.qvtd.build.qvtrtoqvtc/src/org/eclipse/qvtd/build/qvtrtoqvtc/impl/RWhenPatternToMGuardPattern.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 The University of York and Willink Transformations.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Horacio Hoyos - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.qvtd.build.qvtrtoqvtc.impl;
 
 import org.eclipse.jdt.annotation.NonNull;
