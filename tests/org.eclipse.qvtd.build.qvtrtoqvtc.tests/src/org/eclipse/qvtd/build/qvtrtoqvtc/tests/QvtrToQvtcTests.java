2b841332ad829fd1686cb886b69e52b46cdd2269
Horacio Hoyos <arcanefoam@gmail.com>
2014-12-06 18:48:10 +0000

    [fb46] Added additional examples to the test suite for debugging

--- tests/org.eclipse.qvtd.build.qvtrtoqvtc.tests/src/org/eclipse/qvtd/build/qvtrtoqvtc/tests/QvtrToQvtcTests.java
+++ tests/org.eclipse.qvtd.build.qvtrtoqvtc.tests/src/org/eclipse/qvtd/build/qvtrtoqvtc/tests/QvtrToQvtcTests.java
@@ -46,10 +46,57 @@ public class QvtrToQvtcTests extends LoadTestCase {
 		super.tearDown();
     }
     
-    @Test
-    public void testTransformation() throws Exception {
-//    	System.out.println(getClass().getResource("."));
+    /*@Test
+    public void testAbstractToConcrete() throws Exception {
     	URL projectURL = this.getClass().getResource("abstracttoconcrete/AbstractToConcrete.qvtras");
+    	File f = new File(projectURL.getFile());
+    	URI qvtrURI = URI.createFileURI(f.toString());
+    	Resource qvtrResource = metaModelManager.getASResourceSet().getResource(qvtrURI, true);
+    	URI qvtcURI = qvtrURI.trimFileExtension();
+    	qvtcURI = qvtcURI.appendFileExtension("qvtcas");
+    	Resource qvtcResource = metaModelManager.getASResourceSet().createResource(qvtcURI, null);
+    	URI qvtcTraceURI = qvtrURI.trimFileExtension();
+    	qvtcTraceURI = qvtcTraceURI.appendFileExtension("ecore.oclas");
+    	Resource qvtcTraceResource = metaModelManager.getASResourceSet().createResource(qvtcTraceURI, null);
+    	QvtrToQvtcTransformation t = new QvtrToQvtcTransformation(metaModelManager, qvtrResource, qvtcResource, qvtcTraceResource);
+		t.prepare();
+		t.execute();
+		Map<Object, Object> options = new HashMap<Object, Object>();
+        options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+        t.save(qvtcTraceResource, t.getTraceRoots(), options);
+        assertNoResourceErrors("Trace save", qvtcTraceResource);
+        t.save(qvtcResource, t.getCoreRoots(), options);
+        assertNoResourceErrors("Core save", qvtcResource);
+		
+    }*/
+   
+   /* @Test
+    public void testClassModelToClassModel() throws Exception {
+    	URL projectURL = this.getClass().getResource("classmodeltoclassmodel/ClassModelToClassModel.qvtras");
+    	File f = new File(projectURL.getFile());
+    	URI qvtrURI = URI.createFileURI(f.toString());
+    	Resource qvtrResource = metaModelManager.getASResourceSet().getResource(qvtrURI, true);
+    	URI qvtcURI = qvtrURI.trimFileExtension();
+    	qvtcURI = qvtcURI.appendFileExtension("qvtcas");
+    	Resource qvtcResource = metaModelManager.getASResourceSet().createResource(qvtcURI, null);
+    	URI qvtcTraceURI = qvtrURI.trimFileExtension();
+    	qvtcTraceURI = qvtcTraceURI.appendFileExtension("ecore.oclas");
+    	Resource qvtcTraceResource = metaModelManager.getASResourceSet().createResource(qvtcTraceURI, null);
+    	QvtrToQvtcTransformation t = new QvtrToQvtcTransformation(metaModelManager, qvtrResource, qvtcResource, qvtcTraceResource);
+		t.prepare();
+		t.execute();
+		Map<Object, Object> options = new HashMap<Object, Object>();
+        options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+        t.save(qvtcTraceResource, t.getTraceRoots(), options);
+        assertNoResourceErrors("Trace save", qvtcTraceResource);
+        t.save(qvtcResource, t.getCoreRoots(), options);
+        assertNoResourceErrors("Core save", qvtcResource);
+		
+    }*/
+    
+    @Test
+    public void testSeqToStm() throws Exception {
+    	URL projectURL = this.getClass().getResource("seqtostm/SeqToStm.qvtras");
     	File f = new File(projectURL.getFile());
     	URI qvtrURI = URI.createFileURI(f.toString());
     	Resource qvtrResource = metaModelManager.getASResourceSet().getResource(qvtrURI, true);
