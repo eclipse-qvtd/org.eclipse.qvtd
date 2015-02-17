418ccfa4d87564de6a8ff533c2a437df10a06c40
Adolfo SBH <adolfosbh@gmail.com>
2015-02-17 15:27:47 +0000

    [ocl2qvti] - Generating the classes of example1 in the new project

---
+++ tests/org.eclipse.qvtd.build.cs2as.tests/tests-gen/source/Y2.java
@@ -0,0 +1,17 @@
+/**
+ */
+package source;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Y2</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see source.SourcePackage#getY2()
+ * @model
+ * @generated
+ */
+public interface Y2 extends Y {
+} // Y2
