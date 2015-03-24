6cd128b2e2f68d1fb75e96869fc5fd8274fcffa4
Ed Willink <ed@willink.me.uk>
2015-03-24 15:32:20 +0000

    [unrelated] Fix type of Mapping Loop iterator

--- plugins/org.eclipse.qvtd.xtext.qvtimperative/src/org/eclipse/qvtd/xtext/qvtimperative/cs2as/QVTimperativeCSPreOrderVisitor.java
+++ plugins/org.eclipse.qvtd.xtext.qvtimperative/src/org/eclipse/qvtd/xtext/qvtimperative/cs2as/QVTimperativeCSPreOrderVisitor.java
@@ -11,11 +11,19 @@
 package org.eclipse.qvtd.xtext.qvtimperative.cs2as;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.ocl.pivot.Type;
+import org.eclipse.ocl.pivot.Variable;
 import org.eclipse.ocl.pivot.utilities.PivotUtil;
+import org.eclipse.ocl.xtext.base.cs2as.BasicContinuation;
 import org.eclipse.ocl.xtext.base.cs2as.CS2ASConversion;
 import org.eclipse.ocl.xtext.base.cs2as.Continuation;
+import org.eclipse.ocl.xtext.base.cs2as.PivotDependency;
+import org.eclipse.ocl.xtext.base.cs2as.SingleContinuation;
+import org.eclipse.ocl.xtext.basecs.TypedRefCS;
+import org.eclipse.ocl.xtext.essentialoclcs.VariableCS;
 import org.eclipse.qvtd.pivot.qvtimperative.MappingCall;
 import org.eclipse.qvtd.pivot.qvtimperative.MappingCallBinding;
+import org.eclipse.qvtd.pivot.qvtimperative.MappingLoop;
 import org.eclipse.qvtd.xtext.qvtimperativecs.MappingCS;
 import org.eclipse.qvtd.xtext.qvtimperativecs.MappingCallBindingCS;
 import org.eclipse.qvtd.xtext.qvtimperativecs.MappingCallCS;
@@ -25,15 +33,17 @@ import org.eclipse.qvtd.xtext.qvtimperativecs.util.AbstractQVTimperativeCSPreOrd
 
 public class QVTimperativeCSPreOrderVisitor extends AbstractQVTimperativeCSPreOrderVisitor
 {	
-/*	public static class MappingLoopIteratorCompletion extends SingleContinuation<MappingLoopCS>
+	public static class MappingLoopIteratorCompletion extends SingleContinuation<MappingLoopCS>
 	{
 		protected static PivotDependency[] computeDependencies(@NonNull MappingLoopCS csElement) {
-			if (csElement.getOwnedType() != null) {
-				return new PivotDependency[]{new PivotDependency(csElement.getOwnedType())};
+			VariableCS csIterator = csElement.getOwnedIterator();
+			if (csIterator != null) {
+				TypedRefCS ownedType = csIterator.getOwnedType();
+				if (ownedType != null) {
+					return new PivotDependency[]{new PivotDependency(ownedType)};
+				}
 			}
-			else {
-				return new PivotDependency[0];
-			}
+			return new PivotDependency[0];
 		}
 
 		public MappingLoopIteratorCompletion(@NonNull CS2ASConversion context, @NonNull MappingLoopCS csElement) {
@@ -44,15 +54,18 @@ public class QVTimperativeCSPreOrderVisitor extends AbstractQVTimperativeCSPreOr
 		public BasicContinuation<?> execute() {
 			MappingLoop pivotElement = PivotUtil.getPivot(MappingLoop.class, csElement);
 			if (pivotElement != null) {
-				Variable iterator = pivotElement.getIterator();
+				Variable iterator = pivotElement.getOwnedIterators().get(0);
 				if (iterator != null) {
-					Type type = PivotUtil.getPivot(Type.class, csElement.getOwnedType());
-					iterator.setType(type);
+					VariableCS csIterator = csElement.getOwnedIterator();
+					if (csIterator != null) {
+						Type type = PivotUtil.getPivot(Type.class, csIterator.getOwnedType());
+						iterator.setType(type);
+					}
 				}
 			}
 			return null;
 		}
-	} */
+	}
 
 	public QVTimperativeCSPreOrderVisitor(@NonNull CS2ASConversion context) {
 		super(context);
@@ -83,8 +96,7 @@ public class QVTimperativeCSPreOrderVisitor extends AbstractQVTimperativeCSPreOr
 
 	@Override
 	public Continuation<?> visitMappingLoopCS(@NonNull MappingLoopCS csElement) {
-//		return new MappingLoopIteratorCompletion(context, csElement);
-		return null;
+		return new MappingLoopIteratorCompletion(context, csElement);
 	}
 
 	@Override
