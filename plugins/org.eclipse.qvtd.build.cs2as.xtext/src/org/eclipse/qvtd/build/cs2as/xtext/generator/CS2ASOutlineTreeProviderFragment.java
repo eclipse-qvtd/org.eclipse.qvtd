3325bdd46edd2b2760c6b710cda926f8d0ec04cf
Adolfo SBH <adolfosbh@gmail.com>
2015-04-16 16:51:45 +0100

    [xtextIntegration] - Creating some Xtext generator extensions

---
+++ plugins/org.eclipse.qvtd.build.cs2as.xtext/src/org/eclipse/qvtd/build/cs2as/xtext/generator/CS2ASOutlineTreeProviderFragment.java
@@ -0,0 +1,8 @@
+package org.eclipse.qvtd.build.cs2as.xtext.generator;
+
+import org.eclipse.xtext.ui.generator.outline.OutlineTreeProviderFragment;
+
+public class CS2ASOutlineTreeProviderFragment extends OutlineTreeProviderFragment {
+	
+	
+}
