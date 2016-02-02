be8e0d4223d7d5f4ebcffc8214ba1f6605073b26
Ed Willink <ed@willink.me.uk>
2016-02-02 16:05:31 +0000

    [486722] Eliminate org.eclipse.qvtd.build.qvtrtoqvtc.tests

--- tests/org.eclipse.qvtd.all.tests/src/org/eclipse/qvtd/all/tests/AllQVTdTests.java
+++ tests/org.eclipse.qvtd.all.tests/src/org/eclipse/qvtd/all/tests/AllQVTdTests.java
@@ -15,7 +15,6 @@ package org.eclipse.qvtd.all.tests;
 import java.util.Arrays;
 
 import org.eclipse.emf.common.EMFPlugin;
-import org.eclipse.qvtd.build.qvtrtoqvtc.tests.QvtrToQvtcTests;
 import org.eclipse.qvtd.cs2as.compiler.tests.OCL2QVTiTestCases;
 import org.eclipse.qvtd.xtext.qvtbase.tests.QVTbaseLibraryTests;
 import org.eclipse.qvtd.xtext.qvtcore.tests.AllQVTcoreTests;
@@ -53,7 +52,6 @@ public class AllQVTdTests extends TestCase
 			result.addTestSuite(GrammarTests.class);		// *.xtextbin fail to load in Eclipse, but we don't need to test twice anyway.
 		}
 		if (!EMFPlugin.IS_ECLIPSE_RUNNING) {		// FIXME should work as plugin test too
-			result.addTestSuite(QvtrToQvtcTests.class);
 			result.addTestSuite(OCL2QVTiTestCases.class);
 		}
 		return result;
