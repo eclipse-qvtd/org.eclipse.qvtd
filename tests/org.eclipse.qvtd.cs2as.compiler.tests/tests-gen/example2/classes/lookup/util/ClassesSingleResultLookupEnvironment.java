00120651918ddda0e97394a4ed36bcbbe7dcba99
Adolfo SBH <adolfosbh@gmail.com>
2016-03-21 18:27:45 +0000

    [cs2as] - Redefining Example2 to support aliases

--- tests/org.eclipse.qvtd.cs2as.compiler.tests/tests-gen/example2/classes/lookup/util/ClassesSingleResultLookupEnvironment.java
+++ tests/org.eclipse.qvtd.cs2as.compiler.tests/tests-gen/example2/classes/lookup/util/ClassesSingleResultLookupEnvironment.java
@@ -13,8 +13,12 @@ package	example2.classes.lookup.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.ECollections;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
@@ -29,7 +33,9 @@ public class ClassesSingleResultLookupEnvironment extends LookupEnvironmentImpl 
 	private @NonNull Executor executor;
 	private @NonNull String name;
 	private @NonNull EClass typeFilter;
+	private @NonNull Map<@NonNull String, List<NamedElement>> entries = new LinkedHashMap<@NonNull String,List<NamedElement>>();
 	private @Nullable ClassesLookupFilter expFilter;
+	
 	
 	public ClassesSingleResultLookupEnvironment(@NonNull Executor executor, @NonNull EClass typeFilter, @NonNull String name,  @Nullable ClassesLookupFilter expFilter) {
 		this.executor = executor;
@@ -50,31 +56,16 @@ public class ClassesSingleResultLookupEnvironment extends LookupEnvironmentImpl 
 	
 	@Override
 	public boolean hasFinalResult() {
-		for (NamedElement element : getNamedElements()) {
-			if (name.equals(element.getName())) {
-				return true;
-			}
-		}
-		return false;
+		return getNamedElements(name).size() > 0;
 	}
 	
 	@Override
 	@NonNull
 	public LookupEnvironment addElement(@Nullable NamedElement namedElement) {
-		if (namedElement != null) {
-			if (name.equals(namedElement.getName())) {
-				if (typeFilter.isInstance(namedElement)) {
-				    ClassesLookupFilter expFilter2 = expFilter;
-					if (expFilter2 == null || expFilter2.matches(namedElement)) {
-						List<NamedElement> elements = getNamedElements();
-						if (!elements.contains(namedElement)) { 	// FIXME use a set ?
-							elements.add(namedElement);
-						}
-					}
-				}
-			}
+		if (namedElement == null) {
+			return this;
 		}
-		return this;
+		return addElementWithName(namedElement.getName(), namedElement);
 	}
 	
 	@Override
@@ -90,10 +81,39 @@ public class ClassesSingleResultLookupEnvironment extends LookupEnvironmentImpl 
 		return this;
 	}
 	
+	
+	@Override
+	@NonNull
+	public LookupEnvironment addElementWithName(String entryName, NamedElement namedElement) {
+		if (namedElement != null) {
+			if (name.equals(entryName)) {
+				if (typeFilter.isInstance(namedElement)) {
+				    ClassesLookupFilter expFilter2 = expFilter;
+					if (expFilter2 == null || expFilter2.matches(namedElement)) {
+						List<NamedElement> elements = getNamedElements(entryName);
+						if (!elements.contains(namedElement)) { 	// FIXME use a set ?
+							elements.add(namedElement);
+						}
+					}
+				}
+			}
+		}
+		return this;
+	}
+	
+	@Override
+	@NonNull
+	@SuppressWarnings("null")
+	public EList<NamedElement> getNamedElements(String entryName) {
+		List<NamedElement> namedElements = entries.get(entryName);
+		return namedElements == null ? ECollections.emptyEList() : ECollections.asEList(namedElements);
+	}
+	
+	@NonNull
 	@SuppressWarnings("unchecked")
 	public <NE extends NamedElement> List<NE> getNamedElementsByKind(Class<NE> class_) {
 		List<NE> result = new ArrayList<NE>(); 
-		for (NamedElement namedElement : getNamedElements()) {
+		for (NamedElement namedElement : getNamedElements(name)) {
 			if (class_.isAssignableFrom(namedElement.getClass())) {
 				result.add((NE)namedElement);
 			}
