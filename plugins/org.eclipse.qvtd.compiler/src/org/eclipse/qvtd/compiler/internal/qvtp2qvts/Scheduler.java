39dde26a2e98141ab2a2094342a6f9b8085929e0
Ed Willink <ed@willink.me.uk>
2016-06-27 15:06:35 +0100

    [486722] Reduce QVTp2QVTs / QVTs2QVTi coupling

--- plugins/org.eclipse.qvtd.compiler/src/org/eclipse/qvtd/compiler/internal/qvtp2qvts/Scheduler.java
+++ plugins/org.eclipse.qvtd.compiler/src/org/eclipse/qvtd/compiler/internal/qvtp2qvts/Scheduler.java
@@ -20,39 +20,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.ocl.pivot.ExpressionInOCL;
-import org.eclipse.ocl.pivot.Import;
-import org.eclipse.ocl.pivot.Iteration;
 import org.eclipse.ocl.pivot.LanguageExpression;
-import org.eclipse.ocl.pivot.LoopExp;
-import org.eclipse.ocl.pivot.Model;
-import org.eclipse.ocl.pivot.NamedElement;
-import org.eclipse.ocl.pivot.Namespace;
 import org.eclipse.ocl.pivot.OCLExpression;
 import org.eclipse.ocl.pivot.Operation;
 import org.eclipse.ocl.pivot.OperationCallExp;
-import org.eclipse.ocl.pivot.OppositePropertyCallExp;
-import org.eclipse.ocl.pivot.Property;
-import org.eclipse.ocl.pivot.PropertyCallExp;
 import org.eclipse.ocl.pivot.utilities.ClassUtil;
 import org.eclipse.ocl.pivot.utilities.EnvironmentFactory;
 import org.eclipse.ocl.pivot.utilities.NameUtil;
 import org.eclipse.ocl.pivot.utilities.ParserException;
-import org.eclipse.ocl.pivot.utilities.PivotUtil;
 import org.eclipse.ocl.pivot.utilities.TracingOption;
 import org.eclipse.qvtd.compiler.CompilerConstants;
-import org.eclipse.qvtd.compiler.internal.qvts2qvti.QVTs2QVTiVisitor;
-import org.eclipse.qvtd.compiler.internal.utilities.SymbolNameReservation;
-import org.eclipse.qvtd.pivot.qvtbase.Transformation;
-import org.eclipse.qvtd.pivot.qvtbase.TypedModel;
-import org.eclipse.qvtd.pivot.qvtbase.utilities.TreeIterable;
-import org.eclipse.qvtd.pivot.qvtimperative.ImperativeModel;
-import org.eclipse.qvtd.pivot.qvtimperative.QVTimperativePackage;
 import org.eclipse.qvtd.pivot.schedule.AbstractAction;
 import org.eclipse.qvtd.pivot.schedule.ClassDatum;
 import org.eclipse.qvtd.pivot.schedule.MappingAction;
@@ -323,107 +303,6 @@ public class Scheduler extends SchedulerConstants
 		RootScheduledRegion rootRegion = createRootRegion(allRegions);
 		rootRegion.createSchedule();
 		return rootRegion;
-	}
-
-	public @NonNull Resource qvts2qvti(@NonNull RootScheduledRegion scheduledRegion, @NonNull URI qvtiURI, @NonNull SymbolNameReservation symbolNameReservation) {
-		Transformation transformation = getTransformation();
-		QVTs2QVTiVisitor visitor = new QVTs2QVTiVisitor(getEnvironmentFactory(), transformation, symbolNameReservation);
-		Transformation qvtiTransformation = (Transformation)scheduledRegion.accept(visitor);
-		NamedElement qvtiChild = qvtiTransformation;
-		for (org.eclipse.ocl.pivot.Package qvtpPackage = transformation.getOwningPackage(); qvtpPackage != null; qvtpPackage = qvtpPackage.getOwningPackage()) {
-			org.eclipse.ocl.pivot.@NonNull Package qvtiPackage = visitor.createPackage(ClassUtil.nonNull(qvtpPackage.getName()), qvtpPackage.getNsPrefix(), qvtpPackage.getURI());
-			if (qvtiChild instanceof Transformation) {
-				qvtiPackage.getOwnedClasses().add((Transformation)qvtiChild);
-			}
-			else {
-				qvtiPackage.getOwnedPackages().add((org.eclipse.ocl.pivot.Package)qvtiChild);
-			}
-			qvtiChild = qvtiPackage;
-		}
-		Model model = PivotUtil.createModel(ImperativeModel.class, QVTimperativePackage.Literals.IMPERATIVE_MODEL, null);
-		model.getOwnedPackages().add((org.eclipse.ocl.pivot.Package)qvtiChild);
-		Set<@NonNull Namespace> importedNamespaces = new HashSet<@NonNull Namespace>();
-		for (EObject eObject : new TreeIterable(model, false)) {
-			if (eObject instanceof TypedModel) {
-				for (Namespace importedNamespace : ClassUtil.nullFree(((TypedModel)eObject).getUsedPackage())) {
-					if (!importedNamespaces.contains(importedNamespace)) {
-						importedNamespaces.add(importedNamespace);
-					}
-				}
-			}
-			else if (eObject instanceof OperationCallExp) {
-				Operation asOperation = ((OperationCallExp)eObject).getReferredOperation();
-				if (asOperation != null) {
-					org.eclipse.ocl.pivot.Class asClass = asOperation.getOwningClass();
-					if (asClass != null) {
-						org.eclipse.ocl.pivot.Package asPackage = asClass.getOwningPackage();
-						if (asPackage != null) {
-							importedNamespaces.add(asPackage);
-						}
-					}
-				}
-			}
-			else if (eObject instanceof LoopExp) {
-				Iteration asIteration = ((LoopExp)eObject).getReferredIteration();
-				if (asIteration != null) {
-					org.eclipse.ocl.pivot.Class asClass = asIteration.getOwningClass();
-					if (asClass != null) {
-						org.eclipse.ocl.pivot.Package asPackage = asClass.getOwningPackage();
-						if (asPackage != null) {
-							importedNamespaces.add(asPackage);
-						}
-					}
-				}
-			}
-			else if (eObject instanceof PropertyCallExp) {
-				Property asProperty = ((PropertyCallExp)eObject).getReferredProperty();
-				if (asProperty != null) {
-					org.eclipse.ocl.pivot.Class asClass = asProperty.getOwningClass();
-					if (asClass != null) {
-						org.eclipse.ocl.pivot.Package asPackage = asClass.getOwningPackage();
-						if (asPackage != null) {
-							importedNamespaces.add(asPackage);
-						}
-					}
-				}
-			}
-			else if (eObject instanceof OppositePropertyCallExp) {
-				Property asProperty = ((OppositePropertyCallExp)eObject).getReferredProperty();
-				if (asProperty != null) {
-					org.eclipse.ocl.pivot.Class asClass = asProperty.getOwningClass();
-					if (asClass != null) {
-						org.eclipse.ocl.pivot.Package asPackage = asClass.getOwningPackage();
-						if (asPackage != null) {
-							importedNamespaces.add(asPackage);
-						}
-					}
-				}
-			}
-		}
-		List<@NonNull Namespace> sortedImportedNamespaces = new ArrayList<@NonNull Namespace>(importedNamespaces);
-//		for (@NonNull TypedModel typedModel : ClassUtil.nullFree(qvtiTransformation.getModelParameter())) {
-//			for (Namespace importedNamespace : ClassUtil.nullFree(typedModel.getUsedPackage())) {
-//				if (!importedNamespaces.contains(importedNamespace)) {
-//					importedNamespaces.add(importedNamespace);
-//				}
-//			}
-//		}
-		Collections.sort(sortedImportedNamespaces, NameUtil.NAMEABLE_COMPARATOR);
-		List<Import> ownedImports = model.getOwnedImports();
-		for (@NonNull Namespace importedNamespace : importedNamespaces) {
-			ownedImports.add(visitor.createImport(null, importedNamespace));
-		}
-		
-		
-		
-		
-		
-		assert model != null;
-		model.setExternalURI(qvtiURI.toString());
-		Resource eResource = getDependencyGraph().eResource();
-		Resource resource = eResource.getResourceSet().createResource(qvtiURI);
-		resource.getContents().add(model);
-		return resource;
 	}
 
 	/**
