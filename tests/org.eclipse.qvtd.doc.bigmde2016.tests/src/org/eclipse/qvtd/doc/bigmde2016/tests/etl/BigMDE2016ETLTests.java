9be0f898064634e895cf3093239b80eb8260e643
Ed Willink <ed@willink.me.uk>
2016-07-17 10:45:50 +0100

    [exe2016] Fix ETL Families2Persons launch

--- tests/org.eclipse.qvtd.doc.bigmde2016.tests/src/org/eclipse/qvtd/doc/bigmde2016/tests/etl/BigMDE2016ETLTests.java
+++ tests/org.eclipse.qvtd.doc.bigmde2016.tests/src/org/eclipse/qvtd/doc/bigmde2016/tests/etl/BigMDE2016ETLTests.java
@@ -5,7 +5,9 @@ import java.io.InputStream;
 import java.util.Collection;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.emc.emf.EmfModelFactory;
 import org.eclipse.epsilon.emc.emf.EmfModelFactory.AccessMode;
@@ -49,24 +51,36 @@ public class BigMDE2016ETLTests extends TestCase {
 		/*
 		 * Initializations
 		 */
-		EtlModule transformationLauncher = new EtlModule();
-		EmfModelFactory modelFactory = EmfModelFactory.getInstance();
-		String etlpath = "src/org/eclipse/qvtd/doc/bigmde2016/tests/etl/Families2Persons.etl";
-		File file = new File(etlpath);
-		transformationLauncher.parse(file);
 		try {
 	        int[] tests = PrintAndLog.getTestSizes();
 	        for (int testSize : tests) {
+	    		EtlModule transformationLauncher = new EtlModule();
+	    		EmfModelFactory modelFactory = EmfModelFactory.getInstance();
+	    		String etlpath = "src/org/eclipse/qvtd/doc/bigmde2016/tests/etl/Families2Persons.etl";
+	    		File file = new File(etlpath);
+	    		transformationLauncher.parse(file);
 	    		/*
 	    		 * Load models
 	    		 */
-	        	file = new File("src/org/eclipse/qvtd/doc/bigmde2016/tests/atl/samples-Families.xmi");
-	        	EmfModel familiesModel = modelFactory.loadEmfModel("Families", file, FamiliesPackage.eINSTANCE, AccessMode.READ_ONLY);
+//	        	file = new File("src/org/eclipse/qvtd/doc/bigmde2016/tests/atl/samples-Families.xmi");
+//	        	EmfModel familiesModel = modelFactory.loadEmfModel("Families", file, FamiliesPackage.eINSTANCE, AccessMode.READ_ONLY);
+	        	EmfModel familiesModel = new EmfModel();
+	        	familiesModel.setName("Families");
+	        	familiesModel.setMetamodelFileBased(false);
+	        	familiesModel.setMetamodelUri(FamiliesPackage.eINSTANCE.getNsURI());
+	        	familiesModel.setResource(new XMIResourceImpl());
+	        	AccessMode.READ_ONLY.applyTo(familiesModel);
 	    		Collection<@NonNull ? extends EObject> rootObjects = FamiliesGenerator.createFamiliesModel(testSize, 9);
 				familiesModel.getResource().getContents().clear();
 				familiesModel.getResource().getContents().addAll(rootObjects);
-				file = new File("src/org/eclipse/qvtd/doc/bigmde2016/tests/atl/samples-Persosns.xmi");
-				EmfModel personsModel = modelFactory.createEmfModel("Persons", file, PersonsPackage.eINSTANCE, AccessMode.WRITE_ONLY);
+//				file = new File("src/org/eclipse/qvtd/doc/bigmde2016/tests/atl/samples-Persons.xmi");
+//				EmfModel personsModel = modelFactory.createEmfModel("Persons", file, PersonsPackage.eINSTANCE, AccessMode.WRITE_ONLY);
+				EmfModel personsModel = new EmfModel();
+				personsModel.setName("Persons");
+				personsModel.setMetamodelFileBased(false);
+				personsModel.setMetamodelUri(PersonsPackage.eINSTANCE.getNsURI());
+				personsModel.setResource(new XMIResourceImpl());
+	        	AccessMode.WRITE_ONLY.applyTo(personsModel);
 				transformationLauncher.getContext().getModelRepository().addModel(familiesModel);
 				transformationLauncher.getContext().getModelRepository().addModel(personsModel);
 				/*
