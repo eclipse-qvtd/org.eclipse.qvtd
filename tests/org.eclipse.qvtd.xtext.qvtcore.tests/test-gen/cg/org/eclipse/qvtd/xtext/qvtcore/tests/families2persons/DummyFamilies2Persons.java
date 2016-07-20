7f1b04e5a6397fc1dcc38999541801d9017c1dc6
Ed Willink <ed@willink.me.uk>
2016-07-20 12:01:23 +0100

    [exe2016] Ensure autogenerated packages exist

---
+++ tests/org.eclipse.qvtd.xtext.qvtcore.tests/test-gen/cg/org/eclipse/qvtd/xtext/qvtcore/tests/families2persons/DummyFamilies2Persons.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Willink Transformations and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   E.D.Willink - Initial API and implementation
+ *******************************************************************************/
+package cg.org.eclipse.qvtd.xtext.qvtcore.tests.families2persons;
+
+/**
+ * This class is solely used to ensure an otherwise auto-generated package exists when loaded from GIT.
+ */
+public class DummyFamilies2Persons
+{
+}
