e57e3a73442cc14abcea7b2b5d5269b96bc60fa9
Ed Willink <ed@willink.me.uk>
2016-09-18 11:17:52 +0100

    [500369] Refresh QVTi outline

--- plugins/org.eclipse.qvtd.xtext.qvtimperative.ui/src/org/eclipse/qvtd/xtext/qvtimperative/ui/labeling/QVTimperativeLabelProvider.java
+++ plugins/org.eclipse.qvtd.xtext.qvtimperative.ui/src/org/eclipse/qvtd/xtext/qvtimperative/ui/labeling/QVTimperativeLabelProvider.java
@@ -24,7 +24,12 @@ import org.eclipse.qvtd.pivot.qvtimperative.MappingCall;
 import org.eclipse.qvtd.pivot.qvtimperative.MappingCallBinding;
 import org.eclipse.qvtd.pivot.qvtimperative.MappingLoop;
 import org.eclipse.qvtd.pivot.qvtimperative.NewStatement;
+import org.eclipse.qvtd.pivot.qvtimperative.AddStatement;
+import org.eclipse.qvtd.pivot.qvtimperative.CheckStatement;
+import org.eclipse.qvtd.pivot.qvtimperative.ConnectionVariable;
 import org.eclipse.qvtd.pivot.qvtimperative.DeclareStatement;
+import org.eclipse.qvtd.pivot.qvtimperative.GuardVariable;
+import org.eclipse.qvtd.pivot.qvtimperative.LoopVariable;
 import org.eclipse.qvtd.pivot.qvtimperative.SetStatement;
 import org.eclipse.qvtd.xtext.qvtbase.ui.labeling.QVTbaseLabelProvider;
 import org.eclipse.qvtd.xtext.qvtimperativecs.TopLevelCS;
@@ -45,9 +50,29 @@ public class QVTimperativeLabelProvider extends QVTbaseLabelProvider
 		super(delegate);
 	}
 
-	//	protected String image(Direction ele) {
-	//		return QVTCOREBASE_UI_ICONS + "Direction.gif";
-	//	}
+	protected String image(AddStatement ele) {
+		return "AddStatement.gif";
+	}
+
+	protected String image(CheckStatement ele) {
+		return "CheckStatement.gif";
+	}
+
+	protected String image(ConnectionVariable ele) {
+		return "ConnectionVariable.gif";
+	}
+
+	protected String image(DeclareStatement ele) {
+		return QVTIMPERATIVE_UI_ICONS + "DeclareStatement.gif";
+	}
+
+	protected String image(GuardVariable ele) {
+		return "GuardVariable.gif";
+	}
+
+	protected String image(LoopVariable ele) {
+		return "LoopVariable.gif";
+	}
 
 	protected String image(Mapping ele) {
 		return QVTIMPERATIVE_UI_ICONS + "Mapping.gif";
@@ -61,12 +86,12 @@ public class QVTimperativeLabelProvider extends QVTbaseLabelProvider
 		return "MappingCallBinding.gif";
 	}
 
-	protected String image(NewStatement ele) {
-		return QVTIMPERATIVE_UI_ICONS + "NewStatement.gif";
+	protected String image(MappingLoop ele) {
+		return "MappingLoop.gif";
 	}
 
-	protected String image(DeclareStatement ele) {
-		return QVTIMPERATIVE_UI_ICONS + "CheckVariableStatement.gif";
+	protected String image(NewStatement ele) {
+		return QVTIMPERATIVE_UI_ICONS + "NewStatement.gif";
 	}
 
 	protected String image(SetStatement ele) {
@@ -77,20 +102,42 @@ public class QVTimperativeLabelProvider extends QVTbaseLabelProvider
 		return "QVTiModelFile.gif";
 	}
 
-	protected String text(MappingCall ele) {
-		return NameUtil.getSafeName(ele.getReferredMapping());
+	protected String text(AddStatement ele) {
+		assert ele != null;
+		Namespace namespace = PivotUtil.getNamespace(ele.eContainer());
+		if (namespace == null) {
+			return "";
+		}
+		StringBuilder s = new StringBuilder();
+		VariableDeclaration targetVariable = ele.getTargetVariable();
+		if (targetVariable != null) {
+			s.append(PrettyPrinter.printName(targetVariable, namespace));
+		}
+		s.append(" += ");
+		return s.toString();
 	}
 
-	protected String text(MappingCallBinding ele) {
-		return NameUtil.getSafeName(ele.getBoundVariable());
+	protected String text(ConnectionVariable ele) {
+		assert ele != null;
+		Namespace namespace = PivotUtil.getNamespace(ele.eContainer());
+		if (namespace == null) {
+			return "";
+		}
+		StringBuilder s = new StringBuilder();
+		s.append(PrettyPrinter.printName(ele, namespace));
+		s.append(" : ");
+		Type type = ele.getType();
+		if (type != null) {
+			s.append(PrettyPrinter.printType(type, namespace));
+		}
+		return s.toString();
 	}
 
-	protected String text(MappingLoop ele) {
-		return "«for»";
-	}
-
-	protected String text(Predicate ele) {
-		return "«predicate»";
+	protected String text(CheckStatement ele) {
+		assert ele != null;
+		StringBuilder s = new StringBuilder();
+		s.append(PrettyPrinter.print(ele));
+		return s.toString();
 	}
 
 	protected String text(DeclareStatement ele) {
@@ -108,6 +155,54 @@ public class QVTimperativeLabelProvider extends QVTbaseLabelProvider
 		}
 		s.append(" := ");
 		return s.toString();
+	}
+
+	protected String text(GuardVariable ele) {
+		assert ele != null;
+		Namespace namespace = PivotUtil.getNamespace(ele.eContainer());
+		if (namespace == null) {
+			return "";
+		}
+		StringBuilder s = new StringBuilder();
+		s.append(PrettyPrinter.printName(ele, namespace));
+		s.append(" : ");
+		Type type = ele.getType();
+		if (type != null) {
+			s.append(PrettyPrinter.printType(type, namespace));
+		}
+		return s.toString();
+	}
+
+	protected String text(LoopVariable ele) {
+		assert ele != null;
+		Namespace namespace = PivotUtil.getNamespace(ele.eContainer());
+		if (namespace == null) {
+			return "";
+		}
+		StringBuilder s = new StringBuilder();
+		s.append(PrettyPrinter.printName(ele, namespace));
+		s.append(" : ");
+		Type type = ele.getType();
+		if (type != null) {
+			s.append(PrettyPrinter.printType(type, namespace));
+		}
+		return s.toString();
+	}
+
+	protected String text(MappingCall ele) {
+		return NameUtil.getSafeName(ele.getReferredMapping());
+	}
+
+	protected String text(MappingCallBinding ele) {
+		return NameUtil.getSafeName(ele.getBoundVariable());
+	}
+
+	protected String text(MappingLoop ele) {
+		return "«for»";
+	}
+
+	protected String text(Predicate ele) {
+		return "«predicate»";
 	}
 
 	protected String text(SetStatement ele) {
