66743457fc050c5b7914f231b70ceee4f43c5dc4
Ed Willink <ed@willink.me.uk>
2017-02-01 11:35:27 +0000

    [495621] Fix UMLX contentType

--- plugins/org.eclipse.qvtd.umlx/emf-gen/org/eclipse/qvtd/umlx/UMLXPackage.java
+++ plugins/org.eclipse.qvtd.umlx/emf-gen/org/eclipse/qvtd/umlx/UMLXPackage.java
@@ -56,6 +56,14 @@ public interface UMLXPackage extends EPackage {
 	String eNS_PREFIX = "umlx";
 
 	/**
+	 * The package content type ID.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String eCONTENT_TYPE = "org.eclipse.qvtd.umlx";
+
+	/**
 	 * The singleton instance of the package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
