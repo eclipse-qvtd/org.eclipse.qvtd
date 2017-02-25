fa06dd93af926848414983b9ab863c06841ba938
Ed Willink <ed@willink.me.uk>
2017-02-25 11:22:01 +0000

    [unrelated] Normalize Transformation query order

--- plugins/org.eclipse.qvtd.pivot.qvtimperative/src/org/eclipse/qvtd/pivot/qvtimperative/utilities/QVTimperativeASSaverNormalizeVisitor.java
+++ plugins/org.eclipse.qvtd.pivot.qvtimperative/src/org/eclipse/qvtd/pivot/qvtimperative/utilities/QVTimperativeASSaverNormalizeVisitor.java
@@ -13,8 +13,10 @@ package	org.eclipse.qvtd.pivot.qvtimperative.utilities;
 import java.util.Comparator;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.ocl.pivot.Operation;
 import org.eclipse.ocl.pivot.internal.resource.ASSaver;
 import org.eclipse.ocl.pivot.utilities.ClassUtil;
+import org.eclipse.qvtd.pivot.qvtbase.Transformation;
 import org.eclipse.qvtd.pivot.qvtimperative.CheckStatement;
 import org.eclipse.qvtd.pivot.qvtimperative.Mapping;
 import org.eclipse.qvtd.pivot.qvtimperative.MappingCall;
@@ -22,6 +24,18 @@ import org.eclipse.qvtd.pivot.qvtimperative.util.AbstractQVTimperativeASSaverNor
 
 public class QVTimperativeASSaverNormalizeVisitor extends AbstractQVTimperativeASSaverNormalizeVisitor
 {
+	protected static final class OperationComparator implements Comparator<@NonNull Operation>
+	{
+		public static final @NonNull Comparator<@NonNull Operation> INSTANCE = new OperationComparator();
+
+		@Override
+		public int compare(@NonNull Operation o1, @NonNull Operation o2) {
+			String n1 = o1.getOperationId().getDisplayName();
+			String n2 = o2.getOperationId().getDisplayName();
+			return n1.compareTo(n2);
+		}
+	}
+
 	protected static final class PredicateComparator implements Comparator<@NonNull CheckStatement>
 	{
 		public static final @NonNull Comparator<@NonNull CheckStatement> INSTANCE = new PredicateComparator();
@@ -55,5 +69,11 @@ public class QVTimperativeASSaverNormalizeVisitor extends AbstractQVTimperativeA
 	public @Nullable Boolean visitMappingCall(@NonNull MappingCall object) {
 		ClassUtil.sort(ClassUtil.nullFree(object.getOwnedMappingParameterBindings()), QVTimperativeUtil.MappingParameterBindingComparator.INSTANCE);
 		return null;
+	}
+
+	@Override
+	public @Nullable Object visitTransformation(@NonNull Transformation object) {
+		ClassUtil.sort(ClassUtil.nullFree(object.getOwnedOperations()), OperationComparator.INSTANCE);
+		return super.visitTransformation(object);
 	}
 }
