ce471e740c55d9e2e24fb1a7b63379e782e8e666
Ed Willink <ed@willink.me.uk>
2017-05-23 11:19:07 +0100

    [releng] Fix plugin copyrights

--- plugins/org.eclipse.qvtd.compiler/src/org/eclipse/qvtd/compiler/internal/qvtr2qvtc/AbstractQVTr2QVTcRelations.java
+++ plugins/org.eclipse.qvtd.compiler/src/org/eclipse/qvtd/compiler/internal/qvtr2qvtc/AbstractQVTr2QVTcRelations.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2016 The University of York and Willink Transformations.
+ * Copyright (c) 2014, 2017 The University of York and Willink Transformations.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
