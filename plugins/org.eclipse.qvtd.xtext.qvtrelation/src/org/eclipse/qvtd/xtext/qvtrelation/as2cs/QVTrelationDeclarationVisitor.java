ff129a57988124065baa8cc1abb6ed5f5b5728cd
Ed Willink <ed@willink.me.uk>
2019-09-12 19:04:56 +0100

    [unrelated] Fix serialization of optional SharedVariable

--- plugins/org.eclipse.qvtd.xtext.qvtrelation/src/org/eclipse/qvtd/xtext/qvtrelation/as2cs/QVTrelationDeclarationVisitor.java
+++ plugins/org.eclipse.qvtd.xtext.qvtrelation/src/org/eclipse/qvtd/xtext/qvtrelation/as2cs/QVTrelationDeclarationVisitor.java
@@ -368,7 +368,7 @@ public class QVTrelationDeclarationVisitor extends QVTbaseDeclarationVisitor imp
 			TemplateExp templateExpression = asDomainPattern.getTemplateExpression();
 			VariableDeclaration rootVariable = templateExpression.getBindsTo();
 			csDomainPattern.setName(rootVariable.getName());
-			csDomainPattern.setOwnedType(createTypeRefCS(rootVariable));
+			csDomainPattern.setOwnedType(createTypeRefCS(rootVariable, true));
 			return csDomainPattern;
 		}
 	}
@@ -658,7 +658,7 @@ public class QVTrelationDeclarationVisitor extends QVTbaseDeclarationVisitor imp
 		csVarDeclarationId.setPivot(asSharedVariable);
 		VarDeclarationCS csVarDeclaration = context.refreshElement(VarDeclarationCS.class, QVTrelationCSPackage.Literals.VAR_DECLARATION_CS, asSharedVariable);
 		csVarDeclaration.getOwnedVarDeclarationIds().add(csVarDeclarationId);
-		csVarDeclaration.setOwnedType(createTypeRefCS(asSharedVariable));
+		csVarDeclaration.setOwnedType(createTypeRefCS(asSharedVariable, true));
 		csVarDeclaration.setOwnedInitExpression(context.visitDeclaration(ExpCS.class, asSharedVariable.getOwnedInit()));
 		return csVarDeclaration;
 	}
