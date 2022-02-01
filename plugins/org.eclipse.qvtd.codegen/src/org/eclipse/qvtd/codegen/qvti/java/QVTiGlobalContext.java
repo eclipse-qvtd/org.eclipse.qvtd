70d76194fdc2964db68da66dae26c142e7452f57
Ed Willink <ed@willink.me.uk>
2022-02-01 16:17:21 +0000

    ok

--- plugins/org.eclipse.qvtd.codegen/src/org/eclipse/qvtd/codegen/qvti/java/QVTiGlobalContext.java
+++ plugins/org.eclipse.qvtd.codegen/src/org/eclipse/qvtd/codegen/qvti/java/QVTiGlobalContext.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.ocl.examples.codegen.cgmodel.CGElement;
 import org.eclipse.ocl.examples.codegen.cgmodel.CGNamedElement;
 import org.eclipse.ocl.examples.codegen.java.JavaConstants;
 import org.eclipse.ocl.examples.codegen.java.JavaGlobalContext;
@@ -91,10 +90,10 @@ public class QVTiGlobalContext extends JavaGlobalContext<@NonNull QVTiCodeGenera
 		return new QVTiLocalContext(this, (QVTiLocalContext) outerContext, cgNamedElement, asNamedElement);
 	}
 
-	@Override
-	protected @NonNull QVTiLocalContext createNestedContext(@NonNull CGElement cgScope) {
-		return new QVTiLocalContext(this, null, (CGNamedElement) cgScope, (NamedElement)((CGNamedElement)cgScope).getAst()); //, true);
-	}
+	//	@Override
+	//	protected @NonNull QVTiLocalContext createNestedContext(@NonNull CGElement cgScope) {
+	//		return new QVTiLocalContext(this, null, (CGNamedElement) cgScope, (NamedElement)((CGNamedElement)cgScope).getAst()); //, true);
+	//	}
 
 	@Override
 	public @NonNull QVTiLocalContext getLocalContext( @NonNull CGNamedElement cgElement) {
