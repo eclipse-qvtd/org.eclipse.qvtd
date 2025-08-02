46eed4a9a7f3eb27655fcb2777a4456675916527
Ed Willink <ed@willink.me.uk>
2025-08-02 12:15:30 +0100

    [#444] Suppress MiniOCL

--- tests/org.eclipse.qvtd.all.tests/src/org/eclipse/qvtd/all/tests/AllQVTdTests.java
+++ tests/org.eclipse.qvtd.all.tests/src/org/eclipse/qvtd/all/tests/AllQVTdTests.java
@@ -15,7 +15,7 @@ import org.eclipse.qvtd.compiler.tests.AllCompilerTests;
 import org.eclipse.qvtd.cs2as.compiler.tests.OCL2QVTiTestCases;
 import org.eclipse.qvtd.doc.bigmde2016.tests.AllbigMDE2016Tests;
 import org.eclipse.qvtd.doc.exe2016.tests.Allexe2016Tests;
-import org.eclipse.qvtd.doc.minioclcs.xtext.tests.MiniOCLCSParsingTest;
+//import org.eclipse.qvtd.doc.minioclcs.xtext.tests.MiniOCLCSParsingTest;
 import org.eclipse.qvtd.umlx.tests.AllUMLXTests;
 import org.eclipse.qvtd.xtext.qvtbase.tests.QVTruntimeLibraryTests;
 import org.eclipse.qvtd.xtext.qvtcore.tests.AllQVTcoreTests;
@@ -51,7 +51,7 @@ public class AllQVTdTests extends TestCase
 		// result.addTest(AllATLTests.suite()); -- FIXME waiting for BUG 514604
 		result.addTest(AllbigMDE2016Tests.suite());	// NB this is very slow since we are -ea
 		result.addTest(Allexe2016Tests.suite());		// NB this is very slow since we are -ea
-		result.addTestSuite(MiniOCLCSParsingTest.class);
+		//		result.addTestSuite(MiniOCLCSParsingTest.class);
 		return result;
 	}
 
